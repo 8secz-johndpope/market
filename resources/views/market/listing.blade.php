1acc7900a8a0f03b175d2aea9b95b1b055e03dc9
anil <anil@sumra.net>
2017-08-05 14:47:33 +0100

    auto

--- resources/views/market/listing.blade.php
+++ resources/views/market/listing.blade.php
@@ -44,7 +44,7 @@
     <link rel="next" href="/category/322/?page=2">
 
 
-    <link href="/css/app.css?3198405465" rel="stylesheet">
+    <link href="/css/mercari.css?3198405465" rel="stylesheet">
 
     <link rel="apple-touch-icon" sizes="180x180" href="https://sumra.net/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="https://sumra.net/favicons/favicon-32x32.png" sizes="32x32">
