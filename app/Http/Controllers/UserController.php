3662fe8839171769e1960d50c015dfebd8373240
anil <anil@sumra.net>
2017-08-05 11:20:18 +0100

    auto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,11 +12,12 @@ namespace App\Http\Controllers;
 class UserController extends BaseController
 {
     public function create(){
-        $user = \App\User::find(1);
+      //  $user = \App\User::find(1);
 
 // Creating a token without scopes...
-        $token = $user->createToken('Token Name')->accessToken;
-        return $token;
+       // $token = $user->createToken('Token Name')->accessToken;
+        print_r($this->categories);
+        return "";
     }
 
 
