1c6bc6ed2e3a341333d9f543a063ad6697577346
Mauricio Mondragon <mauri@sumra.net>
2017-08-12 17:48:37 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
 		 	@foreach($base as $cat)
 		 		<div class="panel panel-primary {{$cat->class}}">
 		 			<div class="panel-heading">
-		 				<img class="icon" src="{{$cat}}.png">
+		 				<img class="icon" src="{{$cat->slug}}.png">
 		 				{{$cat->title}}
 		 			</div>
 		 			<div class="panel-body">
