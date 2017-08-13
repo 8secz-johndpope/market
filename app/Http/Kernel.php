6f15e142542e1581dc808cd799e9997c5cec928f
anil <anil@sumra.net>
2017-08-13 13:40:49 +0100

    auto

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,8 +55,8 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'auth:api' => \App\Http\Middleware\AppMiddleware::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
