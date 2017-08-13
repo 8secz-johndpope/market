debbeeb0e668231e176688fec2feecdf1dd5e33e
anil <anil@sumra.net>
2017-08-13 13:40:10 +0100

    auto

---
+++ app/Http/Middleware/ApiMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sumra
+ * Date: 13/08/2017
+ * Time: 13:36
+ */
+
+namespace App\Http\Middleware;
+use Closure;
+
+use Illuminate\Support\Facades\Auth;
+
+class ApiMiddleware
+{
+
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!Auth::guard($guard)->check()) {
+            return ['msg'=>'Invalid credentials'];
+        }
+
+        return $next($request);
+    }
+}
