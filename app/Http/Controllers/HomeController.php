03ce71358885f4979390f18895552c1503749b19
Mauricio Mondragon <mauri@sumra.net>
2017-08-13 16:40:21 +0100

    auto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,7 +39,7 @@ class HomeController extends BaseController
             $cat->class = "category-$j";
             if($cat->children()->count() > self::MAX_CHILDREN)
                 $cat->hasMore = true;
-            $cat->children= $cat->children()->limit(MAX_CHILDREN + 1)->get();
+            $cat->children= $cat->children()->limit(self::MAX_CHILDREN)->get();
             $j++;
         }
         return $base;
