aed7d30ebb3f20285ca900a2e530fab19e15ba5c
Mauricio Mondragon <mauri@sumra.net>
2017-08-13 15:05:05 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,10 +43,10 @@
 				<div class="col-lg-3">
 				</div>
     			<div class="col-lg-6">
-    				<form class="navbar-form navbar-left">
-        				<div class="form-group">
+    				<form class="navbar-form">
+        				<div class="form-group col-lg-12">
 								<input type="text" class="form-control .input-lg" placeholder="Search for...">
-								<div class="input-group col-lg-2">
+								<div class="input-group col-lg-4 input-group-lg">
 							      	<input type="text" class="form-control" placeholder="Post Code" >
 							      	<span class="input-group-btn">
 							     		<button class="btn btn-default" type="button">Go!</button>
