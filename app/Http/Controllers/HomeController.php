45cdaa486f87feb46ec45d58196a772aaf134369
Mauricio Mondragon <mauri@sumra.net>
2017-08-13 15:59:35 +0100

    auto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends BaseController
     }
     
     public function baseAndFirstChildren(){
-        $base = Category::where('parent_id',0)->limit(1)->all();
+        $base = Category::where('parent_id',0)->limit(1)->get();
         var_dump($base);
         $children = array();
         $j = 0;
