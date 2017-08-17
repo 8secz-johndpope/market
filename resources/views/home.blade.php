64b64b464d4d3605d0dc36835acd8e197b67a897
Mauricio Mondragon <mauri@sumra.net>
2017-08-17 20:30:02 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,8 +36,7 @@
 					        <span class="icon-bar"></span>
 				     	</button>
 				      <a class="navbar-brand" href="#"><img class="icon" src="css/ic_launcher1.png"></a>
-					</div>
-					<div>
+				      <div>
 						<div>
 							<img src="css/googleplayx233.png">
 						</div>
@@ -47,6 +46,7 @@
 						<div>
 							<img src="css/windowsx233.png">
 						</div>
+					  </div>
 					</div>
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav navbar-right">
