b38b5e3680b401aca83f446bd2f800958f12d6b0
Mauricio Mondragon <mauri@sumra.net>
2017-08-17 19:21:48 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
 					        <span class="icon-bar"></span>
 					        <span class="icon-bar"></span>
 				     	</button>
-				      <a class="navbar-brand" href="#"><img class="icon" src="css/logo2.png"></a>
+				      <a class="navbar-brand" href="#"><img class="icon" src="css/ic_launcher1.png"></a>
 					</div>
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav navbar-right">
