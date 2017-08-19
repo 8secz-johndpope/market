9a36bcb33dcb0bbc9fead61fb5d645f8965f2488
Mauricio Mondragon <mauri@sumra.net>
2017-08-19 15:08:02 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -125,7 +125,7 @@
 							 			<div class="panel-body">
 							 				<div class="text">
 							 					<h3 class="">{{$spl['title']}}</h3>
-							 				<div>
+							 				</div>
 										 	@if($spl['meta']['price']>=0)
 		                                            <div class="items-box-price font-5">£	{{$spl['meta']['price']/100}}{{isset($spl['meta']['price_frequency']) ? $spl['meta']['price_frequency']:''}}
 		                                            </div>
@@ -142,7 +142,9 @@
 							 			<div class="panel-heading" style="background-image:url(https://s3.eu-central-1.amazonaws.com/web.eu-central-1.sumra.net/{{ count($spl['images'])>0?$spl['images'][0]:"1000006.jpg"}});"">
 							 			</div>
 							 			<div class="panel-body">
-							 				<h3 class="">{{$spl['title']}}</h3>
+							 				<div class="text">
+							 					<h3 class="">{{$spl['title']}}</h3>
+							 				</div>
 										 	@if($spl['meta']['price']>=0)
 		                                            <div class="items-box-price font-5">£	{{$spl['meta']['price']/100}}{{isset($spl['meta']['price_frequency']) ? $spl['meta']['price_frequency']:''}}
 		                                            </div>
