f66a7d93139ba4464a0f475d2811cba7e1f441eb
anil <anil@sumra.net>
2017-08-19 17:34:55 +0100

    auto

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -9,7 +9,6 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Model\Category;
 
 class Category extends  Model
 {
@@ -22,43 +21,7 @@ class Category extends  Model
      * [getBase description]
      * @return [type] [description]
      */
-    public static function getBase(){
-        return self::where('parent_id',0)->get();
-    }
-    /**
-     * [firstChildren description]
-     * @param  int    $max
-     * @return [type]      [description]
-     */
-    public function firstChildren(int $max){
-        return $this->children()->limit($max)->get();
-    }
 
-    /**
-     * This function return all descendants of a category
-     * @return [Colletion] with the children and its descendants
-     */
-    public function getAllDescendants(){
-        if($this->children()->count() == 0){
-            return null;
-        }
-        else{
-            $categories = Categories::getInstance()->getCategories();
-            foreach($categories as $cat){
-                if($cat->parent_id == $this->id){
-                    if(isset($this->children))
-                        $this->children = array();
-                    array_push($this->children, $cat);
-                }
-            }
-            $descendants = $this->children()->get();
-            //$categories = Categories::getInstance()->getCategories();
-            foreach ($descendants as $child) {
-                $child->children = $child->getAllDescendants();
-            }
-        }
-        return $descendants;
-    }
     public function parent(){
         return $this->belongsTo('App\Model\Category');
     }
