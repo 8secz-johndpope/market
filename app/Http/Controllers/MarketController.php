07ede15d8ac4b99f89587291b8aa77f9591d5143
anil <anil@sumra.net>
2017-08-20 17:23:33 +0100

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -718,6 +718,7 @@ class MarketController extends BaseController
                             $filter->count = $bucket['doc_count'];
                             if(isset($input[$key])&&$input[$key]===$filter->key){
                                 $filter->selected = 1;
+                                $filter->url = $request->url();
                             }else{
                                 $filter->selected = 0;
                             }
@@ -728,6 +729,7 @@ class MarketController extends BaseController
                     } else {
                         if(isset($input[$key])&&$input[$key]===$field_val->slug){
                             $field_val->selected = 1;
+                            $field_val->url = $request->url();
                         }else{
                             $field_val->selected = 0;
                         }
