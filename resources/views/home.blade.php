332fcbcfb248855ff38ce1cf810aa9cfd6bc2091
Mauricio Mondragon <mauri@sumra.net>
2017-08-20 14:57:03 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,7 @@
 					        <span class="icon-bar"></span>
 				     	</button>
 				      <a class="navbar-brand" href="#"><img class="icon" src="css/ic_launcher1.png"></a>
-				      <div class="col-2 col-lg-5col-md-offset-2 pull-right hidden-xs hidden-sm">
+				      <div class="col-2 pull-right hidden-xs hidden-sm">
 						<div class="center-block">
 							<img class="img-responsive" src="css/googleplayx233.png">
 						</div>
