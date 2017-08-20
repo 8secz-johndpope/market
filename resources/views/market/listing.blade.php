8770a8b479b1cf4bd9ead4389b6f038e364d3b09
anil <anil@sumra.net>
2017-08-20 17:31:09 +0100

    auto

--- resources/views/market/listing.blade.php
+++ resources/views/market/listing.blade.php
@@ -382,7 +382,7 @@
                     @if($val->selected===1)
                             <li>{{$val->title}}</li>
                         @else
-                    <li><a href="{{$val->url}}">{{$val->title}}</a>&nbsp;&nbsp;{{$val->count}}</li>
+                    <li><a href="https://www.google.com">{{$val->title}}</a>&nbsp;&nbsp;{{$val->count}}</li>
                         @endif
                     @endforeach
                 </ul>
