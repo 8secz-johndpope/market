07cf21225a31ae49af3c556e4fd5804f08f3d310
anil <anil@sumra.net>
2017-08-20 17:41:42 +0100

    auto

--- resources/views/market/listing.blade.php
+++ resources/views/market/listing.blade.php
@@ -382,7 +382,7 @@
                     @if($val->selected===1)
                             <li>{{$val->title}}</li>
                         @else
-                    <li><a href="{{ url('/test') }}">{{$val->title}}</a>&nbsp;&nbsp;{{$val->count}}</li>
+                    <li><a href="{{ url('test') }}">{{$val->title}}</a>&nbsp;&nbsp;{{$val->count}}</li>
                         @endif
                     @endforeach
                 </ul>
