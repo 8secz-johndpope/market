7d4f3d90ef06fdf8f7a059ea8183853107e556e0
Mauricio Mondragon <mauri@sumra.net>
2017-08-21 15:27:19 +0100

    auto

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {ellipsis('.text');});
+
+function ellipsis(selector){
+ var nodeList = document.querySelectorAll(selector);
+ arrNodes = [].slice.call(nodeList);
+ for (var i in arrNodes)
+ {
+   var n = arrNodes[i]; 
+   while(n.scrollHeight-n.offsetHeight>0)
+   {
+     var text = (n.innerText != undefined) ? n.innerText : n.textContent;
+     if(n.innerText != undefined)
+     {
+         n.innerText=text.replace(/\W*\s(\S)*$/, '...');
+     }
+     else
+     {
+       // Para Firefox
+       n.textContent = text.replace(/\W*\s(\S)*$/, '...');
+     }
+   }
+ }
+}
