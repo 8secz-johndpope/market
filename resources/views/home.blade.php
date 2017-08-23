c156051ace23f11137ad02f36e1ef58dfd0d76ad
Mauricio Mondragon <mauri@sumra.net>
2017-08-23 18:59:37 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -287,7 +287,7 @@
 					</div>
 					<div class="col-xs-8 col-sm-6 col-md-6 col-lg-4 col-xs-offset-2 col-sm-offset-0">
 						<figure>
-							<img src="css/free-uk.svg">
+							<img src="css/icons/free-uk.svg">
 						</figure>
 						<h2>FREE LISTINGS</h2>
 						<p>Our buyer guarantee means you'll get refunded if something goes wrong</p>
