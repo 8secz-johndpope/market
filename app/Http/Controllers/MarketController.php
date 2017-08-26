2b2ff3ae2f8f31e108550f8ed0319163d94c649e
anil <anil@sumra.net>
2017-08-26 12:26:20 +0100

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -92,6 +92,7 @@ class MarketController extends BaseController
 */
 
             $adverts = Advert::paginate(10);
+            return $adverts;
             foreach ($adverts['data'] as $advert) {
                 $params = [
                     'index' => 'adverts',
