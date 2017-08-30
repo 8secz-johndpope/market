22353849f3202c8c6c67b45913122bd7d79ce2dc
anil <anil@sumra.net>
2017-08-30 20:30:07 +0100

    auto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,11 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
+    <style>
+        img.lazyload{
+            width: 100%;
+        }
+    </style>
 
 </head>
 <body class="">
