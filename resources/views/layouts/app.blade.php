dc0995956def5d1afc50613c41359b734502237d
anil <anil@sumra.net>
2017-08-30 21:02:18 +0100

    auto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
             width: 100%;
         }
         body {
-            font: 1em/1.67 'Open Sans', Arial, Sans-serif;
+           /* font: 1em/1.67 'Open Sans', Arial, Sans-serif; */
             margin: 0;
             background: #e9e9e9;
         }
