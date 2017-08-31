3fbf6b15f85774305618354c523b77d3fab633cf
anil <anil@sumra.net>
2017-08-31 19:01:36 +0100

    auto

---
+++ app/Model/Cv.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sumra
+ * Date: 31/08/2017
+ * Time: 18:47
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Cv extends Model
+{
+
+}
