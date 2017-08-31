8140d11ac4e64e1a425b8c53bb34d143b5ea70c7
anil <anil@sumra.net>
2017-08-31 18:23:22 +0100

    auto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,6 +42,7 @@ class UserController extends BaseController
     {
         // Get the currently authenticated user...
         $user = Auth::user();
+        return $user;
         $id = $request->id;
         $advert = Advert::find($id);
         if($advert===null){
