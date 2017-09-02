7a520b2a3ae6c9bb065d5fd566ec3bfbb19e1265
anil <anil@sumra.net>
2017-09-02 14:28:43 +0100

    auto

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -52,15 +52,24 @@
            </div>
            <div class="selected-category">
                <div class="row">
+                   <div class="col-sm-12">
                    <div class="grayborder">
-                   <div class="col-sm-12"> <span class="glyphicon glyphicon-ok-sign"></span><span class="category-title">Category</span></div>
-                   </div>
-                   <div class="grayborder">
-                   <div class="col-sm-11"></div>
-                   <div class="col-sm-1">
-                       <a class="btn btn-default">Edit</a>
-                   </div>
+                       <div class="row">
+                           <div class="col-sm-12">
+                   <span class="glyphicon glyphicon-ok-sign"></span><span class="category-title">Category</span>
+                           </div>
                        </div>
+                           </div>
+                   </div>
+                   <div class="col-sm-12"> <div class="grayborder">
+                           <div class="row">
+                           <div class="col-sm-11"></div>
+                           <div class="col-sm-1">
+                               <a class="btn btn-default">Edit</a>
+                           </div>
+                           </div>
+                       </div></div>
+
                </div>
            </div>
        </div>
