d06a1cb0d10f4b6ce856ec75ecde2ae1d2e273cd
anil <anil@sumra.net>
2017-09-02 18:59:59 +0100

    auto

--- resources/views/home/suggest.blade.php
+++ resources/views/home/suggest.blade.php
@@ -1,7 +1,8 @@
 @foreach($categories as $category)
     <li class="list-group-item">
-       <span class="suggest-title">{{$category->title}}</span>
-        <p>{{$category->parentstring}}</p>
         <a class="select-category-link">Select</a>
+
+        <span class="suggest-title">{{$category->title}}</span>
+        <p>{{$category->parentstring}}</p>
     </li>
 @endforeach
