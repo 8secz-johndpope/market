cf570368698bc1fe19ba5648756fded8a133ee71
anil <anil@sumra.net>
2017-09-02 17:34:36 +0100

    auto

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -153,13 +153,32 @@
 
             </div>
         </section>
-        <nav aria-label="...">
+        <nav aria-label="Page navigation">
             <ul class="pagination">
-                <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-                <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+                <li>
+                    <a href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+               
+
+                @foreach($pages as $p)
+                    <li class="{{$page==$p?'active':''}}">
+                        @if($page===$p)
+                            <span> {{$p}}<span class="sr-only">(current)</span></span>
+                        @else
+                            <a href="{{$pageurl}}&page={{$p}}">{{$p}}</a>
+                        @endif
+                    </li>
+                @endforeach
+
+                <li>
+                    <a href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
             </ul>
         </nav>
-
         <ul class="pager">
             @if($page!=1)
                 <li class="pager-prev visible-pc">
