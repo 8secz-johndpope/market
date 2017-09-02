883292ba191d75c2aadf52441c55d49a304ec957
anil <anil@sumra.net>
2017-09-02 16:44:21 +0100

    auto

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -180,9 +180,17 @@
                </div>
            </div>
 
-           <div class="row">
-               <div class="col-sm-2 col-sm-offset-10"><a class="btn btn-primary">Post Advert</a> </div>
+
+           <div class="panel panel-primary">
+               <div class="panel-heading">
+                   <h3 class="panel-title">Total</h3>
+               </div>
+               <div class="panel-body">
+                   <div class="row">
+                       <div class="col-sm-2 col-sm-offset-10"><a class="btn btn-primary">Post Advert</a> </div>
+                   </div>               </div>
            </div>
+
        </div>
 
        <div class="col-lg-2"></div>
