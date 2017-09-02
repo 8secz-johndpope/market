a3f672e5f5d84b3de10c35bd730f20ac3ae0632e
anil <anil@sumra.net>
2017-09-02 17:30:42 +0100

    auto

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -153,6 +153,13 @@
 
             </div>
         </section>
+        <nav aria-label="...">
+            <ul class="pagination">
+                <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+                <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+            </ul>
+        </nav>
+
         <ul class="pager">
             @if($page!=1)
                 <li class="pager-prev visible-pc">
