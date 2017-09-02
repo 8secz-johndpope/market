d73d6c8aa8f4c78a7b832a526514fffb72e6f629
anil <anil@sumra.net>
2017-09-02 14:34:51 +0100

    auto

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -87,7 +87,22 @@
                    <div class="col-sm-12">
                        <div class="grayborder height100">
                            <div class="row">
-                               <div class="col-sm-11"></div>
+                               <div class="col-sm-11">
+                                   <form class="form-horizontal">
+                                       <div class="form-group">
+                                           <label class="control-label col-sm-2" for="postcode">Postcode:</label>
+                                           <div class="col-sm-10">
+                                               <input type="text" class="form-control" id="postcode" placeholder="SW115BG">
+                                           </div>
+                                       </div>
+
+                                       <div class="form-group">
+                                           <div class="col-sm-offset-2 col-sm-10">
+                                               <button class="btn btn-danger">Go</button>
+                                           </div>
+                                       </div>
+                                   </form>
+                               </div>
                                <div class="col-sm-1">
                                    <a class="btn btn-default">Edit</a>
                                </div>
