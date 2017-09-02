d2468bc5af9db4244359e32881e9ed4f7263c447
anil <anil@sumra.net>
2017-09-02 18:58:59 +0100

    auto

--- resources/views/home/suggest.blade.php
+++ resources/views/home/suggest.blade.php
@@ -2,6 +2,6 @@
     <li class="list-group-item">
        <span class="suggest-title">{{$category->title}}</span>
         <p>{{$category->parentstring}}</p>
-        <a>Select</a>
+        <a class="select-category-link">Select</a>
     </li>
 @endforeach
