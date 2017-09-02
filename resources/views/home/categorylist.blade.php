dd000dbc8539224bb9791dae0f1ceaa38a4ac36a
anil <anil@sumra.net>
2017-09-02 13:10:26 +0100

    auto

--- resources/views/home/categorylist.blade.php
+++ resources/views/home/categorylist.blade.php
@@ -1,3 +1,3 @@
 @foreach($categories as $category)
-    <li class="list-group-item" data-category="{{$category->id}}">{{$category->title}}</li>
+    <li class="list-group-item" data-category="{{$category->id}}">{{$category->title}} @if(count($category->children)>0)<span class="glyphicon glyphicon-arrow-right"></span>@endif</li>
     @endforeach
