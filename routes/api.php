a41588a5bde0b38e8de39d0424545c5074c2cb76
anil <anil@sumra.net>
2017-09-03 15:33:28 +0100

    auto

--- routes/api.php
+++ routes/api.php
@@ -64,6 +64,8 @@ Route::post('/user/advert/order','UserController@order')->middleware('auth:api')
 Route::post('/user/advert/offer','UserController@offer')->middleware('auth:api');
 Route::post('/user/advert/interest','UserController@interest')->middleware('auth:api');
 Route::post('/user/advert/favorite','UserController@favorite')->middleware('auth:api');
+Route::post('/user/list/favorite','UserController@favorite')->middleware('auth:api');
+
 Route::post('/user/advert/report','UserController@report')->middleware('auth:api');
 Route::post('/user/order/review','UserController@review')->middleware('auth:api');
 
