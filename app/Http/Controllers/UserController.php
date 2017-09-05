a0e3088c74c2117c52a3ab910898b2e9b151f742
luis <luis@sumra.net>
2017-09-05 20:51:25 +0100

    auto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -262,7 +262,7 @@ class UserController extends BaseController
                 'size'=>1,
                 'query' => [
                     'bool' => [
-                        'should'=>['term'=>['source_id'=>$a1[0]]]
+                        'filter'=>['term'=>['source_id'=>$a1[0]]]
                     ]
                 ]
             ]
