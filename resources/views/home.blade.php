1c0aa87670d4392115a7912e9871db08b55f6568
Mauricio Mondragon <mauri@sumra.net>
2017-09-06 19:04:24 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -174,7 +174,7 @@
 				</div>
 				<div class="wave-container" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);"><canvas class="webgl-object active" width="932" height="210" style="width: 932px; height: 210px;"></canvas></div>
 				<div class="wave-copy">
-					<h2 class="typography-subsection-subheadline siri-command relative" style="opacity: 1;">We're more than a marketplace.</h2>
+					<p class="typography-subsection-subheadline siri-command relative" style="opacity: 1;">We're more than a marketplace.</p>
 				</div>
 			</div>	
 		</section>
