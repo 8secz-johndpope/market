46242d82851df3df29eadd1e6f3be584600cacc0
anil <anil@sumra.net>
2017-09-06 19:02:18 +0100

    auto

--- app/User.php
+++ app/User.php
@@ -93,6 +93,6 @@ class User extends Authenticatable
     }
     public function favs()
     {
-        return $this->belongsToMany('App\Model\Advert','favorite');
+        return $this->belongsToMany('App\Model\Advert','favorites');
     }
 }
