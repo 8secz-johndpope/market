7150a13603c8f81a8fde98bf3a9f7da9c28330a3
anil <anil@sumra.net>
2017-09-06 16:52:21 +0100

    auto

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use App\Model\EmailCode;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Mail;
 
 
 class User extends Authenticatable
