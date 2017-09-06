43246b6cb8bc8ba63fbb68055494a2fa01ceb721
anil <anil@sumra.net>
2017-09-06 14:27:29 +0100

    auto

--- app/Model/Favorite.php
+++ app/Model/Favorite.php
@@ -13,6 +13,6 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     public function advert(){
-        return $this->hasOne('App\Model\Advert','advert_id');
+        return $this->belongsTo('App\Model\Advert');
     }
 }
