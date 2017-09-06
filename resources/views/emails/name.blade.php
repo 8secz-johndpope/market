d44fea7b9b2a2ac047ee3d28e514ad43296e02d2
anil <anil@sumra.net>
2017-09-06 16:12:00 +0100

    auto

--- resources/views/emails/name.blade.php
+++ resources/views/emails/name.blade.php
@@ -1,12 +1,6 @@
-@component('mail::message')
-    # Order Shipped
-
-    Your order has been shipped!
-
-    @component('mail::button', ['url' => $url])
-        View Order
-    @endcomponent
-
-    Thanks,<br>
-    {{ config('app.name') }}
+@component('mail::message')# Order Shipped Your order has been shipped!
+@component('mail::button', ['url' => $url])View Order
+@endcomponent
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
