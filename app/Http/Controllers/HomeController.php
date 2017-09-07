57210e51e60e82ac76854184975df7bab90a04b8
anil <anil@sumra.net>
2017-09-07 17:23:48 +0100

    auto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -169,6 +169,7 @@ class HomeController extends BaseController
         $advert->user_id=$user->id;
         $advert->save();
         $total = (int)$request->total;
+        return $request->total;
         if($total>0){
             $orders = array();
             if($request->has('featured')){
