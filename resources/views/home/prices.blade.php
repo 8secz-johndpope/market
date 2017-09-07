3fd18f1fa32f1d31d1bcfd41cbccffe75c40a2e6
anil <anil@sumra.net>
2017-09-07 15:03:40 +0100

    auto

--- resources/views/home/prices.blade.php
+++ resources/views/home/prices.blade.php
@@ -11,7 +11,7 @@
                     <div class="col-sm-10">
                         <div class="form-check">
                             <label class="form-check-label">
-                                <input class="form-check-input extra-check" type="checkbox" name="urgent" value="1">
+                                <input class="form-check-input extra-check" type="checkbox" name="{{$extra->slug}}" value="1">
                                 <span class="span-{{$extra->slug}}">{{$extra->title}}</span> &nbsp;&nbsp;{{$extra->subtitle}}
                             </label>
                         </div>
@@ -20,7 +20,7 @@
                         @if($extra->type==='single')
                         <span class="extra-price">£{{$extra->price->price/100}}</span>
                         @else
-                            <select class="form-control" name="featured-days">
+                            <select class="form-control" name="{{$extra-slug}}-quantity">
                                 @foreach($extra->prices as $price)
                                 <option value="7">{{$price->title}}  (£{{$price->price/100}})</option>
                                     @endforeach
@@ -32,68 +32,7 @@
 
             </li>
             @endforeach
-                    <li class="list-group-item">
-                        <div class="row">
-                            <div class="col-sm-10">
-                                <div class="form-check">
-                                    <label class="form-check-label">
-                                        <input class="form-check-input extra-check" type="checkbox" name="featured" value="1">
-                                        <span class="span-featured">Featured</span>&nbsp;&nbsp;Have your Ad appear at the top of the category listings for 3, 7 or 14 days.
-                                    </label>
-
-                                </div>
-                            </div>
-                            <div class="col-sm-2">
-
-                            </div>
-                        </div>
-                    </li>
-
-
-            <li class="list-group-item">
-
-                <div class="row">
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input class="form-check-input extra-check" type="checkbox" name="spotlight" value="1">
-                                <span class="span-spotlight">Spotlight</span> &nbsp;&nbsp;Have your Ad seen on the Sumra homepage!
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-sm-2">
-                        <span class="extra-price">£60</span>
-                    </div>
-                </div>
-
-
-
-
-
-
-            </li>
-
-            <li class="list-group-item">
-                <div class="row">
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input class="form-check-input extra-check" type="checkbox" name="shipping" value="1">
-                                <span class="span-shipping">CanShip</span> &nbsp;&nbsp;Ship to the buyer when order is placed.
-                            </label>
-
-                        </div>
-                    </div>
-                    <div class="col-sm-2">
-                        <select class="form-control" name="shipping-weight">
-                            <option value="2">Up to 2kg (£4)</option>
-                            <option value="5">Up to 5kg (£7)</option>
-                            <option value="10">Up to 10kg (£10)</option>
-                        </select>
-                    </div>
-                </div>
-
-            </li>
+                   
         </ul>
     </div>
 </div>
