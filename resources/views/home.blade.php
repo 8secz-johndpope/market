2ae8948b85e2d0e282cf9d4d16ed938ede0b49ac
Mauricio Mondragon <mauri@sumra.net>
2017-09-07 18:57:00 +0100

    auto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -171,7 +171,7 @@
 					<a href=""><h2 class="typography-subsection-subheadline siri-command relative" style="opacity: 1;">What is Sumra?</h2></a>
 				</div>
 				<div class="wave-container" style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);">
-					<canvas class="webgl-object active" width="932" height="210" style="width: 932px; height: 210px;"></canvas>
+					<canvas class="webgl-object active"></canvas>
 				</div>
 				<div class="wave-copy">
 					<p class="typography-subsection-subheadline siri-command relative" style="opacity: 1;">We're more than a marketplace.</p>
