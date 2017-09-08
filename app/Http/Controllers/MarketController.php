ddd363e56e198d1c5adafce0cde3f2c395381997
anil <anil@sumra.net>
2017-09-08 13:34:11 +0100

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -248,7 +248,7 @@ class MarketController extends BaseController
                     'size' => 10000,
                     'query' => [
                         'term' => [
-                            "category" => $id
+                            "featured" => 1
                         ]
                     ]
                 ]
@@ -266,7 +266,7 @@ class MarketController extends BaseController
                     'id' => $product['id'],
                     'body' => [
                         'doc' => [
-                            'category' => $replace
+                            'featured_acount' => 0
                         ]
                     ]
                 ];
