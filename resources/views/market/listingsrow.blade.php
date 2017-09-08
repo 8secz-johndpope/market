352fd1c459e9717d00f2022ea1ab6b654b12abf7
anil <anil@sumra.net>
2017-09-08 13:53:53 +0100

    auto

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -122,9 +122,12 @@
                         <div class="listing-img">
                             <div class="main-img">
                                 <img src="https://s3.eu-central-1.amazonaws.com/web.eu-central-1.sumra.net/{{ count($product['images'])>0?$product['images'][0]:"noimage.png"}}" class="lazyload" alt="">
+
+                              @if(isset($product['featured'])&&$product['featured']===1)
                                 <span class="ribbon-featured">
 <strong class="ribbon" data-q="featuredProduct"><span class="hide-visually">This ad is</span>Featured</strong>
 </span>
+                                @endif
                                 <div class="listing-meta">
                                 </div>
                             </div>
