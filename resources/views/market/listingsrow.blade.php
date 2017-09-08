dae3262bd27325b7b425e038a42edc45a44ff51a
anil <anil@sumra.net>
2017-09-08 19:40:57 +0100

    auto

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -175,8 +175,10 @@
 
                         </div>
                          <div class="row">
-                             <div class="col-sm-11 col-md-10"></div>
-                             <div class="col-sm-1 col-md-2"> <span class="posted-text">{{$product['posted']}}</span> </div>
+                             <div class="col-sm-10 col-md-11 "></div>
+                             <div class="col-sm-2 col-md-1">
+                                 <span class="posted-text">{{$product['posted']}}</span>
+                             </div>
                          </div>
                     </a>
 
