c51b1ef1dd4e7b8db9318810ce0c7cb38087f89f
anil <anil@sumra.net>
2017-09-09 14:15:15 +0100

    auto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -155,7 +155,8 @@ class HomeController extends BaseController
         }else{
             $body['images']=[];
         }
-        $body['meta']['price']=(int)(((float)$request->price)*100);
+            $body['meta']['price'] =100000;
+       // $body['meta']['price']=(int)(((float)$request->price)*100);
         foreach ($fields as $field){
             if($request->has($field->slug)){
                 $body['meta'][$field->slug] = $request->get($field->slug);
