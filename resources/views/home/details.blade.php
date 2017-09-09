8b27899b44b4d2b7e21decd5ad2c77cc05872bd0
anil <anil@sumra.net>
2017-09-09 18:05:12 +0100

    auto

--- resources/views/home/details.blade.php
+++ resources/views/home/details.blade.php
@@ -52,7 +52,7 @@
 
             @endforeach
             <h4>CVs</h4>
-            <table>
+            <table class="table">
                 <thead><th>Title</th><th>Category</th></thead>
             @foreach($user->cvs as $cv)
             <tr><td>{{$cv->title}}</td><td>{{$cv->category->title}}</td></tr>
