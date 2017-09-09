e605022a661c3df9493ebde905bc32fd73693639
anil <anil@sumra.net>
2017-09-09 17:23:08 +0100

    auto

--- resources/views/home/orders.blade.php
+++ resources/views/home/orders.blade.php
@@ -74,13 +74,13 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
+                    <h5 class="modal-title">Tracking</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>Modal body text goes here.</p>
+                    <input class="form-control" id="tracking_id" type="text" placeholder="Tracking Info">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary">Save changes</button>
