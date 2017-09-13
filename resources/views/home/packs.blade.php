cc8c3e1db3e3c8eefdf8c344e770aaa48d76faf0
anil <anil@sumra.net>
2017-09-13 19:52:02 +0100

    auto

--- resources/views/home/packs.blade.php
+++ resources/views/home/packs.blade.php
@@ -35,6 +35,7 @@
 </tr>
                 @endforeach
                 </table>
+                <a class="btn-primary btn" href="/user/manage/pricegroup">Add Price Group</a>
             </div>
         </div>
 
