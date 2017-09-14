1331148a386e85edb32cc074c29f9c898fbb6355
anil <anil@sumra.net>
2017-09-14 19:48:58 +0100

    auto

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CheckAdmin
 {
@@ -15,6 +16,7 @@ class CheckAdmin
      */
     public function handle($request, Closure $next, $guard = null)
     {
+       
         if (Auth::guard($guard)->check()) {
 
             return redirect('/home');
