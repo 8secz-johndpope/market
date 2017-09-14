00a49cea060312a0c60146746bd456f301c665ec
anil <anil@sumra.net>
2017-09-14 17:57:11 +0100

    auto

--- resources/views/home/locationlist.blade.php
+++ resources/views/home/locationlist.blade.php
@@ -1,3 +1,3 @@
 @foreach($categories as $category)
-    <li class="list-group-item" data-category="{{$category->id}}" data-children="{{count($category->children)}}"> {{$category->title}} @if(count($category->children)>0)<span class="glyphicon glyphicon-arrow-right floatright"></span>@endif <span class="glyphicon floatright select-arrow"></span> </li>
+    <li class="list-group-item" data-location="{{$category->id}}" data-children="{{count($category->children)}}"> {{$category->title}} @if(count($category->children)>0)<span class="glyphicon glyphicon-arrow-right floatright"></span>@endif <span class="glyphicon floatright select-arrow"></span> </li>
 @endforeach
