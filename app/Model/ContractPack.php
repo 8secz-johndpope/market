ae902c0a6850ca5deca601e54895dbcedfae0a38
anil <anil@sumra.net>
2017-09-15 13:31:40 +0100

    auto

--- app/Model/ContractPack.php
+++ app/Model/ContractPack.php
@@ -12,5 +12,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContractPack extends Model
 {
-
+    public function category(){
+        return $this->belongsTo('App\Model\Category');
+    }
+    public function location(){
+        return $this->belongsTo('App\Model\Location');
+    }
 }
--- resources/views/home/packs.blade.php
+++ resources/views/home/packs.blade.php
@@ -1,7 +1,7 @@
 <table class="table">
-    <thead><th>Title</th><th>Quantity</th><th>Price</th></thead>
+    <thead><th>Title</th><th>Category</th><th>Location</th><th>Quantity</th><th>Price</th></thead>
     @foreach($contract->packs as $pack)
-        <tr><td>{{$pack->title}}</td><td>{{$contract->count}}</td><td>{{$pack->amount/100}}</td></tr>
+        <tr><td>{{$pack->title}}</td><td>{{$pack->category->title}}</td><td>{{$pack->location->title}}</td><td>{{$contract->count}}</td><td>{{$pack->amount/100}}</td></tr>
         @endforeach
     <tr><td>Total</td><td>{{count($contract->packs)*$contract->count}}</td><td>{{$contract->packs->sum('amount')/100}}</td></tr>
 </table>
