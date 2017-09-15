0749c19290492aa72b0630c0bc12ad980495b685
anil <anil@sumra.net>
2017-09-15 14:41:56 +0100

    auto

--- resources/views/home/sign.blade.php
+++ resources/views/home/sign.blade.php
@@ -13,11 +13,11 @@
 @section('content')
 <div class="row">
     <div class="col-sm-2">
-        
+
     </div>
     <div class="col-sm-8">
         <h4>Please read the contract carefully and sign below</h4>
-        <embed src="https://sumra.net/css/contract.pdf" width="800" height="600" type='application/pdf'>
+        <embed src="https://sumra.net/css/contract.pdf" width="800" height="600" type='application/pdf' class="doc">
             <br>
             <input type="checkbox" name="agree">I agree to <a>Terms and Conditions</a>
        <br> Name: <input type="text" class="form-control" placeholder="Full Name">
