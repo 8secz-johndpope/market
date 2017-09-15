b2bfa24a6e232a3bd94f7211a63671ba7422f3e8
anil <anil@sumra.net>
2017-09-15 13:34:00 +0100

    auto

--- resources/views/home/packs.blade.php
+++ resources/views/home/packs.blade.php
@@ -1,7 +1,7 @@
 <table class="table">
     <thead><th>Title</th><th>Category</th><th>Location</th><th>Quantity</th><th>Price</th></thead>
     @foreach($contract->packs as $pack)
-        <tr><td>{{$pack->title}}</td><td>{{$pack->category->title}}</td><td>{{$pack->location->title}}</td><td>{{$contract->count}}</td><td>{{$pack->amount/100}}</td></tr>
+        <tr><td>{{$pack->title}}</td><td>{{$pack->category->title}}</td><td>{{$pack->location->title}}</td><td>{{$contract->count}}</td><td>£{{$pack->amount/100}}</td></tr>
         @endforeach
-    <tr><td>Total</td><td></td><td></td><td>{{count($contract->packs)*$contract->count}}</td><td>{{$contract->packs->sum('amount')/100}}</td></tr>
+    <tr><td><span class="bold-text">Total</span></td><td></td><td></td><td>{{count($contract->packs)*$contract->count}}</td><td><span class="bold-text"> £{{$contract->packs->sum('amount')/100}}</span></td></tr>
 </table>
