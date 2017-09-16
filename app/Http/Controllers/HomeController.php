d3c5328c62bcfa8432efb6325e718426326fce65
anil <anil@sumra.net>
2017-09-16 16:42:25 +0100

    auto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -798,7 +798,7 @@ class HomeController extends BaseController
         $response = $client->createEmbeddedSignatureRequest($embedded_request);
         $client = new \HelloSign\Client('ecd17a4e5e1e6b1d60d17a12711665789956cc4874b608f06f5de462ba26bbc1');
         $response = $client->getEmbeddedSignUrl('559aa46cf6b9ab8bc4599862ee1f5b01');
-        return ['a'=>'b','response'=>$response];
+        return view('pdf.contract');
     }
 
 }
--- resources/views/pdf/contract.blade.php
+++ resources/views/pdf/contract.blade.php
@@ -5,7 +5,7 @@
 <script>
     HelloSign.init("d88c4209bd93093d3815ef0e26069793");
     HelloSign.open({
-        url: "https://app.hellosign.com/editor/embeddedSign?signature_id=2712d7ecccc79d07d604d9b6389f162e&token=1910bcd2dd0cd125973224c1d1eea62b",
+        url: "https://app.hellosign.com/editor/embeddedSign?signature_id=559aa46cf6b9ab8bc4599862ee1f5b01&token=26c2c735bd5f318129b247cf1eb565ed",
         allowCancel: true,
         skipDomainVerification: true,
         messageListener: function(eventData) {
