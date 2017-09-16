56a54011e0bdff9b597bfbd937123139d19d9d47
anil <anil@sumra.net>
2017-09-16 17:24:48 +0100

    auto

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -52,9 +52,9 @@
             <tr><td>Billing Address</td></tr>
         </table>
         <table class="customers">
-            <tr><th>Title</th><th>Category</th><th>Location</th><th>Quantity</th><th>Price</th><th>Delete</th></tr>
+            <tr><th>Title</th><th>Category</th><th>Location</th><th>Quantity</th><th>Price</th></tr>
             @foreach($contract->packs as $pack)
-                <tr><td>{{$pack->title}}</td><td>{{$pack->category->title}}</td><td>{{$pack->location->title}}</td><td>{{$contract->count}}</td><td>£{{$pack->amount/100}}</td><td><a class="delete-pack btn btn-danger" data-id="{{$pack->id}}">Delete</a> </td></tr>
+                <tr><td>{{$pack->title}}</td><td>{{$pack->category->title}}</td><td>{{$pack->location->title}}</td><td>{{$contract->count}}</td><td>£{{$pack->amount/100}}</td></tr>
             @endforeach
             <tr><td><span class="bold-text">Total</span></td><td></td><td></td><td><span class="bold-text">{{count($contract->packs)*$contract->count}}</span></td><td><span class="bold-text"> £{{$contract->packs->sum('amount')/100}}</span></td></tr>
 
