ed2fafa9750e698734c6fca405db37be0a99e63f
anil <anil@sumra.net>
2017-09-16 19:28:00 +0100

    auto

--- resources/views/home/business.blade.php
+++ resources/views/home/business.blade.php
@@ -11,5 +11,43 @@
 @endsection
 
 @section('content')
+    <div class="form-group row">
+        <label for="example-text-input" class="col-2 col-form-label">Company Name</label>
+        <div class="col-10">
+            <input class="form-control" type="text" placeholder="Example Ltd" id="example-text-input">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-search-input" class="col-2 col-form-label">Address Line1</label>
+        <div class="col-10">
+            <input class="form-control" type="text" placeholder="35 Tinkle Street" id="example-search-input">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-email-input" class="col-2 col-form-label">City</label>
+        <div class="col-10">
+            <input class="form-control" type="text" placeholder="London" id="example-email-input">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-url-input" class="col-2 col-form-label">Postcode</label>
+        <div class="col-10">
+            <input class="form-control" type="text" placeholder="DH15AX" id="example-url-input">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-tel-input" class="col-2 col-form-label">Telephone</label>
+        <div class="col-10">
+            <input class="form-control" type="tel" placeholder="0225343433" id="example-tel-input">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="example-password-input" class="col-2 col-form-label">Company Registation No:(if applicable)</label>
+        <div class="col-10">
+            <input class="form-control" type="text" placeholder="01893293" id="example-password-input">
+        </div>
+    </div>
 
-    @endsection
+    <a  class="btn btn-primary go-to-packs">Continue</a>
+
+@endsection
