59e2b5d956fcd1a7886aad1067d6a34685ba0f8a
anil <anil@sumra.net>
2017-09-17 15:50:29 +0100

    auto

--- resources/views/home/packs.blade.php
+++ resources/views/home/packs.blade.php
@@ -1,3 +1,4 @@
+    <div class="col-sm-12" >
 <table class="table">
     <thead><th>Title</th><th>Category</th><th>Location</th><th>Quantity</th><th>Price</th><th>Delete</th></thead>
     @foreach($contract->packs as $pack)
@@ -5,3 +6,10 @@
         @endforeach
     <tr><td><span class="bold-text">Total</span></td><td></td><td></td><td><span class="bold-text">{{count($contract->packs)*$contract->count}}</span></td><td><span class="bold-text"> £{{$contract->packs->sum('amount')/100}}</span></td></tr>
 </table>
+    </div>
+    <div class="col-sm-11">
+
+    </div>
+    <div class="col-sm-1">
+        <a class="btn btn-primary" href="/user/contract/sign">Continue</a>
+    </div>
--- resources/views/home/start.blade.php
+++ resources/views/home/start.blade.php
@@ -20,24 +20,8 @@
                     <h3 class="panel-title">List of Packs</h3>
                 </div>
                 <div class="panel-body">
-                    <div class="row">
-                        <div class="col-sm-12" id="pack-list">
-                            <table class="table">
-                                <thead><th>Title</th><th>Category</th><th>Location</th><th>Quantity</th><th>Price</th><th>Delete</th></thead>
-                                @foreach($contract->packs as $pack)
-                                    <tr><td>{{$pack->title}}</td><td>{{$pack->category->title}}</td><td>{{$pack->location->title}}</td><td>{{$contract->count}}</td><td>£{{$pack->amount/100}}</td><td><a class="delete-pack btn btn-danger" data-id="{{$pack->id}}">Delete</a> </td></tr>
-                                @endforeach
-                                <tr><td><span class="bold-text">Total</span></td><td></td><td></td><td><span class="bold-text">{{count($contract->packs)*$contract->count}}</span></td><td><span class="bold-text"> £{{$contract->packs->sum('amount')/100}}</span></td></tr>
-                            </table>
-
-
-                        </div>
-                        <div class="col-sm-11">
-
-                        </div>
-                        <div class="col-sm-1">
-                            <a class="btn btn-primary" href="/user/contract/sign">Continue</a>
-                        </div>
+                    <div class="row" id="pack-list">
+                        @include('home.packs', array('contract' => $contract));
                     </div>
                 </div>
             </div>
