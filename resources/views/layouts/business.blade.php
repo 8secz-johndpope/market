cb9d14a4acb1b355df485e66e59a3dccb4792030
anil <anil@sumra.net>
2017-09-21 15:48:36 +0100

    auto

--- resources/views/layouts/business.blade.php
+++ resources/views/layouts/business.blade.php
@@ -16,7 +16,6 @@
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 
     <link href="{{ asset('/css/extra.css?q=43') }}" rel="stylesheet">
-    <link href="{{ asset('/css/style.css?q=43') }}" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
