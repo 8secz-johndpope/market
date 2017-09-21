b071a367189bd8906c51804f51d17fdf0a04415d
anil <anil@sumra.net>
2017-09-21 15:50:38 +0100

    auto

--- resources/views/business/ads.blade.php
+++ resources/views/business/ads.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
+        <div class="col-sm-10 col-sm-offset-1">
 
             <ul class="nav nav-tabs">
 
