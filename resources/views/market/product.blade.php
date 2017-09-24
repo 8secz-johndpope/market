c8a67c7d163fb49d1e11210a84141bad2b6a5429
Anil M <anil@sumra.net>
2017-09-24 15:43:48 +0100

    auto

--- resources/views/market/product.blade.php
+++ resources/views/market/product.blade.php
@@ -14,8 +14,8 @@
 
         <div class="row">
 
-        <div class="col-sm-3"></div>
-                <div class="col-sm-6">
+        <div class="col-lg-3  col-md-2"></div>
+                <div class="col-lg-6 col-md-8 col-sm-12">
                     <ol class="breadcrumb">
                         @foreach($parents as $parent)
                         <li class="breadcrumb-item"><a href="/{{$parent->slug}}">{{$parent->title}}</a></li>
