d1a5dc8248d1f417c1d0f3352edf63e98d13f196
Anil M <anil@sumra.net>
2017-09-24 18:49:11 +0100

    auto

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -201,12 +201,14 @@
                     <input type="hidden" id="max_lng" name="max_lng" value="99">
                 <div class="main-search form-group col-md-12 col-lg-12">
                     <div class="input-group col-sm-4 col-md-3 col-lg-3 col-xl-2 input-group-lg">
-                        <select name="search_category">
+                        <select name="search_category" class="form-control">
                             <option value="all">All</option>
                         </select>
                     </div>
-                    <input type="text" class="form-control input-lg" id="autocomplete" name="q" placeholder="SEARCH" value="@if(isset($input['q'])) {{$input['q']}} @endif">
                     <div class="input-group col-sm-4 col-md-3 col-lg-3 col-xl-2 input-group-lg">
+                    <input type="text" class="form-control input-lg" id="autocomplete" name="q" placeholder="SEARCH" value="@if(isset($input['q'])) {{$input['q']}} @endif">
+                    </div>
+                        <div class="input-group col-sm-4 col-md-3 col-lg-3 col-xl-2 input-group-lg">
                         <input type="text" id="pac-input" class="form-control" placeholder="POST CODE" name="location" value="@if(isset($input['location'])) {{$input['location']}} @endif" required>
                         <span class="input-group-btn">
 							     		<button class="btn btn-default" type="submit" id="submitform">Go</button>
