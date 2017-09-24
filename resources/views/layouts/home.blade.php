a69d0ddd0447c6524d6fcf91dc91ae4e4b057ec5
Anil M <anil@sumra.net>
2017-09-24 18:55:37 +0100

    auto

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -204,8 +204,7 @@
                         <select name="search_category" class="form-control">
                             <option value="all">All</option>
                         </select>
-                    </div>
-                    <div class="input-group col-sm-4 col-md-7 col-lg-7 col-xl-8 input-group-lg">
+                    </div><div class="input-group col-sm-4 col-md-7 col-lg-7 col-xl-8 input-group-lg">
                     <input type="text" class="form-control input-lg" id="autocomplete" name="q" placeholder="SEARCH" value="@if(isset($input['q'])) {{$input['q']}} @endif">
                     </div><div class="input-group col-sm-4 col-md-3 col-lg-3 col-xl-2 input-group-lg">
                         <input type="text" id="pac-input" class="form-control" placeholder="POST CODE" name="location" value="@if(isset($input['location'])) {{$input['location']}} @endif" required>
