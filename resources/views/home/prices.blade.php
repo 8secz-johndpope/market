a4cf2e44f8a19f84cf799ce1596e284837db773e
Anil M <anil@sumra.net>
2017-09-26 21:04:49 +0100

    auto

--- resources/views/home/prices.blade.php
+++ resources/views/home/prices.blade.php
@@ -21,9 +21,9 @@
                         <span class="extra-price">£{{$extra->price->price/100}}</span>
                             <input type="hidden" id="{{$extra->slug}}-price" value="{{$extra->price->price/100}}" name="{{$extra->slug}}-price">
                         @else
-                            <select class="form-control extra-change" name="{{$extra->slug}}-price" id="{{$extra->slug}}-price">
+                            <select class="form-control extra-change" name="{{$extra->slug}}-type" id="{{$extra->slug}}-price">
                                 @foreach($extra->prices as $price)
-                                <option value="{{$price->price/100}}">{{$price->title}}  (£{{$price->price/100}})</option>
+                                <option value="{{$price->key}}">{{$price->title}}  (£{{$price->price/100}})</option>
                                     @endforeach
                             </select>
                         @endif
