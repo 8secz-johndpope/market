a28e75c7751598e0bbc353f01718b79928374878
Anil M <anil@sumra.net>
2017-09-29 00:18:08 +0100

    auto

--- resources/views/business/stats.blade.php
+++ resources/views/business/stats.blade.php
@@ -8,7 +8,7 @@
         <tr>
             <td>
                 <table class="table">
-                    <tr>  <td>Views</td></td> <td>935</td><td>Listing views</td></td> <td>9,294</td></tr>
+                    <tr>  <td>Views</td></td> <td>{{$advert->param('views')}}</td><td>Listing views</td></td> <td>{{$advert->param('list_views')}}</td></tr>
                     <tr>  <td>Email replies</td></td> <td>11</td><td>Times bumped up</td></td> <td>0</td></tr>
                     <tr>  <td>Created</td></td> <td>12 days ago</td><td>Last posted</td></td> <td> 12 days ago</td></tr>
                     <tr>  <td>Ad id</td></td> <td>1266508749</td> <td> </td></td> <td></tr>
