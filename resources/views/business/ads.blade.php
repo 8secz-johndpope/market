17ab6143a82aee15aa11daecb4d972f01a13c443
Anil M <anil@sumra.net>
2017-09-29 00:10:17 +0100

    auto

--- resources/views/business/ads.blade.php
+++ resources/views/business/ads.blade.php
@@ -113,10 +113,37 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Modal Header</h4>
+                    <h4 class="modal-title">Statistics</h4>
                 </div>
                 <div class="modal-body">
-                    <p>Some text in the modal.</p>
+                    <table>
+
+                        <tr>
+                            <td>
+                                <table>
+                                 <tr>  <td>Views</td></td> <td>935</td></tr>
+                                    <tr>  <td>Email replies</td></td> <td>11</td></tr>
+                                    <tr>  <td>Created</td></td> <td>12 days ago</td></tr>
+                                    <tr>  <td>Ad id</td></td> <td>1266508749</td></tr>
+
+                                </table>
+
+
+                            </td>
+                            <td>
+
+                                <table>
+                                    <tr>  <td>Listing views</td></td> <td>9,294</td></tr>
+                                    <tr>  <td>Times bumped up</td></td> <td>0</td></tr>
+                                    <tr>  <td>Last posted</td></td> <td> 12 days ago</td></tr>
+                                    <tr>  <td></td></td> <td></td></tr>
+
+                                </table>
+                            </td>
+                        </tr>
+
+                    </table>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
