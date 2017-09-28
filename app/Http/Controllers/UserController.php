d56171b13f1c0e3244dcb3b63dc49748bfe27653
Anil M <anil@sumra.net>
2017-09-28 22:47:41 +0100

    auto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1116,7 +1116,7 @@ class UserController extends BaseController
         $lat = (float)$parts[0];
         $lng = (float)$parts[1];
         $loc = Location::where('min_lat','<=',$lat)->where('max_lat','>=',$lat)->where('min_lng','<=',$lng)->where('max_lng','>=',$lng)->orderBy('product')->first();
-        $body['location_id']=$loc->id;
+        $body['location_id']=$loc->res;
         $body['views']=0;
         $body['list_views']=0;
         $params = [
