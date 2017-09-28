a1f03549f905ecbe5fb98cb001b78b1d239a4979
Anil M <anil@sumra.net>
2017-09-29 00:41:34 +0100

    auto

--- resources/views/business/stats.blade.php
+++ resources/views/business/stats.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal">&times;</button>
-    <h4 class="modal-title">Statistics</h4>
+    <h4 class="modal-title">Statistics for {{$advert->param('title')}}</h4>
 </div>
 <div class="modal-body">
     <table>
