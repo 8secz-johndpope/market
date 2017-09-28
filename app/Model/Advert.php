97fc9253065b17f302c77a0fea98b103713e0e93
Anil M <anil@sumra.net>
2017-09-28 20:14:46 +0100

    auto

--- app/Model/Advert.php
+++ app/Model/Advert.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Advert extends  BaseModel
 {
+    public $dict=null;
     public function offers()
     {
         return $this->hasMany('App\Model\Offer');
@@ -26,41 +27,37 @@ class Advert extends  BaseModel
     {
         return $this->hasMany('App\Model\Application');
     }
-    public function param($param){
+    public function fetch(){
         $params = [
             'index' => 'adverts',
             'type' => 'advert',
             'id' => $this->elastic
         ];
         $response = $this->client->get($params);
+        $this->dict = $response['_source'];
+    }
+    public function param($param){
+       if($this->dict===null)
+           $this->fetch();
 
-        return $response['_source'][$param];
+        return $this->dict[$param];
     }
 
     public function meta($param){
-        $params = [
-            'index' => 'adverts',
-            'type' => 'advert',
-            'id' => $this->elastic
-        ];
-        $response = $this->client->get($params);
 
-        if(isset($response['_source']['meta'][$param]))
-        return $response['_source']['meta'][$param];
+
+        if(isset($this->dict['meta'][$param]))
+        return $this->dict['meta'][$param];
         else
             return '';
     }
     public function featured_expires(){
         $milliseconds = round(microtime(true) * 1000);
-        $params = [
-            'index' => 'adverts',
-            'type' => 'advert',
-            'id' => $this->elastic
-        ];
-        $response = $this->client->get($params);
-        if(isset($response['_source']['featured_expires']))
+        if($this->dict===null)
+            $this->fetch();
+        if(isset($this->dict['featured_expires']))
         {
-            $diff = $response['_source']['featured_expires']-$milliseconds;
+            $diff = $this->dict['featured_expires']-$milliseconds;
             if($diff<0)
                 return false;
             return ((int)($diff/(24*60*60000))+1);
@@ -71,15 +68,11 @@ class Advert extends  BaseModel
     }
     public function urgent_expires(){
         $milliseconds = round(microtime(true) * 1000);
-        $params = [
-            'index' => 'adverts',
-            'type' => 'advert',
-            'id' => $this->elastic
-        ];
-        $response = $this->client->get($params);
-        if(isset($response['_source']['urgent_expires']))
+        if($this->dict===null)
+            $this->fetch();
+        if(isset($this->dict['urgent_expires']))
         {
-            $diff = $response['_source']['urgent_expires'] - $milliseconds;
+            $diff = $this->dict['urgent_expires'] - $milliseconds;
             if($diff<0)
                 return false;
             return ((int)($diff/(24*60*60000))+1);
@@ -90,15 +83,11 @@ class Advert extends  BaseModel
     }
     public function spotlight_expires(){
         $milliseconds = round(microtime(true) * 1000);
-        $params = [
-            'index' => 'adverts',
-            'type' => 'advert',
-            'id' => $this->elastic
-        ];
-        $response = $this->client->get($params);
-        if(isset($response['_source']['spotlight_expires']))
+        if($this->dict===null)
+            $this->fetch();
+        if(isset($this->dict['spotlight_expires']))
         {
-            $diff = $response['_source']['spotlight_expires']-$milliseconds;
+            $diff = $this->dict['spotlight_expires']-$milliseconds;
             if($diff<0)
                 return false;
             return ((int)($diff/(24*60*60000))+1);
@@ -109,13 +98,9 @@ class Advert extends  BaseModel
     }
     public function posted(){
         $milliseconds = round(microtime(true) * 1000);
-        $params = [
-            'index' => 'adverts',
-            'type' => 'advert',
-            'id' => $this->elastic
-        ];
-        $response = $this->client->get($params);
-        $diff = $milliseconds-$response['_source']['created_at'];
+        if($this->dict===null)
+            $this->fetch();
+        $diff = $milliseconds-$this->dict['created_at'];
         if($diff<60*1000){
             $posted = 'Just Now';
         }
