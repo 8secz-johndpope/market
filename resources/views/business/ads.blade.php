dc18f657591dce5c6fa359da16dee13a03a54454
Anil M <anil@sumra.net>
2017-09-29 00:12:59 +0100

    auto

--- resources/views/business/ads.blade.php
+++ resources/views/business/ads.blade.php
@@ -121,22 +121,12 @@
                         <tr>
                             <td>
                                 <table class="table">
-                                 <tr>  <td>Views</td></td> <td>935</td></tr>
-                                    <tr>  <td>Email replies</td></td> <td>11</td></tr>
-                                    <tr>  <td>Created</td></td> <td>12 days ago</td></tr>
-                                    <tr>  <td>Ad id</td></td> <td>1266508749</td></tr>
+                                 <tr>  <td>Views</td></td> <td>935</td><td>Listing views</td></td> <td>9,294</td></tr>
+                                    <tr>  <td>Email replies</td></td> <td>11</td><td>Times bumped up</td></td> <td>0</td></tr>
+                                    <tr>  <td>Created</td></td> <td>12 days ago</td><td>Last posted</td></td> <td> 12 days ago</td></tr>
+                                    <tr>  <td>Ad id</td></td> <td>1266508749</td> <td> </td></td> <td></tr>
 
-                                </table>
-
-
-                            </td>
-                            <td>
-
-                                <table class="table">
-                                    <tr>  <td>Listing views</td></td> <td>9,294</td></tr>
-                                    <tr>  <td>Times bumped up</td></td> <td>0</td></tr>
-                                    <tr>  <td>Last posted</td></td> <td> 12 days ago</td></tr>
-                                    <tr>  <td></td></td> <td></td></tr>
+                                
 
                                 </table>
                             </td>
