6d3df9c3fb9abd66d2819494c3325021ec92c0fd
Anil M <anil@sumra.net>
2017-09-29 17:14:46 +0100

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -23,6 +23,7 @@ class MessageController extends BaseController
         $g = $client->request('POST', 'https://fire.sumra.net/groups', [
             'form_params' => ['id'=>104]
         ]);
+        return $g;
         return view('home.messages',['r'=>$r,'g'=>$g]);
     }
 
