b3ce3913c8c29a415484b070d59ed0618d6667cd
Anil M <anil@sumra.net>
2017-09-29 18:28:23 +0100

    auto

--- resources/views/home/messages.blade.php
+++ resources/views/home/messages.blade.php
@@ -63,7 +63,7 @@
                             @endforeach
                     </ul>
                     <div class="bottom-div-messages">
-                        <form class="form-inline">
+                        <form>
                             <div class="message-input-div"><input type="text" class="form-control"  placeholder="Type Your Message here" required></div>
                             <div class="message-send-div"><button type="submit" class="btn btn-primary">Send</button></div>
                         </form>
