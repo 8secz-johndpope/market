d557de7fb3a559432bd526f9f7a3e5d24c7167ab
Anil <anil@sumra.net>
2017-10-05 13:31:38 +0100

    auto

--- resources/views/business/company.blade.php
+++ resources/views/business/company.blade.php
@@ -58,6 +58,7 @@
                         </div>
                         <div id="package_summary" class="tab-pane fade">
                             <table class="table">
+                                <tr><th>Product</th><th>Total</th><th>Used</th><th>Remaining</th></tr>
                            @foreach($user->packs as $pack)
                                <tr><td>{{$pack->type()->stitle}} in {{$pack->category->title}},{{$pack->location->title}}</td><td>{{$pack->total}}</td><td>{{$pack->total-$pack->remaining}}</td><td><span class="red-text">{{$pack->remaining}}</span> </td></tr>
                                @endforeach
