fcdbead3b5fb0c11c2b19de36fea3ec56cb023fe
Anil <anil@sumra.net>
2017-10-11 16:55:27 +0100

    auto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -421,34 +421,46 @@ class HomeController extends BaseController
             $advert->publish();
         }
         if($request->has('post')||$request->has('update')){
-            $order = new Order;
-            $order->amount = 70;
-            $order->type = 'bump';
-            $order->buyer_id = $user->id;
-            $order->advert_id = $advert->id;
-            $order->save();
+
             $extratypes=ExtraType::all();
+            $needsorder=false;
             foreach ($extratypes as $type) {
                 if ($request->has($type->slug) && $request->get($type->slug) == 1) {
-                    $key = $type->slug;
-                    if ($type->type === 'list') {
-                        $key = $request->get($type->key);
-                    }
-                    $extraprice = ExtraPrice::where('key', $key)->first();
-                    $orderitem = new OrderItem;
-                    $orderitem->title = 'Featured';
-                    $orderitem->slug = 'featured';
-                    $orderitem->advert_id = $advert->id;
-                    $orderitem->category_id = $advert->category->id;
-                    $orderitem->location_id = $advert->postcode->location->id;
-                    $orderitem->type_id = $extraprice->id;
-                    $orderitem->amount = 0;
-                    $orderitem->save();
-                    $order->items()->save($orderitem);
+                    $needsorder=true;
+                }
+            }
+            if($needsorder) {
+                $order = new Order;
+                $order->amount = 70;
+                $order->type = 'bump';
+                $order->buyer_id = $user->id;
+                $order->advert_id = $advert->id;
+                $order->save();
 
+                foreach ($extratypes as $type) {
+                    if ($request->has($type->slug) && $request->get($type->slug) == 1) {
+                        $key = $type->slug;
+                        if ($type->type === 'list') {
+                            $key = $request->get($type->key);
+                        }
+                        $extraprice = ExtraPrice::where('key', $key)->first();
+                        $orderitem = new OrderItem;
+                        $orderitem->title = 'Featured';
+                        $orderitem->slug = 'featured';
+                        $orderitem->advert_id = $advert->id;
+                        $orderitem->category_id = $advert->category->id;
+                        $orderitem->location_id = $advert->postcode->location->id;
+                        $orderitem->type_id = $extraprice->id;
+                        $orderitem->amount = 0;
+                        $orderitem->save();
+                        $order->items()->save($orderitem);
+
+
+                    }
 
                 }
-
+                $request->session()->put('order_id', $order->id);
+                return redirect('/user/manage/order');
             }
         }
         return redirect('/user/manage/ads');
