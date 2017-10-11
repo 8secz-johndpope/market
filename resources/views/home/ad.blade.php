b8cc79006d54d0b5bd1b8c5d49929a071ca9080e
Anil <anil@sumra.net>
2017-10-11 15:36:04 +0100

    auto

--- resources/views/home/ad.blade.php
+++ resources/views/home/ad.blade.php
@@ -147,6 +147,9 @@
             @if($advert->category_id>0&&$advert->postcode_id>0)
             <form action="/user/advert/save" method="post" id="advert-form">
                 <input type="hidden" name="id" value="{{$advert->id}}" id="id">
+                <input type="hidden" name="category" value="{{$advert->category_id}}" id="id">
+                <input type="hidden" name="location_id" value="{{$advert->postcode->location->id}}" id="id">
+
                 {{ csrf_field() }}
                 <div class="all-panels" >
 
