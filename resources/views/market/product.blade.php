d753a36d4bc66e890bd5ab3e71acbf52859b3540
Anil <anil@sumra.net>
2017-10-13 15:21:19 +0100

    auto

--- resources/views/market/product.blade.php
+++ resources/views/market/product.blade.php
@@ -139,7 +139,7 @@
                     </div>
                         <a href="/userads/{{$advert->user->id}}">Ads({{count($advert->user->adverts)}})</a>
                         <ul class="list-group">
-                            <li class="list-group-item"><h4>{{$advert->user->diplay_name}}</h4></li>
+                            <li class="list-group-item"><h4>{{$advert->user->display_name}}</h4></li>
                         </ul>
                         @else
                         <ul class="list-group">
