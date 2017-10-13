369e2c4e51c9564462614c83b126ba23d60a914a
Anil <anil@sumra.net>
2017-10-13 21:10:04 +0100

    auto

---
+++ app/Model/SearchAlert.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 13/10/2017
+ * Time: 20:56
+ */
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SearchAlert extends Model
+{
+    public $timestamps=false;
+}
