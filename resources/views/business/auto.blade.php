4fe3c8c81de9ba5fcd4a0838f4e8c2df1dd0665a
Anil <anil@sumra.net>
2017-10-14 20:32:52 +0100

    auto

--- resources/views/business/auto.blade.php
+++ resources/views/business/auto.blade.php
@@ -78,6 +78,9 @@
             </table>
                 <button type="submit">Save</button>
             </form>
+            <div style="min-height: 50px">
+                
+            </div>
         </div>
     </div>
 
