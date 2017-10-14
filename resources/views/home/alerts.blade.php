5e0f0ffa3cc5ce81d01f69531e540fab3b2229f5
Anil <anil@sumra.net>
2017-10-14 14:08:39 +0100

    auto

--- resources/views/home/alerts.blade.php
+++ resources/views/home/alerts.blade.php
@@ -60,7 +60,7 @@
                       <span class="listing-location">{{$alert->location->title}}</span>
                       <br>
                       <label class="switch">
-                          <input type="checkbox" checked>
+                          <input type="checkbox" @if($alert->active===1) checked @endif>
                           <span class="slider round"></span>
                       </label>
                       <a class="red-color delete-alert-link" href="/user/delete/alert/{{$alert->id}}"><span class="glyphicon glyphicon-remove-circle"></span>&nbsp;&nbsp; Delete</a>
