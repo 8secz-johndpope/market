5bbba76a6475f22941a4f1c884dab223d09ca12e
Anil <anil@sumra.net>
2017-10-15 21:17:28 +0100

    auto

--- app/Model/Advert.php
+++ app/Model/Advert.php
@@ -84,7 +84,7 @@ class Advert extends  BaseModel
             $params = [
                 'index' => 'adverts',
                 'type' => 'advert',
-                'body' => ['title'=>'']
+                'body' => ['title'=>'','draft'=>1]
             ];
             $response = $this->client->index($params);
             $this->elastic = $response['_id'];
