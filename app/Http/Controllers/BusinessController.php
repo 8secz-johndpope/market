2d5306cfad59d9917d5aad4e1fbfd283f5a75c57
Anil <anil@sumra.net>
2017-10-18 16:38:31 +0100

    auto

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -113,8 +113,18 @@ class BusinessController extends BaseController
     public function csv(Request $request){
 
         $path = $request->csv;//->store('images');
+        $row = 1;
 
         if (($handle = fopen($path->path(), "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                echo "<p> $num fields in line $row: <br /></p>\n";
+                $row++;
+                for ($c=0; $c < $num; $c++) {
+                    echo $data[$c] . "<br />\n";
+                }
+            }
+            fclose($handle);
 
          }
         return $path->path();
