c52def15f4404181c199fd365f4ed44b69259086
Anil <anil@sumra.net>
2017-10-18 13:30:57 +0100

    auto

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/userads/{id}', 'MarketController@userads');
 Route::get('/user/manage/messages', 'MessageController@messages');
 Route::get('/user/manage/messages/{rid}', 'MessageController@gmessages');
 
-Route::post('/check','MessageController@check');
+Route::get('/check','MessageController@check');
 
 Route::post('/user/message/send','MessageController@send');
 Route::post('/user/message/rsend','MessageController@rsend');
