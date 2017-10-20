3f78e9807d8a7d88ede1dcc1fcd26bda530bff0e
Anil <anil@sumra.net>
2017-10-20 13:12:14 +0100

    auto

--- resources/views/home/ad.blade.php
+++ resources/views/home/ad.blade.php
@@ -389,7 +389,7 @@
                             <div class="panel-body">
                                 <div class="row">
                                     <div class="col-sm-6">
-                                        <input  type="checkbox" name="candeliver" value="1"><span class="delivery-text">Can Deliver</span>
+                                        <input  type="checkbox" name="candeliver" value="1"><span class="delivery-text">Can Deliver Locally</span>
                                     </div>
                                     <div class="col-sm-6">
                                         <select class="form-control" name="distance">
