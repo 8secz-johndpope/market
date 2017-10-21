23746000d4f04ccbff5fda00c1ce60c0d697b163
Anil <anil@sumra.net>
2017-10-21 18:02:15 +0100

    auto

---
+++ app/Http/Controllers/CronController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 21/10/2017
+ * Time: 17:55
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class CronController extends BaseController
+{
+    public function parse_page(Request $request){
+        $text = file_get_contents('http://www.greatcare.co.uk/nanny-and-childcare-jobs/206372/russian-speaking-baby-nanny-needed---%C2%A31000p-w.html');
+        $dom = new \DOMDocument();
+        $dom->loadHTML($text);
+        $element = $dom->getElementById('tblJobDetails');
+        echo $dom->saveHTML($element);
+
+
+    }
+
+}
