6f5be288701a5f66a47f9b952164cf5f32fb51e4
Anil <anil@sumra.net>
2017-10-21 18:03:17 +0100

    auto

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -16,6 +16,7 @@ class CronController extends BaseController
     public function parse_page(Request $request){
         $text = file_get_contents('http://www.greatcare.co.uk/nanny-and-childcare-jobs/206372/russian-speaking-baby-nanny-needed---%C2%A31000p-w.html');
         $dom = new \DOMDocument();
+        libxml_use_internal_errors(true);
         $dom->loadHTML($text);
         $element = $dom->getElementById('tblJobDetails');
         echo $dom->saveHTML($element);
