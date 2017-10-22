da42d33fd2bc59fdb2f944b19f426a33d7e4f69d
Anil <anil@sumra.net>
2017-10-22 16:18:17 +0100

    auto

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -141,6 +141,7 @@ class CronController extends BaseController
         $classname="turnstileLink";
         $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
         //echo $nodes[0]->getAttribute('href');
+        $title=$nodes[0]->nodeValue;
         $text=file_get_contents('https://www.indeed.co.uk'.$nodes[0]->getAttribute('href'));
         $dom = new \DOMDocument();
         libxml_use_internal_errors(true);
@@ -156,8 +157,50 @@ class CronController extends BaseController
         $random_surname = $surnames[mt_rand(0, sizeof($surnames) - 1)];
 
 //Combine them together and print out the result.
-        if($nodes[0]->nodeValue==='Private Family')
-        echo $random_name . ' ' . $random_surname;
+        if($nodes[0]->nodeValue==='Private Family'){
+            $fullname = $random_name . ' ' . $random_surname;
+            $email=$random_name.$random_surname.'@sumra.net';
+        }else{
+            $fullname=$nodes[0]->nodeValue;
+            $email=str_replace(' ','',$nodes[0]->nodeValue).'@sumra.net';
+        }
+        $user = User::where('email',$email)->first();
+        if($user===null){
+            $user = new User;
+            $user->email=$email;
+            $user->name=$fullname;
+            $user->display_name=$fullname;
+            $user->password= bcrypt('password');
+            $user->phone='07777777777';
+            // $user->more(['email' => 'g'.$body['source_id'].'@sumra.net', 'name' => $body['username'], 'password' => bcrypt('password'), 'phone' => '07777777777']);
+            //  $user->id=(int)$body['user_id'];
+            $user->save();
+        }
+        $category=418020000;
+        $description = $dom->getElementById('job_summary')->nodeValue;
+        echo $description;
+
+        $advert = new Advert;
+        $advert->category_id=$category;
+        $advert->save();
+        $advert->create_dd($user);
+
+
+
+        $body=[];
+        $body['title']=$title;
+        $body['category']=$category;
+        $body['description']=$description;
+        $body['location_id']=1250000000;
+        $body['location']='52.2,0.13';
+        $body['username']=$user->display_name;
+        $body['phone']=$user->phone;
+        $body['user_id']=0;
+        $advert->update_fields($body);
+        $advert->publish();
+        echo 'done';
+
+
     }
 
 }
