a2d22fa51e03b1e7ed908a1413094ab6619d90b1
Anil <anil@sumra.net>
2017-10-22 16:40:38 +0100

    auto

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -134,8 +134,7 @@ class CronController extends BaseController
 
 
         $text=file_get_contents('https://www.indeed.co.uk/nanny-jobs-in-London-Borough-of-Hackney,-Greater-London');
-        echo $text;
-        exit;
+      
         $dom = new \DOMDocument();
         libxml_use_internal_errors(true);
         $dom->loadHTML($text);
