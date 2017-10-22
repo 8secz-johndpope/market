6c0f84b1f2ff9bc3a8f2b05a3c1a4d0bdb12e45f
Anil <anil@sumra.net>
2017-10-22 16:20:57 +0100

    auto

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -141,64 +141,68 @@ class CronController extends BaseController
         $classname="turnstileLink";
         $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
         //echo $nodes[0]->getAttribute('href');
-        $title=$nodes[0]->nodeValue;
-        $text=file_get_contents('https://www.indeed.co.uk'.$nodes[0]->getAttribute('href'));
-        $dom = new \DOMDocument();
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($text);
-        $finder = new \DomXPath($dom);
-        $classname="company";
-        $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
-        echo $nodes[0]->nodeValue;
-        //Generate a random forename.
-        $random_name = $names[mt_rand(0, sizeof($names) - 1)];
+        $links=$nodes;
+        for($i=0;$i<count($links);$i++) {
+
+
+            $title = $links[$i]->nodeValue;
+            $text = file_get_contents('https://www.indeed.co.uk' . $links[$i]->getAttribute('href'));
+            $dom = new \DOMDocument();
+            libxml_use_internal_errors(true);
+            $dom->loadHTML($text);
+            $finder = new \DomXPath($dom);
+            $classname = "company";
+            $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+            echo $nodes[0]->nodeValue;
+            //Generate a random forename.
+            $random_name = $names[mt_rand(0, sizeof($names) - 1)];
 
 //Generate a random surname.
-        $random_surname = $surnames[mt_rand(0, sizeof($surnames) - 1)];
+            $random_surname = $surnames[mt_rand(0, sizeof($surnames) - 1)];
 
 //Combine them together and print out the result.
-        if($nodes[0]->nodeValue==='Private Family'){
-            $fullname = $random_name . ' ' . $random_surname;
-            $email=$random_name.$random_surname.'@sumra.net';
-        }else{
-            $fullname=$nodes[0]->nodeValue;
-            $email=str_replace(' ','',$nodes[0]->nodeValue).'@sumra.net';
+            if ($nodes[0]->nodeValue === 'Private Family') {
+                $fullname = $random_name . ' ' . $random_surname;
+                $email = $random_name . $random_surname . '@sumra.net';
+            } else {
+                $fullname = $nodes[0]->nodeValue;
+                $email = str_replace(' ', '', $nodes[0]->nodeValue) . '@sumra.net';
+            }
+            $user = User::where('email', $email)->first();
+            if ($user === null) {
+                $user = new User;
+                $user->email = $email;
+                $user->name = $fullname;
+                $user->display_name = $fullname;
+                $user->password = bcrypt('password');
+                $user->phone = '07777777777';
+                // $user->more(['email' => 'g'.$body['source_id'].'@sumra.net', 'name' => $body['username'], 'password' => bcrypt('password'), 'phone' => '07777777777']);
+                //  $user->id=(int)$body['user_id'];
+                $user->save();
+            }
+            $category = 418020000;
+            $description = $dom->getElementById('job_summary')->nodeValue;
+            echo $description;
+
+            $advert = new Advert;
+            $advert->category_id = $category;
+            $advert->save();
+            $advert->create_dd($user);
+
+
+            $body = [];
+            $body['title'] = $title;
+            $body['category'] = $category;
+            $body['description'] = $description;
+            $body['location_id'] = 1250000000;
+            $body['location'] = '52.2,0.13';
+            $body['username'] = $user->display_name;
+            $body['phone'] = $user->phone;
+            $body['user_id'] = 0;
+            $advert->update_fields($body);
+            $advert->publish();
+            echo 'done';
         }
-        $user = User::where('email',$email)->first();
-        if($user===null){
-            $user = new User;
-            $user->email=$email;
-            $user->name=$fullname;
-            $user->display_name=$fullname;
-            $user->password= bcrypt('password');
-            $user->phone='07777777777';
-            // $user->more(['email' => 'g'.$body['source_id'].'@sumra.net', 'name' => $body['username'], 'password' => bcrypt('password'), 'phone' => '07777777777']);
-            //  $user->id=(int)$body['user_id'];
-            $user->save();
-        }
-        $category=418020000;
-        $description = $dom->getElementById('job_summary')->nodeValue;
-        echo $description;
-
-        $advert = new Advert;
-        $advert->category_id=$category;
-        $advert->save();
-        $advert->create_dd($user);
-
-
-
-        $body=[];
-        $body['title']=$title;
-        $body['category']=$category;
-        $body['description']=$description;
-        $body['location_id']=1250000000;
-        $body['location']='52.2,0.13';
-        $body['username']=$user->display_name;
-        $body['phone']=$user->phone;
-        $body['user_id']=0;
-        $advert->update_fields($body);
-        $advert->publish();
-        echo 'done';
 
 
     }
