5931df3dd0c89a137b820c98149a6d009d1fe8de
Anil <anil@sumra.net>
2017-10-25 16:08:22 +0100

    auto

--- resources/views/market/product.blade.php
+++ resources/views/market/product.blade.php
@@ -139,6 +139,8 @@
                         <input class="form-control" placeholder="SW153AZ" name="postcode">
 
                     <button class="btn btn-default">Check</button>
+                    <br>
+                    <span id="delivery-info">Can be delivered to <span class="bold-text" id="postcode-text">M139AX</span> </span>
                         <h4>Can Delivery Locally(Within {{$advert->meta('distance')}}  Miles)</h4>
                         <p>Price</p>
                         <span class="bold-text">£{{$advert->price()}}</span><span>+£{{$advert->delivery()}}&nbsp;&nbsp; Delivery</span>
