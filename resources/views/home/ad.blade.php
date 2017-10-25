1f0bacff9af536c98b9412f7db4121245787fc73
Anil <anil@sumra.net>
2017-10-25 13:23:43 +0100

    auto

--- resources/views/home/ad.blade.php
+++ resources/views/home/ad.blade.php
@@ -369,7 +369,7 @@
                                             <p class="bold-text">Buyer Pays</p>
                                             <div class="input-group">
                                                 <span class="input-group-addon"><i class="glyphicon glyphicon-gbp"></i></span>
-                                                <input type="number" name="delivery" id="delivery"  class="form-control  mb-2 mr-sm-2 mb-sm-0" placeholder="" @if(!$advert->has_param('candeliver')||$advert->param('candeliver')!==1) disabled @endif  value="@if($advert->has_param('candeliver')&&$advert->param('candeliver')===1&&$advert->has_meta('delivery')){{$advert->delivery()}}@endif" step="1">
+                                                <input type="number" name="delivery" id="delivery"  class="form-control  mb-2 mr-sm-2 mb-sm-0" placeholder="" @if(!$advert->has_param('candeliver')||$advert->param('candeliver')!==1) disabled @endif  value="@if($advert->has_param('candeliver')&&$advert->param('candeliver')===1&&$advert->has_meta('delivery')){{$advert->delivery()}}@endif" step="0.01">
                                             </div>
                                         </div>
                                         </div>
@@ -405,7 +405,7 @@
                                             <p class="bold-text">Buyer Pays</p>
                                             <div class="input-group">
                                                 <span class="input-group-addon"><i class="glyphicon glyphicon-gbp"></i></span>
-                                                <input type="number" name="buyer_pays" id="buyer_pays"  class="form-control  mb-2 mr-sm-2 mb-sm-0" placeholder="" @if($advert->has_param('freeshipping')&&$advert->param('freeshipping')===1) disabled @endif  value="@if($advert->has_param('freeshipping')&&$advert->param('freeshipping')===1) 0.00 @elseif($advert->has_meta('shipping')&&$advert->meta('shipping')>=0){{$advert->shipping()}}@endif" step="1">
+                                                <input type="number" name="buyer_pays" id="buyer_pays"  class="form-control  mb-2 mr-sm-2 mb-sm-0" placeholder="" @if($advert->has_param('freeshipping')&&$advert->param('freeshipping')===1) disabled @endif  value="@if($advert->has_param('freeshipping')&&$advert->param('freeshipping')===1) 0.00 @elseif($advert->has_meta('shipping')&&$advert->meta('shipping')>=0){{$advert->shipping()}}@endif" step="0.01">
                                             </div>
                                             <br>
                                             <input  type="checkbox" id="freeshipping" name="freeshipping" value="1" @if($advert->has_param('freeshipping')&&$advert->param('freeshipping')===1) checked @endif><span class="delivery-text">Free Shipping</span>
