724dcf2918304fa062fa6ddbc371e4f44517bda8
Anil <anil@sumra.net>
2017-10-27 15:23:26 +0100

    auto

--- resources/views/market/product.blade.php
+++ resources/views/market/product.blade.php
@@ -146,7 +146,9 @@
                     <br>
                         @if (Auth::guest()|| Auth::user()->default_address===0)
                             @else
-                    <span id="delivery-info" @if(!$advert->can_deliver_to(Auth::user()->address->zip)) style="display: none" @endif>Can be delivered to  <span class="bold-text" id="postcode-text">{{ Auth::user()->address->postcode}} </span>  <a id="edit-post">Edit</a></span>
+                            <span><span id="delivery-info" @if(!$advert->can_deliver_to(Auth::user()->address->zip)) style="display: none" @endif>Can be delivered to </span> <span id="s-info" @if($advert->can_deliver_to(Auth::user()->address->zip)) style="display: none" @endif>Cannot be delivered to </span>  <span class="bold-text" id="postcode-text">{{ Auth::user()->address->postcode}} </span>
+                           <a id="edit-post">Edit</a></span>
+
                         @endif
                             <h4>Can Delivery Locally(Within {{$advert->meta('distance')}}  Miles)</h4>
                         <p>Price</p>
