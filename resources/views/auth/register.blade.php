ce7573b24c342e5ba96ab652059bc34707230bef
Anil <anil@sumra.net>
2017-10-27 20:05:45 +0100

    auto

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,12 +13,21 @@
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                            <div class="col-md-3">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus placeholder="First Name">
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="col-md-3">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('last') }}" required placeholder="Last Name">
+
+                                @if ($errors->has('last'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('last') }}</strong>
                                     </span>
                                 @endif
                             </div>
