8268628989aac6942eb2f37ba4941fd986ebe1e3
Anil <anil@sumra.net>
2017-10-27 18:18:38 +0100

    auto

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -81,7 +81,7 @@ class BusinessController extends BaseController
     public function details(Request $request){
 
         $user = Auth::user();
-        $balance = \Stripe\Balance::retrieve($user->stripe_account);
+        $balance = \Stripe\Balance::retrieve( array("stripe_account" => $user->stripe_account));
         return $balance;
         $stripe_id = $user->stripe_id;
 
