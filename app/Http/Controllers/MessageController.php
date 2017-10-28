46423d15bb404257090827cc14038334f5f30ea6
Anil <anil@sumra.net>
2017-10-28 16:19:55 +0100

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -253,7 +253,9 @@ class MessageController extends BaseController
         $g = $client->request('POST', 'https://fcm.googleapis.com/fcm/send', [
             'form_params' => ['to' => 'cFX7C7fVoHA:APA91bE4gCqSZ6YynKZd98Ar8ZoI8ST1HBToikZjTk1Q0xyT6qOvm06kg8inGioJ7P9MCYrATTUQNmurmQAq3wCtheaH9yb2COtNSR4SDUD2l-h5uuS9idhPHJBRpvU0_5K5lFAoyXmh', 'priority'=>'high','data','1','notification'=>['title'=>'title','body'=>'message','sound'=>'mySound']]
         ]);
+        return $g->getBody();
         $g = json_decode($g->getBody(), true);
+
         return ['great'=>'yes','res'=>$g];
     }
     public function all_messages(Request $request){
