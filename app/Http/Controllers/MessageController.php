b5d7514b21d91870629106d0b3505bccbf176b4f
Anil <anil@sumra.net>
2017-10-29 15:23:34 +0000

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -30,35 +30,6 @@ class MessageController extends BaseController
         $user = Auth::user();
 
         return view('home.messages',['cur'=>$user->rooms()->first(),'user'=>$user]);
-
-        /*
-
-        $client = new Client();
-
-        $g = $client->request('POST', 'https://fire.sumra.net/groups', [
-            'form_params' => ['id'=>$user->id]
-        ]);
-        ///var_dump($g);
-        //exit;
-        $g = json_decode($g->getBody(),true);
-        if(count($g)>0){
-            usort($g,function ($a, $b)
-            {
-                if ($a['modified_at'] == $b['modified_at']) {
-                    return 0;
-                }
-                return ($a['modified_at'] < $b['modified_at']) ? 1 : -1;
-            });
-            $r = $client->request('POST', 'https://fire.sumra.net/groupmessages', [
-                'form_params' => ['rid'=>$g[0]['rid'],"time"=>0]
-            ]);
-            $r = json_decode($r->getBody(),true);
-
-            return view('home.messages',['r'=>$r,'g'=>$g,'rid'=>$g[0]['rid'],'user'=>$user]);
-        }
-        else
-            return view('home.messages',['r'=>[],'g'=>[],'rid'=>'nomessages','user'=>$user]);
-        */
     }
     public function gmessages(Request $request,$rid){
         $user = Auth::user();
@@ -68,31 +39,6 @@ class MessageController extends BaseController
 
         }
         return view('home.messages',['cur'=>$room,'user'=>$user]);
-
-        /*
-        $client = new Client();
-
-        $r = $client->request('POST', 'https://fire.sumra.net/groupmessages', [
-            'form_params' => ['rid'=>$rid,"time"=>0]
-        ]);
-
-        $g = $client->request('POST', 'https://fire.sumra.net/groups', [
-            'form_params' => ['id'=>$user->id]
-        ]);
-        ///var_dump($g);
-        //exit;
-        $r = json_decode($r->getBody(),true);
-        $g = json_decode($g->getBody(),true);
-        usort($g,function ($a, $b)
-        {
-            if ($a['modified_at'] == $b['modified_at']) {
-                return 0;
-            }
-            return ($a['modified_at'] < $b['modified_at']) ? 1 : -1;
-        });
-        //return $g;
-        return view('home.messages',['r'=>$r,'g'=>$g,'rid'=>$rid,'user'=>$user]);
-        */
     }
 
     public function reply(Request $request,$id){
@@ -115,22 +61,6 @@ class MessageController extends BaseController
             $user = Auth::user();
             $advert = Advert::find($request->id);
 
-            /*
-            $client = new Client();
-            $g = $client->request('POST', 'https://fire.sumra.net/creategroup', [
-                'form_params' => ['advert_id' => $advert->sid, 'users' => [$user->id, $advert->user_id], 'title' => $advert->param('title'), 'image' => $advert->first_image(), 'id' => $user->id]
-            ]);
-            $g = json_decode($g->getBody(), true);
-            $k = $client->request('POST', 'https://fire.sumra.net/groupmessage', [
-                'form_params' => ['from' => $user->id, 'message' => $request->message, 'rid' => $g['rid'], 'type' => 'text']
-            ]);
-            $advert->replies++;
-            $advert->save();
-
-
-            */
-            $uuid = Uuid::uuid1();
-            $mid = Uuid::uuid1();;
 
 
             $room = Room::where('advert_id',$advert->id)->where('sender_id',$user->id)->first();
@@ -138,7 +68,6 @@ class MessageController extends BaseController
                 $room = new Room;
                 $room->advert_id = $advert->id;
                 $room->image=$advert->first_image();
-                $room->rid=$uuid;
                 $room->title=$advert->param('title');
                 $room->sender_id=$user->id;
                 $room->save();
@@ -152,8 +81,6 @@ class MessageController extends BaseController
 
             $message = new Message;
             $message->message=$request->message;
-            $message->rid=$uuid;
-            $message->mid=$mid;
             $message->from_msg=$user->id;
             $message->to_msg=$advert->user_id;
             $message->room_id=$room->id;
@@ -172,29 +99,8 @@ class MessageController extends BaseController
         }
 
     }
-    public function fsend(Request $request){
-        $user = Auth::user();
-        $advert = Advert::find($request->id);
-        if($advert===null){
-            $advert = Advert::where('sid',$request->id)->first();
-        }
-        $client = new Client();
-        $g = $client->request('POST', 'https://fire.sumra.net/creategroup', [
-            'form_params' => ['advert_id'=>$advert->sid,'users'=>[$user->id,$advert->user_id],'title'=>$advert->param('title'),'image'=>$advert->first_image(),'id'=>$user->id]
-        ]);
-        $g = json_decode($g->getBody(),true);
-        $k = $client->request('POST', 'https://fire.sumra.net/groupmessage', [
-            'form_params' => ['from'=>$user->id,'message'=>$request->message,'rid'=>$g['rid'],'type'=>'text']
-        ]);
-        $advert->replies++;
-        $advert->save();
 
-        return ['rid'=>$g['rid'],'msg'=>'sent'];
-
-    }
     public function create_message(Request $request){
-        $uuid = Uuid::uuid1();
-        $mid = Uuid::uuid1();;
 
         $user = Auth::user();
         $advert = Advert::find($request->id);
@@ -206,7 +112,6 @@ class MessageController extends BaseController
             $room = new Room;
             $room->advert_id = $advert->id;
             $room->image=$advert->first_image();
-            $room->rid=$uuid;
             $room->title=$advert->param('title');
             $room->sender_id=$user->id;
             $room->save();
@@ -222,8 +127,6 @@ class MessageController extends BaseController
 
         $message = new Message;
         $message->message=$request->message;
-        $message->rid=$uuid;
-        $message->mid=$mid;
         $message->from_msg=$user->id;
         $message->to_msg=$advert->user_id;
         $message->room_id=$room->id;
@@ -242,8 +145,7 @@ class MessageController extends BaseController
     }
 
     public function normal_message(Request $request){
-        $uuid = Uuid::uuid1();
-        $mid = Uuid::uuid1();
+
 
         $user = Auth::user();
         $room = Room::find($request->id);
@@ -251,8 +153,6 @@ class MessageController extends BaseController
 
         $message = new Message;
         $message->message=$request->message;
-        $message->rid=$uuid;
-        $message->mid=$mid;
         $message->from_msg=$user->id;
         $message->to_msg=$advert->user_id;
         $message->room_id=$room->id;
@@ -367,7 +267,6 @@ class MessageController extends BaseController
 
             $message = new Message;
             $message->message=$request->message;
-            $message->rid=$room->rid;
             $message->mid=$mid;
             $message->from_msg=$user->id;
             $message->to_msg=$advert->user_id;
@@ -383,13 +282,6 @@ class MessageController extends BaseController
 
 
 
-
-            /*
-            $client = new Client();
-            $g = $client->request('POST', 'https://fire.sumra.net/groupmessage', [
-                'form_params' => ['from'=>$user->id,'message'=>$request->message,'rid'=>$request->rid,'type'=>'text']
-            ]);
-            */
 
         }
 
