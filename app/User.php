2f84dec33130f6c4b11779b2cd91023eb2080eab
Anil <anil@sumra.net>
2017-10-29 16:24:06 +0000

    auto

--- app/User.php
+++ app/User.php
@@ -75,7 +75,7 @@ class User extends Authenticatable
     }
     public function rooms()
     {
-        return $this->belongsToMany('App\Model\Room');
+        return $this->belongsToMany('App\Model\Room')->orderBy('room_id','desc');
     }
     public function addresses()
     {
