e69497ae4d3a228096b6469456dcaa6fd1c65094
Anil <anil@sumra.net>
2017-10-29 15:06:49 +0000

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -334,7 +334,7 @@ class MessageController extends BaseController
         ]);
         //$tk='cFX7C7fVoHA:APA91bE4gCqSZ6YynKZd98Ar8ZoI8ST1HBToikZjTk1Q0xyT6qOvm06kg8inGioJ7P9MCYrATTUQNmurmQAq3wCtheaH9yb2COtNSR4SDUD2l-h5uuS9idhPHJBRpvU0_5K5lFAoyXmh';
         $g = $client->request('POST', 'https://fcm.googleapis.com/fcm/send', [
-            'json' => ['to' => $token->token , 'priority'=>'high','data','1','notification'=>['title'=>$room->title,'body'=>$message->message,'sound'=>'mySound']]
+            'json' => ['to' => $token->token , 'priority'=>'high','data',['id'=>$room->id],'notification'=>['title'=>$room->title,'body'=>$message->message,'sound'=>'mySound']]
         ]);
         $g = json_decode($g->getBody(), true);
 
