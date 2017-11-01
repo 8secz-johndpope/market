a7af987b8701bdebd36b39c50789dfc74ca1884b
Anil <anil@sumra.net>
2017-11-01 16:30:28 +0000

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -114,7 +114,7 @@ class MessageController extends BaseController
     }
 public function notify($room,$message){
     $user = Auth::user();
-    $message->type=$message->type;
+    $message->type='text';
     foreach ($room->users as $usr){
         if($usr->id!==$user->id)
         {
