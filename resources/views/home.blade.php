69d9c4cf6b66714b58b07256de40dd1526d459a7
Mauricio <mauri@sumra.net>
2017-11-01 13:19:38 +0000

    fixed spotlight

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,21 +82,9 @@
 											</div>
 										</div>
 									</div>
-						    </div>
 						    @endfor
 						</div>
-						<!-- Left and right controls -->
-						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-							<span class="glyphicon glyphicon-chevron-left"></span>
-						    <span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#myCarousel" data-slide="next">
-						    <span class="glyphicon glyphicon-chevron-right"></span>
-						    <span class="sr-only">Next</span>
-						</a>
 					</div>
-				</div>
-			</div>
 		</section>
 		<section class="features">
 			<div class="container">
