0847bfca503a7e2b738cc2caaf8e2e3765a78c79
Mauricio <mauri@sumra.net>
2017-11-02 17:25:42 +0000

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -922,6 +922,7 @@ class MarketController extends BaseController
         $latlng=explode(',',$latlng);
 
         $meta = $product['meta'];
+        var_dump($meta)
         $metas = array();
         foreach ($meta as $key => $value){
             $field = Field::where('slug',$key)->first();
@@ -969,10 +970,7 @@ class MarketController extends BaseController
         ];
         $response = $this->client->search($params);
         $products = array_map(function ($a) { return $a['_source']; },$response['hits']['hits']);
-        /*if(isset($metas['available_date'])){
-            $date = $metas['available_date'];
-            $metas['available_date'] = date("d F Y", $date / 1000);
-        }*/
+        
         return View('market.product',['advert'=>$advert,'product'=>$product,'products'=>$products,'image'=>$image,'images'=>$images,'counts'=>range(1,count($images)),'metas'=>$metas,'parents'=>$parents,'category'=>$category,'lat'=>$latlng[0],'lng'=>$latlng[1]]);
     }
     private  function haversineGreatCircleDistance(
