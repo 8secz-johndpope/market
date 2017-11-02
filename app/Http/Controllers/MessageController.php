405b659ced1ade96f40852cb115264a820dc6ea1
Anil <anil@sumra.net>
2017-11-02 13:28:20 +0000

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -43,6 +43,7 @@ class MessageController extends BaseController
 
         $user = Auth::user();
         $room=Room::find($th);
+        $room->toggle=!$room->toggle;
         $room->save();
         return view('home.rooms',['cur'=>Room::find($id),'user'=>$user,'leftclass'=>'left-div-noroom','rightclass'=>'right-div-noroom']);
     }
