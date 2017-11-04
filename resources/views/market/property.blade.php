063790fb1aef4bd8323d2738dd23abc25325e026
Mauricio <mauri@sumra.net>
2017-11-04 15:48:26 +0000

    auto

--- resources/views/market/property.blade.php
+++ resources/views/market/property.blade.php
@@ -14,7 +14,7 @@
     <link href="{{ asset('/css/property.css?q=874') }}" rel="stylesheet">
         <div class="row">
 
-        <div class="col-lg-3"></div>
+        <div class="col-lg-2"></div>
                 <div class="col-lg-6 col-md-9 col-sm-12">
                     <ol class="breadcrumb">
                         @foreach($parents as $parent)
