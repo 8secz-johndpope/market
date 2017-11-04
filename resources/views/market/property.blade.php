a0dff46908329f82b582152b7349f0c1dd18213b
Mauricio <mauri@sumra.net>
2017-11-04 19:29:40 +0000

    auto

--- resources/views/market/property.blade.php
+++ resources/views/market/property.blade.php
@@ -44,7 +44,7 @@
                         <div class="carousel-inner">
                             @for($i=0; $i<count($images); $i++)
                             <div class="item frame">
-                                @for($j=0; $j < 5; $j++)
+                                @for($j=0; $j < 5 && ($i+$j)<count($images); $j++)
                                 <img src="https://s3.eu-central-1.amazonaws.com/web.eu-central-1.sumra.net/{{$images[$i+$j]}}?1500586448" alt="Los Angeles">
                                 @endfor
                                 @php
