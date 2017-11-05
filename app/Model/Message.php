e229d49a15713e498673fd007af2205b79de6d8a
Anil <anil@sumra.net>
2017-11-05 18:15:34 +0000

    auto

--- app/Model/Message.php
+++ app/Model/Message.php
@@ -16,4 +16,22 @@ class Message extends Model
     {
         return $this->belongsTo('App\User','from_msg');
     }
+    public function timestamp(){
+        return date('G:i',strtotime($this->created_at));
+    }
+    public function day(){
+        $current = strtotime(date("Y-m-d"));
+        $yesterday = $current - 24*60*60;
+        $msg = strtotime(date('Y-m-d',strtotime($this->created_at)));
+
+        $diff = $msg-$current;
+        if($diff<24*3600){
+            return 'Today';
+        }
+        $diff = $msg-$yesterday;
+        if($diff<24*3600*2){
+            return 'Yesterday';
+        }
+        return date('D d M',strtotime($this->created_at));
+    }
 }
--- resources/views/home/messages.blade.php
+++ resources/views/home/messages.blade.php
@@ -89,9 +89,9 @@
                         @foreach($cur->messages as $message)
                             @if($message->from_msg===$user->id)
 
-                            <div class="right-message"><span class="message"> {{$message->message}}&nbsp;&nbsp; {{$message->created_at}}</span></div>
+                            <div class="right-message"><span class="message"> {{$message->message}}&nbsp;&nbsp; {{$message->timestamp()}}</span></div>
                             @else
-                                <div class="left-message"><span class="message">{{$message->message}}&nbsp;&nbsp; {{$message->created_at}}</span></div>
+                                <div class="left-message"><span class="message">{{$message->message}}&nbsp;&nbsp; {{$message->timestamp()}}</span></div>
                             @endif
 
                         @endforeach
