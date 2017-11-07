041939c9704ca36b10f942956a9ab4a79f717074
Mauricio <mauri@sumra.net>
2017-11-07 19:20:26 +0000

    auto

--- resources/views/market/property.blade.php
+++ resources/views/market/property.blade.php
@@ -587,7 +587,7 @@
     function prueba(response, status) {
     // See Parsing the Results for
     // the basics of a callback function.
-        console.log(response);
+        console.log(response.rows[0].elements);
     }
 
 
