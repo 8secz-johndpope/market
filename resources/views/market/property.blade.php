81ac528ce7c5090bf14626123caa7b9de16d6e0b
Mauricio <mauri@sumra.net>
2017-11-08 18:13:56 +0000

    auto

--- resources/views/market/property.blade.php
+++ resources/views/market/property.blade.php
@@ -431,9 +431,9 @@
                         @endphp
                         <div class="col-sm-6">
                             <div class="advert-img">
-                                <img src="https://s3.eu-central-1.amazonaws.com/web.eu-central-1.sumra.net/{{$products['images'][0]}}?1500586448">
+                                <img src="https://s3.eu-central-1.amazonaws.com/web.eu-central-1.sumra.net/{{$product['images'][0]}}?1500586448">
                             </div>
-                            <h4>{{$product['title']}}</h4>
+                            <h4>{{$product['price']}}</h4>
                         </div>
                     @endforeach
                 </div>
