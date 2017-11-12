b0b41a306d774538da2c3c292b1df339182f9224
anil <anil@sumra.net>
2017-11-12 19:36:53 +0000

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -457,15 +457,9 @@ class MarketController extends BaseController
         return $this->categories[$id];
     }
     public function categories(Request $request){
-        $base = Category::where('parent_id',0)->get();
-        $categories = Category::all();
-        $maps=array();
-        foreach ($categories as $category){
-            $category->children=$category->children;
-            $maps[$category->id]=$category;
-        }
 
-        return ['base'=>$base,'categories'=>$maps];
+
+        return Category::all();
 
     }
     public function locations(Request $request){
