80e208441f98f5bf417c4076646f0f23f6c7c62e
Mauricio <mauri@sumra.net>
2017-11-12 17:15:22 +0000

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -886,7 +886,6 @@ class MarketController extends BaseController
             $advert= Advert::where('sid',$sid)->first();
 
         }
-        var_dump($advert->user->address());
         $similar = $advert->similar();
         $similarUnder =  $advert->similarUnderPrice();
         $params = [
