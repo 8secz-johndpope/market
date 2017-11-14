f86e794dceaf3f7932a60ee3ae9ed16dfba79a6f
Mauricio <mauri@sumra.net>
2017-11-14 21:19:36 +0000

    auto

--- resources/views/market/property.blade.php
+++ resources/views/market/property.blade.php
@@ -1299,18 +1299,7 @@
     $('.input-elem-textbox input, .input-elem-textbox textarea').focusout(function(){
         $(this).prev().css('visibility','hidden');
     });
-    /*$('.input-elem-textbox input').keypress(function(){
-        var value = $(this).val();
-        var lessCount = 150 - value.length;
-        $(this).prev().text(lessCount);
-    });*/
-    $('.input-elem-textbox textarea').keypress(function(){
-        var value = $(this).val();
-        var lessCount = 5000 - value.length;
-        $(this).prev().text(lessCount);
-    });
     $('.input-elem-textbox input').keydown(function(e){
-            console.log("delete");
             var value = $(this).val();
             var lessCount = 150 - value.length;
             $(this).prev().text(lessCount);
