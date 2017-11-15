006bc711fd7d15a9767635c805b9f33cf61f89ea
Mauricio <mauri@sumra.net>
2017-11-15 14:34:24 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -12,10 +12,41 @@
 
 <div class="container">
 	<div class="row">
+		<div class="col-sm-12 banner-agency">
+			<img src="" class="img-banner">
+		</div>
+	</div> 
+	<div class="row">
+		<!-- div info-content -->
 		<div class="col-md-8 col-sm-12">
+			<div class="row">
+				<div class="col-sm-12">
+					<ul class="nav nav-tabs">
+                        <li class="active"><a data-toggle="tab" href="#tab-overview">Overview</a></li>
+                        <li><a data-toggle="tab" href="#tab-about">About Us</a></li>
+                        <li><a data-toggle="tab" href="#tab-branch-loc">Branch location</a></li>
+                        <li><a data-toggle="tab" href="#tab-contact">Contact Us</a></li>
+                    </ul>
+                    <div class="tabs-content">
+                    	<div class="tabs-content" id="tab-overview">
+                    	</div>
+                    	<div class="tabs-content" id="tab-about">
+                    	</div>
+                    	<div class="tabs-content" id="tab-branch-loc">
+                    	</div>
+                    	<div class="tabs-content" id="tab-contact">
+                    	</div>
+                    </div>
+
+				</div>
+			</div>
 		</div>
+		<!-- end info-content -->
+		<!-- div col-agency-contact -->
 		<div class="col-md-4 col-sm-12">
+
 		</div>
+		<!-- end col-agency-contact -->
 	</div>
 </div>
 @endsection
