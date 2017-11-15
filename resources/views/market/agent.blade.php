096c44c90f6a894c422d0c6999429236cce09d2a
Mauricio <mauri@sumra.net>
2017-11-15 14:49:56 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -27,19 +27,19 @@
                         <li><a data-toggle="tab" href="#tab-branch-loc">Branch location</a></li>
                         <li><a data-toggle="tab" href="#tab-contact">Contact Us</a></li>
                     </ul>
-                    <div class="tabs-content">
-                    	<div class="tabs-content" id="tab-overview">
+                    <div class="tab-content">
+                    	<div id="tab-overview">
                     		<div class="text">
 	                    		<p>
 	                    		With over 140 years experience in selling and letting property, Hamptons International has a network of over 85 branches across the country and internationally, marketing a huge variety of properties from compact flats to grand country estates. We’re national estate agents, with local offices. We know our local areas as well as any local agent. But our network means we can market your property to a much greater number of the right sort of buyers or tenants. And selling and letting property is not all we do. If you want to find out more about any of our additional services including Property Finance, Property Management, International Sales and Investments or Residential Developments and Investments, give us a call. Whether you are buying or renting, have a property to sell or let, need property finance, or management services, the Hamptons International brand is one you can trust.
 	                    		</p>
                     		</div>
                     	</div>
-                    	<div class="tabs-content" id="tab-about">
+                    	<div id="tab-about">
                     	</div>
-                    	<div class="tabs-content" id="tab-branch-loc">
+                    	<div id="tab-branch-loc">
                     	</div>
-                    	<div class="tabs-content" id="tab-contact">
+                    	<div id="tab-contact">
                     	</div>
                     </div>
 
