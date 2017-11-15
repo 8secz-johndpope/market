2940412916dc55881f1d27f49a760de112fafb66
Mauricio <mauri@sumra.net>
2017-11-15 14:46:31 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -29,9 +29,11 @@
                     </ul>
                     <div class="tabs-content">
                     	<div class="tabs-content" id="tab-overview">
-                    		<p>
-                    		With over 140 years experience in selling and letting property, Hamptons International has a network of over 85 branches across the country and internationally, marketing a huge variety of properties from compact flats to grand country estates. We’re national estate agents, with local offices. We know our local areas as well as any local agent. But our network means we can market your property to a much greater number of the right sort of buyers or tenants. And selling and letting property is not all we do. If you want to find out more about any of our additional services including Property Finance, Property Management, International Sales and Investments or Residential Developments and Investments, give us a call. Whether you are buying or renting, have a property to sell or let, need property finance, or management services, the Hamptons International brand is one you can trust.
-                    		</p>
+                    		<div class="text">
+	                    		<p>
+	                    		With over 140 years experience in selling and letting property, Hamptons International has a network of over 85 branches across the country and internationally, marketing a huge variety of properties from compact flats to grand country estates. We’re national estate agents, with local offices. We know our local areas as well as any local agent. But our network means we can market your property to a much greater number of the right sort of buyers or tenants. And selling and letting property is not all we do. If you want to find out more about any of our additional services including Property Finance, Property Management, International Sales and Investments or Residential Developments and Investments, give us a call. Whether you are buying or renting, have a property to sell or let, need property finance, or management services, the Hamptons International brand is one you can trust.
+	                    		</p>
+                    		</div>
                     	</div>
                     	<div class="tabs-content" id="tab-about">
                     	</div>
