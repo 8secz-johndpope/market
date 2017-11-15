7425d10adc8b4494089544f98709902cb0bc5905
Mauricio <mauri@sumra.net>
2017-11-15 14:59:36 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -34,6 +34,30 @@
 	                    		With over 140 years experience in selling and letting property, Hamptons International has a network of over 85 branches across the country and internationally, marketing a huge variety of properties from compact flats to grand country estates. We’re national estate agents, with local offices. We know our local areas as well as any local agent. But our network means we can market your property to a much greater number of the right sort of buyers or tenants. And selling and letting property is not all we do. If you want to find out more about any of our additional services including Property Finance, Property Management, International Sales and Investments or Residential Developments and Investments, give us a call. Whether you are buying or renting, have a property to sell or let, need property finance, or management services, the Hamptons International brand is one you can trust.
 	                    		</p>
                     		</div>
+                    		<div>
+                    			<table class="averages">
+                    				<tbody>
+                    					<tr>
+                    						<th>Properties</th>
+                    						<th>Number of properties</th>
+                    						<th>Avg. asking price</th>
+                    						<th>Avg. listing age</th>
+                    					</tr>
+                    					<tr>
+                    						<td><a>Residential to rent</a></td>
+                    						<td>6</td>
+                    						<td>1,010</td>
+                    						<td>6 weeks</td>
+                    					</tr>
+                    					<tr>
+                    						<td><a>Residential to sale</a></td>
+                    						<td>6</td>
+                    						<td>1,010</td>
+                    						<td>6 weeks</td>
+                    					</tr>
+                    				</tbody>
+                    			</table>
+                    		</div>
                     	</div>
                     	<div id="tab-about" class="tab-pane fade">
                     	</div>
