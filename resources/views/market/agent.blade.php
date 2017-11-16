f88f1e478f3e40b2bb8059c69fd82872ea331aa1
Mauricio <mauri@sumra.net>
2017-11-16 15:23:16 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -114,13 +114,14 @@
                     						<textarea id="address" name="address" rows="3"></textarea>
                     					</div>
                     					<div class="inputset large-validation is-valid">
-                    						<label for="email">
-                    							Email: <span class="required">*</span>
+                    						<label for="postcode">
+                    							Postcode: <span class="required">*</span>
                     						</label>
-                    						<input id="email" type="email" name="email" title="Email" placeholder="">
-                    						<div id="branch-enquiry-email-error" class="validation-failed">
-                								Please enter your email address.
-                							</div>
+                    						<input id="postcode" type="postcode" name="postcode" title="Postcode" placeholder="">
+                    					</div>
+                    					<div class="inputset large-validation is-valid">
+                    						<label class="contact-label">Your message: </label>
+                    						<textarea id="comment" name="comment" rows="3"></textarea>
                     					</div>
 
                     				</fieldset>
