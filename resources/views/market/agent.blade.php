ca2695cbe5858908c99ab43d4f0315701f606b2f
Mauricio <mauri@sumra.net>
2017-11-16 18:12:40 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -35,9 +35,15 @@
                     <div class="tab-content">
                     	<div id="tab-overview" class="tab-pane fade in active">
                     		<div class="content-text">
+                    			@if(isset($user->business))
+                    			<p>
+                    				{{$user->business->description}}
+                    			</p>
+                    			@else
 	                    		<p>
 	                    		With over 140 years experience in selling and letting property, Hamptons International has a network of over 85 branches across the country and internationally, marketing a huge variety of properties from compact flats to grand country estates. We’re national estate agents, with local offices. We know our local areas as well as any local agent. But our network means we can market your property to a much greater number of the right sort of buyers or tenants. And selling and letting property is not all we do. If you want to find out more about any of our additional services including Property Finance, Property Management, International Sales and Investments or Residential Developments and Investments, give us a call. Whether you are buying or renting, have a property to sell or let, need property finance, or management services, the Hamptons International brand is one you can trust.
 	                    		</p>
+	                    		@endif
                     		</div>
                     		<div>
                     			<table class="averages">
@@ -69,6 +75,7 @@
                     		</div>
                     	</div>
                     	<div id="tab-about" class="tab-pane fade">
+                    		<h3>Meet the team</h3>
                     	</div>
                     	<div id="tab-branch-loc" class="tab-pane fade">
                     		<h3>Agent's branch</h3>
