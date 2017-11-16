b9d17ba242d78e8868fe6bfa5cda5c0e08da2bb7
Mauricio <mauri@sumra.net>
2017-11-16 21:32:49 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -329,17 +329,6 @@
 	}
 	function StreeView(controlDiv, map) {
 
-	  // Set CSS for the control border.
-	  var controlUI = document.createElement('div');
-	  controlUI.style.backgroundColor = '#fff';
-	  controlUI.style.border = '2px solid #fff';
-	  controlUI.style.borderRadius = '3px';
-	  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-	  controlUI.style.cursor = 'pointer';
-	  controlUI.style.marginBottom = '22px';
-	  controlUI.style.textAlign = 'center';
-	  controlUI.title = 'Click to view StreetView';
-	  controlDiv.appendChild(controlUI);
 
 	  // Set CSS for the control interior.
 	  var controlText = document.createElement('div');
