7fdbc5b21d96297f24214d2f21ba42130eac3bb7
Mauricio <mauri@sumra.net>
2017-11-16 21:25:05 +0000

    auto

--- resources/views/market/agent.blade.php
+++ resources/views/market/agent.blade.php
@@ -304,6 +304,7 @@
 <script>
 
 	var map;
+	var panorama;
 	function initMap() {
 	    var uluru = {lat: {!! $postcode->lat !!}, lng: {!! $postcode->lng !!}};
 	     map = new google.maps.Map(document.getElementById('map'), {
@@ -315,10 +316,47 @@
 			}
  
 	    });
+	    panorama = map.getStreetView();
 	    var marker = new google.maps.Marker({
 	        position: uluru,
 	        map: map
 	    });
+	    var streeViewDiv = document.createElement('div');
+  		var streeView = new StreeView(centerControlDiv, map);
+
+  		streeViewDiv.index = 1;
+  		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(streeView);
+	}
+	function StreeView(controlDiv, map) {
+
+	  // Set CSS for the control border.
+	  var controlUI = document.createElement('div');
+	  controlUI.style.backgroundColor = '#fff';
+	  controlUI.style.border = '2px solid #fff';
+	  controlUI.style.borderRadius = '3px';
+	  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+	  controlUI.style.cursor = 'pointer';
+	  controlUI.style.marginBottom = '22px';
+	  controlUI.style.textAlign = 'center';
+	  controlUI.title = 'Click to view StreetView';
+	  controlDiv.appendChild(controlUI);
+
+	  // Set CSS for the control interior.
+	  var controlText = document.createElement('div');
+	  controlText.style.color = 'rgb(25,25,25)';
+	  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+	  controlText.style.fontSize = '16px';
+	  controlText.style.lineHeight = '38px';
+	  controlText.style.paddingLeft = '5px';
+	  controlText.style.paddingRight = '5px';
+	  controlText.innerHTML = 'StreetView';
+	  controlUI.appendChild(controlText);
+
+	  // Setup the click event listeners: simply set the map to Chicago.
+	  controlUI.addEventListener('click', function() {
+	    panorama.setVisible(true);
+	  });
+
 	}
 	$(document).ready(function() {
 	    initMap();
