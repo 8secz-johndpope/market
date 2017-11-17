41cfda3ecb78eccadb780a3698c42041e0bfce45
anil <anil@sumra.net>
2017-11-17 19:19:33 +0000

    auto

--- routes/web.php
+++ routes/web.php
@@ -207,9 +207,9 @@ Route::get('/notfound', 'MarketController@notfound');
 
 Route::get('/', 'MarketController@index');
 Route::get('/user/leaves','MarketController@leaves');
+Route::get('/p/r/{cat}/{id}', 'HomeController@product_url');
 
 Route::get('/p/{cat}/{id}', 'MarketController@product');
-Route::get('/p/r/{cat}/{id}', 'HomeController@product_url');
 
 Route::get('/{any}', 'MarketController@search');
 Route::get('/{any}/{loc}', 'MarketController@lsearch');
