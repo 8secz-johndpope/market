1711c423b8894634ba5b6fcfe0cf578e09b602dc
Mauricio <mauri@sumra.net>
2017-11-20 15:16:38 +0000

    auto

--- resources/views/market/job.blade.php
+++ resources/views/market/job.blade.php
@@ -375,8 +375,6 @@
                         <small>Remember: You should never send cash or cheques to a prospective employer, or provide your bank details or any other financial information. We pay great attention to vetting all jobs that appear on our site, but please get in touch if you see any roles asking for such payments or financial details from you. For more information on conducting a safe job hunt online, visit safer-jobs.</small>
                     </p>
                 </div>
-
-            <!-- fin -->
             </div>
         </div>
     </div>
