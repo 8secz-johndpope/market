b208a8a3c2c1f445266ba9998356b329cbf40b75
Mauricio <mauri@sumra.net>
2017-11-21 15:40:20 +0000

    auto

--- resources/views/market/job.blade.php
+++ resources/views/market/job.blade.php
@@ -428,6 +428,7 @@
                     <a class="btn btn-default">VideoCall</a>
                     <a class="btn btn-default">Email</a>
                 </div>
+                @endif
                 <div class="col-md-12 safe-jobs">
                     <p>
                         <small>Remember: You should never send cash or cheques to a prospective employer, or provide your bank details or any other financial information. We pay great attention to vetting all jobs that appear on our site, but please get in touch if you see any roles asking for such payments or financial details from you. For more information on conducting a safe job hunt online, visit safer-jobs.</small>
