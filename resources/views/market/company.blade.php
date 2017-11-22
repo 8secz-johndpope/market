86b2cfd5c897b8fe452f56e8dd82c2325ffece66
Mauricio <mauri@sumra.net>
2017-11-22 12:24:30 +0000

    auto

--- resources/views/market/company.blade.php
+++ resources/views/market/company.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
- <link href="{{ asset('/css/agent.css?q=874') }}" rel="stylesheet">
+ <link href="{{asset('/css/agent.css?q=874')}} | {{ isset($user->business) ? $user->business->name : 'Company'}}" rel="stylesheet">
 <div class="container">
 	<div class="row">
 		 <div class="col-md-8 col-sm-12">
