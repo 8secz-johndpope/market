b5ff5b6a950b5143f021053e67cc22dbb1eba916
Mauricio <mauri@sumra.net>
2017-11-22 17:49:09 +0000

    auto

---
+++ resources/views/market/profilecv.blade.php
@@ -0,0 +1,37 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.app')
+
+@section('title', 'Josh E. |' . env('APP_NAME'))
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+        </div>
+        <div class="col-md-4 col-sm-12">
+            <div class="row border-outside">
+                <div class="col-sm-12 details-agent title">
+                    <h3>Contact and Personal Info</h3>
+                </div>
+                <div class="col-sm-12 details-agent">
+                    <div class="personal-details">
+                        <p>
+                            David H.
+                        </p>
+                        <address>
+                            Blenheim, New Zealand
+                        </address>
+                        <p><strong>Tel: </strong>0788998878</p>
+                        <a href="#" class="btn btn-default">Email</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
