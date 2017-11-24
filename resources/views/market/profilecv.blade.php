e72471c8e7a870ade65cd788f46e74b784b9d6ee
Mauricio <mauri@sumra.net>
2017-11-24 18:11:21 +0000

    auto

--- resources/views/market/profilecv.blade.php
+++ resources/views/market/profilecv.blade.php
@@ -111,45 +111,23 @@
                                 </div>
                                 <div id="tab-cover" class="tabcontent">
                                     <p>
-                                    Your Name
-                                    Your Address
-                                    Your City, State, Zip Code
-                                    Your Phone Number
-                                    Your Email
-
-                                    Date
-
-                                    Name
-                                    Title
-                                    Organization
-                                    Address
-                                    City, State, Zip Code
-
-                                    Dear Mr./Ms. Last Name:
-
                                     I am interested in the Coordinator position advertised on XYZ. My resume is enclosed for your review. Given my related experience and excellent capabilities, I would appreciate your consideration for this job opening. My skills are an ideal match for this position.
-
+                                    <br><br>
                                     Your Requirements:
-
+                                    <br><br>
                                     Responsible for evening operations in Student Center and other facilities, including managing registration, solving customer problems, dealing with risk management and emergencies, enforcement of department policies.
-                                    Assists with hiring, training, and management of staff. Coordinate statistics and inventory.
-                                    Experience in the supervision of student staff and strong interpersonal skills are also preferred.
-                                    Valid Minnesota driver's license with good driving record. Ability to travel to different sites required.
+                                    Assists with hiring, training, and management of staff. Coordinate statistics and inventory.<br>
+                                    Experience in the supervision of student staff and strong interpersonal skills are also preferred.<br>
+                                    Valid Minnesota driver's license with good driving record. Ability to travel to different sites required.<br>
                                     Experience in collegiate programming and management.
+                                    <br><br>
                                     My Qualifications:
-
-                                    Register students for courses, design and manage program software, solve customer problems, enforce department policies, and serve as a contact for students, faculty, and staff.
-                                    Hiring, training, scheduling and management of staff, managing supply inventory, and ordering.
-                                    Minnesota driver's license with NTSA defensive driving certification.
-                                    Extensive experience in collegiate programming and management.
-                                    Excellent interpersonal and communication skills.
-                                    I appreciate your taking the time to review my credentials and experience. Again, thank you for your consideration.
-
-                                    Sincerely,
-
-                                    Your Signature (for hard copy letter)
-
-                                    Your Typed Name
+                                    <br><br>
+                                    Register students for courses, design and manage program software, solve customer problems, enforce department policies, and serve as a contact for students, faculty, and staff.<br>
+                                    Hiring, training, scheduling and management of staff, managing supply inventory, and ordering.<br>
+                                    Minnesota driver's license with NTSA defensive driving certification.<br>
+                                    Extensive experience in collegiate programming and management.<br>
+                                    Excellent interpersonal and communication skills.<br>
                                     </p>
                                 </div>
                                 <div id="tab-charge" class="tabcontent">
