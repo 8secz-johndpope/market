b77efe24ebba1fb3d7ad1de56fbe1992905ee971
Mauricio <mauri@sumra.net>
2017-11-27 13:58:21 +0000

    auto

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -235,25 +235,25 @@
                     <span class="posted-text">{{$product['posted']}}</span>
                 @endif
             </div>
-            <div class="extra-info">
-                @if(isset($product['urgent'])&&$product['urgent']===1&&$product['urgent_expires']>$milli)
-                    <span class="urgent-span">
-                        Urgent
-                    </span>
-                @endif
-                @if(isset($product['canship'])&&$product['canship']===1)
-                    <span class="ribbon-shipping">
-                        <strong class="ship-ribbon" data-q="featuredProduct"><span class="hide-visually">This ad is</span>Can Ship</strong>
-                    </span>
-                @endif
-                @if(isset($product['candeliver'])&&$product['candeliver']===1)
-                    <span class="ribbon-delivery">
-                        <strong class="deliver-ribbon" data-q="featuredProduct"><span class="hide-visually">This ad is</span>Local Delivery</strong>
-                    </span>
-                @endif
-            </div>
         </div>
-</div>
+        <div class="extra-info">
+            @if(isset($product['urgent'])&&$product['urgent']===1&&$product['urgent_expires']>$milli)
+                <span class="urgent-span">
+                    Urgent
+                </span>
+            @endif
+            @if(isset($product['canship'])&&$product['canship']===1)
+                <span class="ribbon-shipping">
+                    <strong class="ship-ribbon" data-q="featuredProduct"><span class="hide-visually">This ad is</span>Can Ship</strong>
+                </span>
+            @endif
+            @if(isset($product['candeliver'])&&$product['candeliver']===1)
+                <span class="ribbon-delivery">
+                    <strong class="deliver-ribbon" data-q="featuredProduct"><span class="hide-visually">This ad is</span>Local Delivery</strong>
+                </span>
+            @endif
+        </div>
+    </div>
             @endforeach
 
 
