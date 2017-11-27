673edc145a9d43acede6da96eb383674b3db1709
Mauricio <mauri@sumra.net>
2017-11-27 17:44:49 +0000

    auto

--- resources/views/market/profilecv.blade.php
+++ resources/views/market/profilecv.blade.php
@@ -369,7 +369,7 @@
                                 <div id="tab-make-offer" class="tabcontent">
                                     <h3>Make me an offer</h3>
                                     <div class="offer-content">
-                                        <p>Charges: £25.00 /hr
+                                        <p><strong>Charges:</strong> £25.00 /hr
                                         <h4>Your offer:</h4>
                                         <form >
                                             <label for="price-input">
