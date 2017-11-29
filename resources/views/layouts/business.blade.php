6f173465a051ad7b418cbea0684e3ca910d7da1a
anil <anil@sumra.net>
2017-11-29 15:54:55 +0000

    auto

--- resources/views/layouts/business.blade.php
+++ resources/views/layouts/business.blade.php
@@ -16,6 +16,7 @@
 
     <link href="{{ asset('/css/extra.css?q=43') }}" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/1.0.0-alpha.2/classic/ckeditor.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
