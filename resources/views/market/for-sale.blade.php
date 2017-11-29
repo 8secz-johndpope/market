4d44a084b0b3f605b306972d230204877b610040
Mauricio <mauri@sumra.net>
2017-11-29 19:29:09 +0000

    auto

--- resources/views/market/for-sale.blade.php
+++ resources/views/market/for-sale.blade.php
@@ -1022,55 +1022,7 @@
     $('#upload-cv').change(function () {
         upload_cv();
     });
-    $('.carousel').carousel({
-      interval: false
-    })
-    $('.small-image>a').click(function () {
-        if(timer != null){
-            var element = $('.icon-before');
-            stopAnimationGallery(element);
-        }
-        var src = $(this).children().first().attr('src');
-        $('.selected').removeClass('selected');
-        $(this).parent().addClass('selected');
-        $('#image-active').attr('src', src);
-        var index = $(this).attr('data-index');
-        $('#image-active').attr('data-index', index);
-        $('.index').text(index);
-        var indexCarousel = $('.carousel-inner .item.active').index();
-        var firsElementCarousel = indexCarousel * 5; 
-        var lastElementCarousel = firsElementCarousel + 5;
-
-        if(lastElementCarousel < (index-1)){
-            $("#myCarousel").carousel("next");
-        }else if(firsElementCarousel > (index-1)){
-            $("#myCarousel").carousel("prev");
-        }
-    });
-    $('.prev>a').click(function () {
-        if(timer != null){
-            var element = $('.icon-before');
-            stopAnimationGallery(element);
-        }
-        var index = $('#image-active').attr('data-index');
-        if(index > 1){
-            var children = $('.carousel-inner .item').children();
-            $('.selected').removeClass('selected');
-            var child = children.eq(index - 2);
-            child.addClass('selected');
-            var prevImage = child.find('img').attr('src');
-            index -= 1
-            $('#image-active').attr('data-index', index);
-            $('#image-active').attr('src', prevImage);
-            $('.index').text(index);
-            var indexCarousel = $('.carousel-inner .item.active').index();
-            var firsElementCarousel = indexCarousel * 5; 
-            var lastElementCarousel = firsElementCarousel + 5;
-            if(firsElementCarousel > (index-1)){
-                $("#myCarousel").carousel("prev");
-            }
-        }
-    });
+    
     $('.next>a').click(function () {
         if(timer != null){
             var element = $('.icon-before');
@@ -1181,34 +1133,7 @@
         $(element).text('Start slideshow');
         console.log('stop animation');
     }
-    function changeImageGallery(cycle = true){
-        var index = parseInt($('#image-active').attr('data-index'));
-        var children = $('.carousel-inner .item').children();
-        var numImg = children.length
-        console.log("is actived timer");
-        if( cycle || index < numImg ){
-            $('.selected').removeClass('selected');
-            index = index %  (numImg);
-            console.log("current: " + index);
-            var child = children.eq(index);
-            child.addClass('selected');
-            var nextImage = child.find('img').attr('src');
-            index = index + 1;
-            if(cycle && index == 1){
-                $("#myCarousel").carousel("next");
-            }
-            console.log("next: " + index);
-            $('#image-active').attr('data-index', index);
-            $('#image-active').attr('src', nextImage);
-            $('.index').text(index);
-            var indexCarousel = $('.carousel-inner .item.active').index();
-            var firsElementCarousel = indexCarousel * 5; 
-            var lastElementCarousel = firsElementCarousel + 5;
-            if(lastElementCarousel < (index)){
-                $("#myCarousel").carousel("next");
-            }
-        }
-    }
+    
     function isUnderground(types){
         return types.indexOf('tube') != -1;
     }
