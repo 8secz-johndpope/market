2a8a18c12385c1bf636a4be5096a5405f845235d
Mauricio <mauri@sumra.net>
2017-11-30 21:24:22 +0000

    auto

--- resources/views/home/sale.blade.php
+++ resources/views/home/sale.blade.php
@@ -70,11 +70,11 @@
                         <label>
                             Local delivery
                         </label>
-                        <input type="radio" name="post-option" id="delivery">
+                        <input type="radio" name="post-option" id="del-address">
                         <label>
                             United Kingdom delivery
                         </label>
-                        <input type="radio" name="post-option" id="shipping">
+                        <input type="radio" name="post-option" id="ship-address">
                     </div>
                     <div id="ship-address">
                             <h4>Shipping Address</h4>
@@ -262,5 +262,10 @@
             $(".all-divs").show();
             $(".add-card-form").hide();
         });
+        $('input[type=radio][name=post-option]').change(function(){
+        if(this.id != "collect"){
+            console.log(this.id);
+        }
+    });
     </script>
 @endsection
