56b07a81df42c6804c53d2e6cafec6616e9a9227
anil <anil@sumra.net>
2017-11-30 15:03:04 +0000

    auto

--- resources/views/home/view_applications.blade.php
+++ resources/views/home/view_applications.blade.php
@@ -63,7 +63,7 @@
             <table class="table">
                 <tr><th>Name</th><th>Phone</th><th>Cover</th><th>CV</th><th>Profile</th><th>Reply</th></tr>
             @foreach($job->applications as $application)
-                <tr><td>{{$application->user->name}}</td><td>{{$application->user->phone}}</td><td>@if({$application->cover){{$application->cover->cover}} @else <span>No Cover</span> @endif</td> <td>              @if($application->cv)                      <a target="_blank" href="{{env('AWS_CV_IMAGE_URL')}}/{{$application->cv->file_name}}">View/Download</a> @else <span>No Cv</span> @endif</td><td><a href="/job/profile/view/{{$application->user_id}}">View Profile</a></td><td><a class="btn btn-primary" href="/user/areply/{{$application->id}}">Reply</a></td></tr>
+                <tr><td>{{$application->user->name}}</td><td>{{$application->user->phone}}</td><td>@if($application->cover){{$application->cover->cover}} @else <span>No Cover</span> @endif</td> <td>              @if($application->cv)                      <a target="_blank" href="{{env('AWS_CV_IMAGE_URL')}}/{{$application->cv->file_name}}">View/Download</a> @else <span>No Cv</span> @endif</td><td><a href="/job/profile/view/{{$application->user_id}}">View Profile</a></td><td><a class="btn btn-primary" href="/user/areply/{{$application->id}}">Reply</a></td></tr>
             @endforeach
             </table>
         </div>
