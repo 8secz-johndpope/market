030a70e2dbe5ce40531019c8c066696a9fdba8f0
anil <anil@sumra.net>
2017-11-30 18:37:06 +0000

    auto

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -30,7 +30,7 @@ class MarketController extends BaseController
     public function more(Request $request,$id){
     $category = Category::find($id);
     if(count($category->children)>0){
-        return view('market.more',['category'=>$category,'input'=>[]]);
+        return view('market.more',['category'=>$category,'input'=>[],'location'=>Location::find(0)]);
     }else{
         return redirect('/'+$category->slug);
     }
