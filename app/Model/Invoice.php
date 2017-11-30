c3dc6cd3d2d7b99323061a2282234c7268b26e2f
anil <anil@sumra.net>
2017-11-30 16:43:52 +0000

    auto

---
+++ app/Model/Invoice.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 30/11/2017
+ * Time: 16:41
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Invoice extends Model
+{
+
+}
---
+++ app/Model/InvoiceItem.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 30/11/2017
+ * Time: 16:43
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+
+class InvoiceItem extends Model
+{
+
+}
