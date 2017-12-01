8490fb70420627e686e7ebfa3bfa6db6b475d347
Mauricio <mauri@sumra.net>
2017-12-01 20:49:30 +0000

    auto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="body">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -70,6 +71,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 <script>
     function onSubmit(token) {
