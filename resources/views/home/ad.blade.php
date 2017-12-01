f215b17bdb527df6ac0d0d498f2af10eb97bfd88
Mauricio <mauri@sumra.net>
2017-12-01 20:56:59 +0000

    auto

--- resources/views/home/ad.blade.php
+++ resources/views/home/ad.blade.php
@@ -17,7 +17,7 @@
         }
     </style>
 
-
+<div class="body">
     <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10  nopadding">
@@ -593,6 +593,7 @@
             </div>
         </div>
     </div>
+</div>
     <script>
         $(".change-shipping").change( function(){
 
