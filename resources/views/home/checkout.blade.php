237a7438b94b7fa59624fb510b819e344987e31e
Mauricio <mauri@sumra.net>
2017-12-04 19:24:46 +0000

    auto

--- resources/views/home/checkout.blade.php
+++ resources/views/home/checkout.blade.php
@@ -589,7 +589,8 @@
         });
         $(window).scroll(function(e) {
             if ($('body').scrollTop() > $('checkout-summry').position().top) {
-                $('checkout-summry').css('position', 'fixed');
+                $('.checkout-summry').css('position', 'fixed');
+                $('.checkout-summry').css('top', '0px');
             }
         });
 
