91fecd6906562141ada9f79421a5b46d2e22d64d
Mauricio <mauri@sumra.net>
2017-12-05 17:50:23 +0000

    auto

--- resources/views/home/checkout.blade.php
+++ resources/views/home/checkout.blade.php
@@ -466,7 +466,6 @@
             $('#cc-change-ctr').hide();
             $('label[for=saved-card]').text($('label[for='+ this.id +']').text());
             $('label[for=saved-card] li').attr('class', '');
-            $('label[for=saved-card] li').attr('class', '');
             $('label[for=saved-card] li').addClass($('label[for='+ this.id +'] .t-cc').attr('class'));  
         })
         $('#card-fields input').focus(function(){
