e2903533b0e47d18ec1dcb1e08d19a7684fec147
Mauricio <mauri@sumra.net>
2017-12-05 15:30:30 +0000

    auto

--- resources/views/home/checkout.blade.php
+++ resources/views/home/checkout.blade.php
@@ -411,11 +411,11 @@
          });
         $('input[type=radio][name=addrs-post-radio]').change(function(){
             var label = $('label[for="' + this.id + '"]');
-            var address = label.text().replace('span', 'div');
+            var address = label.text().replace("span", "div");
             $('#shipping-address-ctr .postage-address').toggleClass('expanded');
             $('.sa-edit-btn span').toggleClass('expanded');
             $('#shipping-address').hide();
-            $('.main-pa-inf div').text(address);
+            $('.main-pa-inf div:first-child').text(address);
             $('#sa-change-link').show();
         });
         $('#card-fields input').focus(function(){
