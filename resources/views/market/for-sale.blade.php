ed8ba524c3d536ac081c6d229fa042d5bdba7ffd
Mauricio <mauri@sumra.net>
2017-12-06 17:23:51 +0000

    auto

--- resources/views/market/for-sale.blade.php
+++ resources/views/market/for-sale.blade.php
@@ -1196,7 +1196,7 @@
                 console.log(data);
         }).fail(function( jqXHR, textStatus, errorThrown ) {
             if ( console && console.log ) {
-                console.log( "Error send request: " +  errorThrown);
+                console.log( "Error send request: " +  jqXHR.error);
             }
         });
     });
