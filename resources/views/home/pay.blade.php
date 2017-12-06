03846754da5cd98abbaa60d7aeba863bf0da1420
Mauricio <mauri@sumra.net>
2017-12-06 19:55:21 +0000

    auto

--- resources/views/home/pay.blade.php
+++ resources/views/home/pay.blade.php
@@ -96,7 +96,7 @@
                         </div>
                         <div class="col-sm-6"></div>
                         <div class="col-sm-6">
-                            <table class="table">
+                            <table class="w100p amount_total">
                                 <tbody>
                                     <tr>
                                         <td>Subtotal</td>
