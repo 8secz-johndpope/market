48e770e0b0460f9151a5fdb4f4479a15bae87c69
Mauricio <mauri@sumra.net>
2017-12-06 19:10:31 +0000

    auto

--- resources/views/home/pay.blade.php
+++ resources/views/home/pay.blade.php
@@ -25,22 +25,22 @@
                 <div class="col-sm-6">
                     <div class="seller-info">
                         <div>
-                            <h2 class="seller-name">{{$user->name}}</h2>
+                            <h2 class="seller-name">{{$seller->name}}</h2>
                         </div>
                         <div>
-                            {{$user->address->line1}}
+                            {{$seller->address->line1}}
                         </div>
                         <div>
-                            {{$user->address->city}} {{$user->address->postcode}}
+                            {{$seller->address->city}} {{$seller->address->postcode}}
                         </div>
                         <div>
                             United Kingdom
                         </div>
                         <div>
-                            {{$user->phone}}
+                            {{$seller->phone}}
                         </div>
                         <div>
-                            {{$user->email}}
+                            {{$seller->email}}
                         </div>
                     </div>
                 </div>
