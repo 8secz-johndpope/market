a0b09198f1b8a505bb906aafa027bc0e0f1d0dcf
Mauricio <mauri@sumra.net>
2017-12-09 20:14:31 +0000

    auto

--- resources/views/home/pay-logout.blade.php
+++ resources/views/home/pay-logout.blade.php
@@ -439,7 +439,7 @@
             $('.btn.btn-submit').prop('disabled', true);
             if($('button.fs-edit-btn').hasClass('expanded')){
                 $('button.fs-edit-btn').toggleClass('expanded');
-                $('#cc-new-ctr').css('display', 'none')
+                $('#cc-new-ctr').css('display', 'none');
             }
             $('#braintree-paypal-button').click();
         }
