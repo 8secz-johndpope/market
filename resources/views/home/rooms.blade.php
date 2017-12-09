5db1d7d753d83fb4ab6af2ea30e5624b85656446
Mauricio <mauri@sumra.net>
2017-12-09 14:46:07 +0000

    auto

--- resources/views/home/rooms.blade.php
+++ resources/views/home/rooms.blade.php
@@ -10,9 +10,24 @@
             </a>
         </div>
         <div class="media-body">
-            <a href="/user/manage/messages/{{$room->id}}"><h4 class="media-heading">{{$room->title}}</h4></a>
-            <p class="@if($room->unread===1) unread-message @endif">{{$room->last_message()->message}}</p>
-            <strong>{{$room->last_message()->user->name}}</strong>
+            <div class="chat-main">
+                <div class="chat-title">
+                    <a href="/user/manage/messages/{{$room->id}}">
+                        <h4 class="media-heading">{{$room->title}}</h4>
+                    </a>
+                </div>
+                <div class="chat-meta">
+                    <span class="message-time"> {{$room->last_message()->timestamp()}}</span>
+                </div>
+            </div>
+            <div class="chat-secondary">
+                <div class="chat-status"> 
+                    <p class="@if($room->unread===1) unread-message @endif">{{$room->last_message()->message}}</p>
+                </div>
+                <div class="chat-meta">
+                    <strong>{{$room->last_message()->user->name}}</strong>
+                </div>
+            </div>
         </div>
     </div>
 @endforeach
