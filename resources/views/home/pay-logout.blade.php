f18d8c2181e7027154de329f72c72a0e86b19422
Mauricio <mauri@sumra.net>
2017-12-11 18:00:21 +0000

    auto

--- resources/views/home/pay-logout.blade.php
+++ resources/views/home/pay-logout.blade.php
@@ -10,7 +10,7 @@
     <p>This is appended to the master sidebar.</p>
 @endsection
 @section('styles')
-<link href="{{ asset('/css/pay-invoice.css?q=874') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset("'/css/pay-invoice.css?q='. Datetime()->getTimestamp() .'874'") }}" rel="stylesheet" type="text/css">
 @endsection
 @section('content')
 <div class="body background-body">
