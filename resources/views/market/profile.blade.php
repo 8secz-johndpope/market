1831b2f5c3a7fe858f52340f252b0fbd14965503
Mauricio <mauri@sumra.net>
2017-12-11 18:16:00 +0000

    auto

--- resources/views/market/profile.blade.php
+++ resources/views/market/profile.blade.php
@@ -25,41 +25,43 @@
                 <div class="products">
                @foreach($user->adverts as $advert)
                <div class="listing-max-pro">
-                    <div class="listing-side">
-                        <div class="listing-thumbnail">
-                            <img src="{{env('AWS_WEB_IMAGE_URL')}}/{{ count($advert->param('images'))>0?$advert->param('images')[0]:"noimage.png"}}" class="lazyload" alt="">
+                    <div class="product">
+                        <div class="listing-side">
+                            <div class="listing-thumbnail">
+                                <img src="{{env('AWS_WEB_IMAGE_URL')}}/{{ count($advert->param('images'))>0?$advert->param('images')[0]:"noimage.png"}}" class="lazyload" alt="">
 
-                            @if($advert->featured_expires())
-                                <span class="ribbon-featured">
-                                    <strong class="ribbon" data-q="featuredProduct">
-                                        <span class="hide-visually">This ad is</span>
-                                        Featured
-                                    </strong>
-                                </span>
-                            @endif
+                                @if($advert->featured_expires())
+                                    <span class="ribbon-featured">
+                                        <strong class="ribbon" data-q="featuredProduct">
+                                            <span class="hide-visually">This ad is</span>
+                                            Featured
+                                        </strong>
+                                    </span>
+                                @endif
 
-                            <div class="listing-meta txt-sub">
-                                <span class="glyphicon glyphicon-camera"> </span> <span class="image-number"> {{count($advert->param('images'))}}</span>
+                                <div class="listing-meta txt-sub">
+                                    <span class="glyphicon glyphicon-camera"> </span> <span class="image-number"> {{count($advert->param('images'))}}</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="info">
-                        <a class="listing-product" href="/p/{{$advert->param('category')}}/{{$advert->sid}}"> <h4 class="product-title">{{$advert->param('title')}}</h4></a>
-                        <span class="listing-location">
-                                    {{$advert->param('location_name')}}
+                        <div class="info">
+                            <a class="listing-product" href="/p/{{$advert->param('category')}}/{{$advert->sid}}"> <h4 class="product-title">{{$advert->param('title')}}</h4></a>
+                            <span class="listing-location">
+                                        {{$advert->param('location_name')}}
+                                    </span>
+                            <p class="listing-description">
+                                {{$advert->param('description')}}
+                            </p>
+                            @if($advert->meta('price')>=0)
+                                <span class="product-price">£ {{$advert->meta('price')/100}}{{$advert->meta('price_frequency')}}
+                                    </span>
+                            @endif
+                            @if($advert->urgent_expires())
+                                <span class="clearfix txt-agnosticRed txt-uppercase" data-q="urgentProduct">
+                                    <span class="hide-visually">This ad is </span>Urgent
                                 </span>
-                        <p class="listing-description">
-                            {{$advert->param('description')}}
-                        </p>
-                        @if($advert->meta('price')>=0)
-                            <span class="product-price">£ {{$advert->meta('price')/100}}{{$advert->meta('price_frequency')}}
-                                </span>
-                        @endif
-                        @if($advert->urgent_expires())
-                            <span class="clearfix txt-agnosticRed txt-uppercase" data-q="urgentProduct">
-                                <span class="hide-visually">This ad is </span>Urgent
-                            </span>
-                        @endif
+                            @endif
+                        </div>
                     </div>
                 </div>
                @endforeach
