8719074c99e3c27fc8ba2fd32ad9063260439e3d
anil <anil@sumra.net>
2017-12-13 20:54:53 +0000

    auto

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -69,7 +69,7 @@ class MessageController extends BaseController
             return redirect('/user/manage/messages');
 
         }
-        $room->modify();
+       // $room->modify();
 
         if(!$user->access_token){
             $token = $user->createToken('Message Token')->accessToken;
