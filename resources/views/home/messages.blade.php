534b1c55483152d0965d20ea152ce125cf1e4906
Mauricio <mauri@sumra.net>
2017-12-15 16:12:47 +0000

    auto

--- resources/views/home/messages.blade.php
+++ resources/views/home/messages.blade.php
@@ -85,6 +85,13 @@
                                             </span>
                                         </div>
                                     </div>
+                                    <div class="controls-fields">
+                                        <div role="button">
+                                            <span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="#263238" fill-opacity=".6" d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"></path></svg>
+                                            </span>
+                                        </div>
+                                    </div>
                                 </span>
                             </div>
                         </div>
