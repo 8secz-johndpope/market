47607be4fb6129721e39a18366864fdb7d4e3edd
Mauricio <mauri@sumra.net>
2017-12-15 17:40:25 +0000

    auto

--- resources/views/home/messages.blade.php
+++ resources/views/home/messages.blade.php
@@ -148,13 +148,15 @@
                             </div>
                         </div>
                         <div class="chat-body">
-                            <div class="title-product">
-                                <div class="title-main">
-                                    <a class="listing-product" href="/p/{{$cur->advert->param('category')}}/{{$cur->advert->id}}"> <h4>{{$cur->advert->param('title')}}</h4></a>
-                                    @if($cur->advert->meta('price')>=0)
-                                        <span class="product-price">£ {{$cur->advert->meta('price')/100}}{{$cur->advert->meta('price_frequency')}}
-                                        </span>
-                                    @endif
+                            <div class="chat-main">
+                                <div class="title-product">
+                                    <div class="title-main">
+                                        <a class="listing-product" href="/p/{{$cur->advert->param('category')}}/{{$cur->advert->id}}"> <h4>{{$cur->advert->param('title')}}</h4></a>
+                                        @if($cur->advert->meta('price')>=0)
+                                            <span class="product-price">£ {{$cur->advert->meta('price')/100}}{{$cur->advert->meta('price_frequency')}}
+                                            </span>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
