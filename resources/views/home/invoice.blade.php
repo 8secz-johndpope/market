5f32da6fc9b31d3319307f3aefeb6a6f1cc2205d
Mauricio <mauri@sumra.net>
2017-12-19 19:50:30 +0000

    auto

--- resources/views/home/invoice.blade.php
+++ resources/views/home/invoice.blade.php
@@ -260,7 +260,7 @@
             }
         });
         $('.prices').focusout(function(){
-            var quantity = parseFloat($(this).parent().prev().find('.quantities').val);
+            var quantity = parseFloat($(this).parent().prev().find('.quantities').val());
             var price = parseFloat($(this).val()) * quantity;
             $(this).parent().next().find('.amount').text(price);
         });
