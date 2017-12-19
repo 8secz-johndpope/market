202e97d2884aa73e007b0db1d3938d3ee1159367
Mauricio <mauri@sumra.net>
2017-12-19 18:21:25 +0000

    auto

--- resources/views/home/invoice.blade.php
+++ resources/views/home/invoice.blade.php
@@ -16,49 +16,6 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-sm-12 over-hidden">
-
-            <ul class="nav nav-tabs top-main-nav">
-
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span> &nbsp;&nbsp; Manage  ads</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
-                </li>
-                @if($user->contract!==null)
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
-                    </li>
-                @endif
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/favorites"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="row">
         <form action="/room/invoice/save" method="post" id="change-category">
             <div class="col-sm-9">
                 <div class="container-invoice">
