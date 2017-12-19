5cc8c6b4cff9994532a919654cc50131a574ab7a
Mauricio <mauri@sumra.net>
2017-12-19 20:00:33 +0000

    auto

--- resources/views/home/invoice.blade.php
+++ resources/views/home/invoice.blade.php
@@ -261,7 +261,7 @@
         });
         $('.prices').focusout(function(){
             var quantity = parseFloat($(this).parent().prev().find('.quantities').val());
-            console.log(quantity);
+            console.log($(this).parent().prev().find('.quantities'));
             var price = parseFloat($(this).val()) * quantity;
             $(this).parent().next().find('.amount').text(price);
             console.log(price);
