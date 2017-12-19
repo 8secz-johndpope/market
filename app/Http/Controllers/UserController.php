8330c778662c1999b757709e5542786d39c7f8d6
anil <anil@sumra.net>
2017-12-19 19:22:44 +0000

    auto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -134,7 +134,7 @@ class UserController extends BaseController
 
         $numbers = $request->numbers;
         $nums = array_map(function ($phone){
-           return '132';
+           return str_replace(' ','',$phone);
         },$numbers);
         return $nums;
         foreach ($numbers as $number){
