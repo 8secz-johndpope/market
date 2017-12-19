17c498c2a36b04d1426f9957c389fa81f22604a3
Mauricio <mauri@sumra.net>
2017-12-19 18:02:28 +0000

    auto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -252,53 +252,121 @@
                             <li><a href="/register">Sign Up</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    Hello, {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Hello, {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu options-user" role="menu">
+                                <div class="list-menu-common">
+                                    <div class="title-list">
+                                        <span class="nav-link nav-color">Our Offers</span>
+                                    </div>
+                                    <ul>
+                                        <li>
+                                            <span class="nav-link nav-offer">We do not have offers at the moment</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="list-menu-common">
+                                    <div class="title-list">
+                                        <span class="nav-link nav-color">Your account</span>
+                                    </div>
+                                    <ul>
+                                        <li><a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;Manage My Ads</a> </li>
+                                        <li><a class="nav-link nav-color" href="/user/ad/create"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Post an Ad</a> </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span>&nbsp;&nbsp;Orders</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Messages</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;My Details</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/user/manage/favorites"><span class="glyphicon glyphicon-heart"></span>&nbsp;&nbsp;Favorites</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span>&nbsp;&nbsp;Search Alerts</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                               <span class="glyphicon glyphicon-log-out"></span>&nbsp;&nbsp; Logout
+                                            </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;Manage My Ads</a> </li>
-                                    <li>
-                                    <li><a href="/user/ad/create"><span class="glyphicon glyphicon-file"></span>&nbsp;&nbsp;Post and Ad</a> </li>
-                                    <li>
-                                        <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
-                                    </li>
-                                    <li>
-                                        <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
-                                    </li>
-                                    <li>
-                                        <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
-                                    </li>
-                                    @if( Auth::user()->contract!==null)
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="list-menu-common">
+                                    <div class="title-list">
+                                        <span class="nav-link nav-color">Invoices</span>
+                                    </div>
+                                    <ul>
                                         <li>
-                                            <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
+                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Send Invoice</a>
                                         </li>
                                         <li>
-                                            <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
+                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Unpaid Invoice</a>
                                         </li>
                                         <li>
-                                            <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
+                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Paid Invoice</a>
                                         </li>
-                                    @endif
-                                    <li>
-                                        <a class="nav-link nav-color" href="/user/manage/favorites"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
-                                    </li>
-                                    <li>
-                                        <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            <span class="glyphicon glyphicon-log-out"></span>&nbsp;&nbsp; Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Pending Invoices</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                @if(Auth::user()->contract!==null)
+                                <div class="list-menu-common">
+                                    <div class="title-list">
+                                        <span>Your business</span>
+                                    </div>
+                                    <ul>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Company</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp;Financials</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp;Metrics</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                @endif
+                                <div class="list-menu-common list-messages">
+                                    <div class="title-list">
+                                        <span>Your messages</span>
+                                    </div>
+                                    <ul>
+                                        @foreach(Auth::user()->rooms as $room)
+                                        <li class="list-group-item">
+                                            @if($room->last_message())
+                                                <a href="/user/manage/messages/{{$room->id}}">
+                                                    <div class="message-inside">
+                                                        <span class="message-username">{{$room->last_message()->user->name}}</span>
+                                                        <span class="title-advert">{{$room->title}}</span>
+                                                        <p class="@if($room->unread===1) unread-message @endif">{{$room->last_message()->message}}</p>
+                                                        
+                                                    </div>
+                                                </a>
+                                            @endif
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
 
                         @endif
                         @if (!Auth::guest())
