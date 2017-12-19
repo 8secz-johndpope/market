0605b7ce5d9d1e0eca8bb9a11529602ca54c4ec0
Mauricio <mauri@sumra.net>
2017-12-19 22:14:01 +0000

    auto

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="{{ asset('/css/base.css?q=432') }}" rel="stylesheet">
     <link href="{{ asset('/css/extra.css?q=874') }}" rel="stylesheet">
-    <link href="{{ asset('/css/css/font-awesome.min?q=874') }}" rel="stylesheet">
+    <link href="{{ asset('/css/css/font-awesome.min.css?q=874') }}" rel="stylesheet">
     
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 
