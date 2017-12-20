d116c312102ce8bf5e4afefc89fd192df3bcbb3f
Mauricio <mauri@sumra.net>
2017-12-20 19:46:35 +0000

    auto

--- resources/views/home/applications.blade.php
+++ resources/views/home/applications.blade.php
@@ -43,7 +43,7 @@
                                                     @foreach($jobs as $job)
                                                     <li class="list-group-item">
                                                         <div class="container-job-title">
-                                                            <p>{{$job->param('title')}} - {{$job->param('location_name')}}</p>
+                                                            <p>{{$job->param('title')}} - <span class="job-location">{{$job->param('location_name')}}</span></p>
                                                             <p>{{count($job->applications)}} Unread Candidates</p>
                                                         </div>
                                                     </li>
