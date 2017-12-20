a62aa4dd1475b99e162799688b7bf7cfe3293305
Mauricio <mauri@sumra.net>
2017-12-20 19:38:32 +0000

    auto

--- resources/views/home/applications.blade.php
+++ resources/views/home/applications.blade.php
@@ -168,7 +168,7 @@
                                                     <td>{{$job->param('title')}}</td>
                                                     <td>{{$application->user->name}}</td>
                                                     <td>{{$application->user->phone}}</td>
-                                                    <td></td>
+                                                    <td>{{$application->create_at}}</td>
                                                     <td></td>
                                                     <td>@if($application->cover){{$application->cover->cover}} @else <span>No Cover</span> @endif</td> 
                                                     <td>
