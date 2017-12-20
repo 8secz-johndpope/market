3f2701a966f119ced9ff1746de43c25c52202dd2
Mauricio <mauri@sumra.net>
2017-12-20 15:39:29 +0000

    auto

--- resources/views/home/applications.blade.php
+++ resources/views/home/applications.blade.php
@@ -11,60 +11,20 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
+<div class="body background-body">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
 
-            <ul class="nav nav-tabs">
-
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span> &nbsp;&nbsp; Manage  ads</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
-                </li>
-                @if($user->contract!==null)
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
-                    </li>
-                @endif
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/favorites"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
-                </li>
-            </ul>
+            </div>
+            <div class="col-md-6">
+                @foreach($jobs as $job)
+                    <a href="{{$job->url()}}"><h4>{{$job->param('title')}}</h4></a>
+                    <br>
+                    <a href="/job/manage/applications/{{$job->id}}">{{count($job->applications)}} Applications</a>
+                    @endforeach
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-3">
-
-        </div>
-        <div class="col-md-6">
-            @foreach($jobs as $job)
-                <a href="{{$job->url()}}"><h4>{{$job->param('title')}}</h4></a>
-                <br>
-                <a href="/job/manage/applications/{{$job->id}}">{{count($job->applications)}} Applications</a>
-                @endforeach
-        </div>
-    </div>
-
+</div>
 @endsection
