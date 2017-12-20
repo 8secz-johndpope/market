ff1eae05e61694bfdedcedf30c087036e56e4db1
Mauricio <mauri@sumra.net>
2017-12-20 21:35:34 +0000

    auto

--- resources/views/home/view_applications.blade.php
+++ resources/views/home/view_applications.blade.php
@@ -21,10 +21,21 @@
             <div class="col-sm-12">
                 <a href="{{$job->url()}}"><h4>Applications for {{$job->param('title')}}</h4></a>
                 <table class="table table-striped table-hover">
-                    <tr><th>Name</th><th>Phone</th><th>Cover</th><th>CV</th><th>Profile</th><th>Reply</th></tr>
-                @foreach($job->applications as $application)
-                    <tr><td>{{$application->user->name}}</td><td>{{$application->user->phone}}</td><td>@if($application->cover){{$application->cover->cover}} @else <span>No Cover</span> @endif</td> <td>              @if($application->cv)                      <a target="_blank" href="{{env('AWS_CV_IMAGE_URL')}}/{{$application->cv->file_name}}">View/Download</a> @else <span>No Cv</span> @endif</td><td><a href="/job/profile/view/{{$application->user_id}}">View Profile</a></td><td><a class="btn btn-primary" href="/user/areply/{{$application->id}}">Reply</a></td></tr>
-                @endforeach
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Cover</th>
+                            <th>CV</th>
+                            <th>Profile</th>
+                            <th>Reply</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($job->applications as $application)
+                        <tr><td>{{$application->user->name}}</td><td>{{$application->user->phone}}</td><td>@if($application->cover){{$application->cover->cover}} @else <span>No Cover</span> @endif</td> <td>              @if($application->cv)                      <a target="_blank" href="{{env('AWS_CV_IMAGE_URL')}}/{{$application->cv->file_name}}">View/Download</a> @else <span>No Cv</span> @endif</td><td><a href="/job/profile/view/{{$application->user_id}}">View Profile</a></td><td><a class="btn btn-primary" href="/user/areply/{{$application->id}}">Reply</a></td></tr>
+                    @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
