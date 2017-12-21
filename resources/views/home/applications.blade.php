ddb27e95f1ee56cb259caf80683fd5997710b81d
Mauricio <mauri@sumra.net>
2017-12-21 16:24:05 +0000

    auto

--- resources/views/home/applications.blade.php
+++ resources/views/home/applications.blade.php
@@ -279,8 +279,10 @@
                                 </div>
                                 <div class="col-sm-8">
                                     <div class="row">
-                                        <div class="container-history">
-                                            
+                                        <div class="col-sm-12">
+                                            <div class="container-history">
+                                                
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
