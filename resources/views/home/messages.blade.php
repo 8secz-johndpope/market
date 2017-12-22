117531716c7cedbda1948688b30f086f62a24a89
Mauricio <mauri@sumra.net>
2017-12-22 11:36:35 +0000

    auto

--- resources/views/home/messages.blade.php
+++ resources/views/home/messages.blade.php
@@ -147,13 +147,13 @@
                                         </span>
                                     </div>
                                 </div>
-                                <div class="controls-fields">
-                                    <div role="button" data-role="Menu" class="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <div class="controls-fields dropdown">
+                                    <div role="button" data-role="Menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <span>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="#263238" fill-opacity=".6" d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"></path></svg>
                                         </span>
-                                        <div class="dropdown-menu">
-                                        </div>
+                                    </div>
+                                    <div class="dropdown-menu">
                                     </div>
                                 </div>
                                 
