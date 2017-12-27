19e67225e29da17dd3bc0c74a77fe2f2f2da1b98
Mauricio <mauri@sumra.net>
2017-12-27 00:03:24 +0000

    auto

--- resources/views/market/profile.blade.php
+++ resources/views/market/profile.blade.php
@@ -29,7 +29,7 @@
             <div class="col-sm-6">
                 <div class="container-img-advertiser">
                     <div class="img-advertiser">
-                        <img class="circle" src="{{env('AWS_WEB_IMAGE_URL')}}/{{$advertiser->image}}"> 
+                        <img class="circle image-advertiser" src="{{env('AWS_WEB_IMAGE_URL')}}/{{$advertiser->image}}"> 
                     </div>
                 </div>
             </div>
