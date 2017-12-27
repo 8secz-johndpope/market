ad576254d1c7926dfa6bdb8f2c5446c9250c8d44
Mauricio <mauri@sumra.net>
2017-12-27 18:35:04 +0000

    auto

--- resources/views/market/companies-template2.blade.php
+++ resources/views/market/companies-template2.blade.php
@@ -15,9 +15,14 @@
 		<div class="row">
 			<!-- div info-content -->
 			<div class="col-sm-8">
-				
+				<div class="title-block">
+					<h1>We are<br><div class="big-title">UBER</div></h1>
+				</div>
 			</div>
 			<div class="col-sm-4">
+				<div class="logo-block">
+					<a href="/companies/uber"><img src="https://assets.themuse.com/uploaded/companies/61/small_logo.png?v=None" alt="Uber Careers"></a>
+				</div>
 			</div>
 		</div>
 	</div>
