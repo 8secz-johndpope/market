3865f4221b9ce817a191d6d8b997448c76845486
Mauricio <mauri@sumra.net>
2017-12-27 22:25:02 +0000

    auto

--- resources/views/market/companies-template2.blade.php
+++ resources/views/market/companies-template2.blade.php
@@ -42,175 +42,177 @@
 				</div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-sm-8">
-				<a href="/companies/uber/offices">
-					<div class="grid_2_2">
-						<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff1.jpg%3Fv%3DNone&amp;h=960&amp;w=1300" class="share-base" alt="Uber Careers">
-						<div class="text-lower-right gradient"><h2>Our Office</h2></div>
+		<div class="company-wrapper">
+			<div class="row">
+				<div class="col-sm-8">
+					<a href="/companies/uber/offices">
+						<div class="grid_2_2">
+							<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff1.jpg%3Fv%3DNone&amp;h=960&amp;w=1300" class="share-base" alt="Uber Careers">
+							<div class="text-lower-right gradient"><h2>Our Office</h2></div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-4">
+					<div class="grid_1_1">
+						<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff2.jpg%3Fv%3DNone&amp;h=235&amp;w=320" alt="Uber Careers">
+						<div class="text-lower-right gradient">
+							<p>We work &amp; ride in San Francisco</p>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col-sm-4">
-				<div class="grid_1_1">
-					<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff2.jpg%3Fv%3DNone&amp;h=235&amp;w=320" alt="Uber Careers">
-					<div class="text-lower-right gradient">
-						<p>We work &amp; ride in San Francisco</p>
+				</div>
+				<div class="col-sm-4">
+					<div class="grid_1_1">
+						<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff3.jpg%3Fv%3DNone&amp;h=235&amp;w=320" alt="Uber Careers">
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-4">
-				<div class="grid_1_1">
-					<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff3.jpg%3Fv%3DNone&amp;h=235&amp;w=320" alt="Uber Careers">
+			<div class="row">
+				<div class="col-sm-4">
+					<a href="/companies/uber/people/brian&#10;&#10;" data-person-id="182" data-person-short-name="brian" data-person-position="0">
+						<div class="spotlight grid_1_2">
+							<img src="https://assets.themuse.com/uploaded/companies/61/people/182-small.jpg?v=None" alt="Brian McMullen, Community Management Specialist - Uber Careers">
+							<div class="spotlight-about">
+								<div class="spotlight-name">Brian McMullen</div>
+								<div class="spotlight-title">Community Management Specialist</div>
+								<div class="spotlight-clicktomeet">
+									Click to meet Brian 
+									<i class="fa fa-long-arrow-right"></i>
+								</div>
+							</div>
+						</div>
+					</a>
 				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-4">
-				<a href="/companies/uber/people/brian&#10;&#10;" data-person-id="182" data-person-short-name="brian" data-person-position="0">
-					<div class="spotlight grid_1_2">
-						<img src="https://assets.themuse.com/uploaded/companies/61/people/182-small.jpg?v=None" alt="Brian McMullen, Community Management Specialist - Uber Careers">
-						<div class="spotlight-about">
-							<div class="spotlight-name">Brian McMullen</div>
-							<div class="spotlight-title">Community Management Specialist</div>
+				<div class="col-sm-4">
+					<a href="/companies/uber/people/swathy&#10;&#10;" data-person-id="184" data-person-short-name="swathy" data-person-position="1">
+						<div class="spotlight grid_1_2">
+							<img src="https://assets.themuse.com/uploaded/companies/61/people/184-small.jpg?v=None" alt="Swathy Prithivi, Operations &amp; Logistics Manager - Uber Careers">
+							<div class="spotlight-about">
+							<div class="spotlight-name">Swathy Prithivi</div>
+							<div class="spotlight-title">Operations &amp; Logistics Manager</div>
 							<div class="spotlight-clicktomeet">
-								Click to meet Brian 
+								Click to meet Swathy 
 								<i class="fa fa-long-arrow-right"></i>
 							</div>
+							</div>
 						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-sm-4">
-				<a href="/companies/uber/people/swathy&#10;&#10;" data-person-id="184" data-person-short-name="swathy" data-person-position="1">
-					<div class="spotlight grid_1_2">
-						<img src="https://assets.themuse.com/uploaded/companies/61/people/184-small.jpg?v=None" alt="Swathy Prithivi, Operations &amp; Logistics Manager - Uber Careers">
-						<div class="spotlight-about">
-						<div class="spotlight-name">Swathy Prithivi</div>
-						<div class="spotlight-title">Operations &amp; Logistics Manager</div>
-						<div class="spotlight-clicktomeet">
-							Click to meet Swathy 
-							<i class="fa fa-long-arrow-right"></i>
-						</div>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-sm-4">
-				<a href="/companies/uber/people/jess&#10;&#10;" data-person-id="183" data-person-short-name="jess" data-person-position="2">
-					<div class="spotlight grid_1_2">
-						<img src="https://assets.themuse.com/uploaded/companies/61/people/183-small.jpg?v=None" alt="Jess Stanton, Software Engineer - Uber Careers">
-						<div class="spotlight-about">
-						<div class="spotlight-name">Jess Stanton</div>
-						<div class="spotlight-title">Software Engineer</div>
-						<div class="spotlight-clicktomeet">
-							Click to meet Jess 
-							<i class="fa fa-long-arrow-right"></i>
-						</div>
-						</div>
-					</div>
-				</a>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-4">
-				<div data-widget-id="2267" class="widget insight video grid_1_2 " data-muse-widget-name="video">
-					<iframe src="//player.vimeo.com/video/53834658?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="320" height="440" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+					</a>
 				</div>
-				<div data-widget-id="2268" class="widget insight grid_1 " data-muse-widget-name="insight">
-					<div class="insight-image">
-						<img src="https://assets.themuse.com/uploaded/companies/61/widgets/2268.jpg?v=None" alt="Careers - Office Life
-					 The White Unicorn">
-					</div>
-					<div class="insight-body">
-						<div class="insight-date">Office Life</div>
-						<div class="insight-title">The White Unicorn</div>
-						<div class="insight-text">For Uber fans, the “White Unicorn” has become the stuff of San Francisco legends. The elusive white car—the only one in Uber’s all-black fleet—accomplishes Herculean feats, having even whisked one woman off her feet to a surprise engagement party. When not busy playing matchmaker, though, the Unicorn and its faithful driver, Sofian, can be found deftly battling rush-hour traffic on San Francisco’s winding streets, getting Uber users to their destinations.</div>
-					</div>
-				</div>
-				<div data-widget-id="2269" class="widget  " data-muse-widget-name="help_wanted_dynamic">
-					<a href="/jobs?company=Uber" target="_blank">
-						<div class="help-wanted">
-							<div class="help-wanted-title">We're Hiring</div>
-							<div class="help-wanted-job">Check out open jobs at Uber</div>
-							<div class="help-wanted-location"></div>
-							<div class="help-wanted-footer"></div>
+				<div class="col-sm-4">
+					<a href="/companies/uber/people/jess&#10;&#10;" data-person-id="183" data-person-short-name="jess" data-person-position="2">
+						<div class="spotlight grid_1_2">
+							<img src="https://assets.themuse.com/uploaded/companies/61/people/183-small.jpg?v=None" alt="Jess Stanton, Software Engineer - Uber Careers">
+							<div class="spotlight-about">
+							<div class="spotlight-name">Jess Stanton</div>
+							<div class="spotlight-title">Software Engineer</div>
+							<div class="spotlight-clicktomeet">
+								Click to meet Jess 
+								<i class="fa fa-long-arrow-right"></i>
+							</div>
+							</div>
 						</div>
 					</a>
 				</div>
 			</div>
-			<div class="col-sm-4">
-				<div data-widget-id="2270" class="widget insight grid_1 " data-muse-widget-name="insight">
-					<div class="insight-image">
-						<img src="https://assets.themuse.com/uploaded/companies/61/widgets/2270.jpg?v=None" alt="Careers - What Uber Does Uber 101">
+			<div class="row">
+				<div class="col-sm-4">
+					<div data-widget-id="2267" class="widget insight video grid_1_2 " data-muse-widget-name="video">
+						<iframe src="//player.vimeo.com/video/53834658?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="320" height="440" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
 					</div>
-					<div class="insight-body">
-						<div class="insight-date">What Uber Does
+					<div data-widget-id="2268" class="widget insight grid_1 " data-muse-widget-name="insight">
+						<div class="insight-image">
+							<img src="https://assets.themuse.com/uploaded/companies/61/widgets/2268.jpg?v=None" alt="Careers - Office Life
+						 The White Unicorn">
 						</div>
-						<div class="insight-title">Uber 101
+						<div class="insight-body">
+							<div class="insight-date">Office Life</div>
+							<div class="insight-title">The White Unicorn</div>
+							<div class="insight-text">For Uber fans, the “White Unicorn” has become the stuff of San Francisco legends. The elusive white car—the only one in Uber’s all-black fleet—accomplishes Herculean feats, having even whisked one woman off her feet to a surprise engagement party. When not busy playing matchmaker, though, the Unicorn and its faithful driver, Sofian, can be found deftly battling rush-hour traffic on San Francisco’s winding streets, getting Uber users to their destinations.</div>
 						</div>
-						<div class="insight-text">Fasten your seat belts: Uber is an on-demand car service that’s shaking up the transportation systems in 20 cities (and growing) around the world. Customers use the Uber app, website, or text message service to connect with the nearest available driver. Customers can choose which kind of vehicle they want—from basic taxis to snazzy limousines to hybrid vehicles—and have their ride show up within minutes of their request.</div>
+					</div>
+					<div data-widget-id="2269" class="widget  " data-muse-widget-name="help_wanted_dynamic">
+						<a href="/jobs?company=Uber" target="_blank">
+							<div class="help-wanted">
+								<div class="help-wanted-title">We're Hiring</div>
+								<div class="help-wanted-job">Check out open jobs at Uber</div>
+								<div class="help-wanted-location"></div>
+								<div class="help-wanted-footer"></div>
+							</div>
+						</a>
 					</div>
 				</div>
-				<div data-widget-id="2271" class="widget insight video grid_1_2 " data-muse-widget-name="video">
-					<iframe src="//player.vimeo.com/video/53833676?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="320" height="440" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+				<div class="col-sm-4">
+					<div data-widget-id="2270" class="widget insight grid_1 " data-muse-widget-name="insight">
+						<div class="insight-image">
+							<img src="https://assets.themuse.com/uploaded/companies/61/widgets/2270.jpg?v=None" alt="Careers - What Uber Does Uber 101">
+						</div>
+						<div class="insight-body">
+							<div class="insight-date">What Uber Does
+							</div>
+							<div class="insight-title">Uber 101
+							</div>
+							<div class="insight-text">Fasten your seat belts: Uber is an on-demand car service that’s shaking up the transportation systems in 20 cities (and growing) around the world. Customers use the Uber app, website, or text message service to connect with the nearest available driver. Customers can choose which kind of vehicle they want—from basic taxis to snazzy limousines to hybrid vehicles—and have their ride show up within minutes of their request.</div>
+						</div>
+					</div>
+					<div data-widget-id="2271" class="widget insight video grid_1_2 " data-muse-widget-name="video">
+						<iframe src="//player.vimeo.com/video/53833676?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="320" height="440" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+					</div>
 				</div>
-			</div>
-			<div class="col-sm-4">
-				<div data-widget-id="2274" class="widget  widget-col-2" data-muse-widget-name="social_facebook">
-					<a href="https://www.facebook.com/uber" target="_blank">
-						<div class="social social-facebook grid_1">
-							<div class="social-container">
-								<div class="social-title">Uber on Facebook
+				<div class="col-sm-4">
+					<div data-widget-id="2274" class="widget  widget-col-2" data-muse-widget-name="social_facebook">
+						<a href="https://www.facebook.com/uber" target="_blank">
+							<div class="social social-facebook grid_1">
+								<div class="social-container">
+									<div class="social-title">Uber on Facebook
+									</div>
+									<div class="social-body">Choice is a beautiful thing! Now offering a SUV option in Dallas; your whole posse can show up together in style.</div>
+									<div class="social-footer"></div>
 								</div>
-								<div class="social-body">Choice is a beautiful thing! Now offering a SUV option in Dallas; your whole posse can show up together in style.</div>
-								<div class="social-footer"></div>
 							</div>
-						</div>
-					</a>
-				</div>
-				<div data-widget-id="2273" class="widget  widget-col-2" data-muse-widget-name="social_twitter">
-					<a href="https://twitter.com/uber" target="_blank">
-						<div class="social social-twitter grid_1">
-							<div class="social-container">
-								<div class="social-title">‏@Uber </div>
-								<div class="social-body">Go NYC! RT @Uber_NYC: Subways may be closed but we're up! We have some cars available at the moment, and see more coming online. </div>
-								<div class="social-footer"></div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div data-widget-id="2275" class="widget  widget-col-2" data-muse-widget-name="quote_green">
-					<div class="quote quote-green">
-						<div class="quote-green-top">&nbsp;</div>
-						<div class="quote-green-side">&nbsp;</div>
-						<div class="quote-body">
-							“The things that we do here really impact people’s day-to-day lives and we’re proud of that.”
-							<div class="quote-source">
-								<div class="quote-author">-Brian </div>
-								<div class="quote-author-position">Community Management Specialist</div>
-							</div>
-						</div>
-						<div class="quote-green-side">&nbsp;</div>
-						<img alt="“The things that we do here really impact people’s day-to-day lives and we’re proud of that.” " class="quote-bottom" src="https://www.themuse.com/static/images/quote_green_bottom.png?v=b075c27b5205eefaca5afe5c7dab5a4ddd9f8e2ec3d9489a6fe048d8856d0311">
+						</a>
 					</div>
-				</div>
-				<div data-widget-id="2276" class="widget  widget-col-2" data-muse-widget-name="quote_yellow">
-					<div class="quote quote-yellow">
-						<div class="quote-yellow-top">&nbsp;</div>
-						<div class="quote-yellow-side">&nbsp;</div>
-						<div class="quote-body">
-							“I downloaded Uber and it was a magical experience—you press a button, a town car rolls up, and there’s a amazing driver who opens the door for you with gum and candy inside!”
-							<div class="quote-source">
-								<div class="quote-author">-Swathy
+					<div data-widget-id="2273" class="widget  widget-col-2" data-muse-widget-name="social_twitter">
+						<a href="https://twitter.com/uber" target="_blank">
+							<div class="social social-twitter grid_1">
+								<div class="social-container">
+									<div class="social-title">‏@Uber </div>
+									<div class="social-body">Go NYC! RT @Uber_NYC: Subways may be closed but we're up! We have some cars available at the moment, and see more coming online. </div>
+									<div class="social-footer"></div>
 								</div>
-								<div class="quote-author-position">Operations &amp; Logistics Manager</div>
 							</div>
-						</div>
-						<div class="quote-yellow-side">&nbsp;</div>
-							<img alt="“I downloaded Uber and it was a magical experience—you press a button, a town car rolls up, and there’s a amazing driver who opens the door for you with gum and candy inside!”" class="quote-bottom" src="https://www.themuse.com/static/images/quote_yellow_bottom.png?v=5a8a4600d998f7e1d0553d8062fda1c17cb42d2914e76ac96acfb8e679b683e9">
+						</a>
+					</div>
+					<div data-widget-id="2275" class="widget  widget-col-2" data-muse-widget-name="quote_green">
+						<div class="quote quote-green">
+							<div class="quote-green-top">&nbsp;</div>
+							<div class="quote-green-side">&nbsp;</div>
+							<div class="quote-body">
+								“The things that we do here really impact people’s day-to-day lives and we’re proud of that.”
+								<div class="quote-source">
+									<div class="quote-author">-Brian </div>
+									<div class="quote-author-position">Community Management Specialist</div>
+								</div>
+							</div>
+							<div class="quote-green-side">&nbsp;</div>
+							<img alt="“The things that we do here really impact people’s day-to-day lives and we’re proud of that.” " class="quote-bottom" src="https://www.themuse.com/static/images/quote_green_bottom.png?v=b075c27b5205eefaca5afe5c7dab5a4ddd9f8e2ec3d9489a6fe048d8856d0311">
 						</div>
 					</div>
+					<div data-widget-id="2276" class="widget  widget-col-2" data-muse-widget-name="quote_yellow">
+						<div class="quote quote-yellow">
+							<div class="quote-yellow-top">&nbsp;</div>
+							<div class="quote-yellow-side">&nbsp;</div>
+							<div class="quote-body">
+								“I downloaded Uber and it was a magical experience—you press a button, a town car rolls up, and there’s a amazing driver who opens the door for you with gum and candy inside!”
+								<div class="quote-source">
+									<div class="quote-author">-Swathy
+									</div>
+									<div class="quote-author-position">Operations &amp; Logistics Manager</div>
+								</div>
+							</div>
+							<div class="quote-yellow-side">&nbsp;</div>
+								<img alt="“I downloaded Uber and it was a magical experience—you press a button, a town car rolls up, and there’s a amazing driver who opens the door for you with gum and candy inside!”" class="quote-bottom" src="https://www.themuse.com/static/images/quote_yellow_bottom.png?v=5a8a4600d998f7e1d0553d8062fda1c17cb42d2914e76ac96acfb8e679b683e9">
+							</div>
+						</div>
+				</div>
 			</div>
 		</div>
 	</div>
