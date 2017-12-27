5671f2f042f8edeef00b1f086da2b412e20f3d94
Mauricio <mauri@sumra.net>
2017-12-27 15:46:23 +0000

    auto

--- resources/views/market/make-contact.blade.php
+++ resources/views/market/make-contact.blade.php
@@ -11,42 +11,42 @@
 @section('content')
  <link href="{{ asset('/css/agent.css?q=874') }}" rel="stylesheet">
 <div class="body">
-<div class="container">
-	<div class="row">
-		 <div class="col-md-8 col-sm-12">
-		 	<div class="nav-back">
-		 	<a href="{{ url()->previous()}}"> < Go back</a>
-		 	</div>
-		 </div>
-	</div>
-	<div class="row container-info-advertiser">
-		<!-- div info-content -->
-		<div class="col-sm-3">
-			<div class="container-img-advertiser">
-                <div class="img-advertiser">
-                    <img class="circle image-advertiser" src="{{env('AWS_WEB_IMAGE_URL')}}/{{$user->image}}">
-                </div>
-            </div>
+	<div class="container">
+		<div class="row">
+			 <div class="col-md-8 col-sm-12">
+			 	<div class="nav-back">
+			 	<a href="{{ url()->previous()}}"> < Go back</a>
+			 	</div>
+			 </div>
 		</div>
-		<div class="col-sm-9">
+		<div class="row container-info-advertiser">
+			<!-- div info-content -->
+			<div class="col-sm-3">
+				<div class="container-img-advertiser">
+	                <div class="img-advertiser">
+	                    <img class="circle image-advertiser" src="{{env('AWS_WEB_IMAGE_URL')}}/{{$user->image}}">
+	                </div>
+	            </div>
+			</div>
+			<div class="col-sm-9">
+			</div>
+			<!-- end info-content -->
 		</div>
-		<!-- end info-content -->
-	</div>
-	<div class="row container-buttons">
-		<div class="col-sm-offset-2 col-sm-10">
-			<div class="row">
-				<div class="col-sm-4">
-					<a class="btn">Send Message</a>
-				</div>
-				<div class="col-sm-4">
-					<a class="btn">Call</a>
-				</div>
-				<div class="col-sm-4">
-					<a class="btn">VideoCall</a>
+		<div class="row container-buttons">
+			<div class="col-sm-offset-2 col-sm-10">
+				<div class="row">
+					<div class="col-sm-4">
+						<a class="btn">Send Message</a>
+					</div>
+					<div class="col-sm-4">
+						<a class="btn">Call</a>
+					</div>
+					<div class="col-sm-4">
+						<a class="btn">VideoCall</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </div>
 @endsection
