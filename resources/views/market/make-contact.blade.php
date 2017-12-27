8bf75f3d2462d38c514496ffbbfee3822b708364
Mauricio <mauri@sumra.net>
2017-12-27 15:36:05 +0000

    auto

--- resources/views/market/make-contact.blade.php
+++ resources/views/market/make-contact.blade.php
@@ -19,13 +19,11 @@
 		 	</div>
 		 </div>
 	</div>
-	<div class="row">
+	<div class="row container-info-advertiser">
 		<!-- div info-content -->
-		<div class="col-sm-12">
-			<div class="row">
-			</div>
-			<div class="row">
-			</div>
+		<div class="col-sm-3">
+		</div>
+		<div class="col-sm-9">
 		</div>
 		<!-- end info-content -->
 	</div>
