7e4854ae00bf2051fd3e1bb4f78239d447bcd10f
Mauricio <mauri@sumra.net>
2017-12-27 19:32:05 +0000

    auto

--- resources/views/market/companies-template2.blade.php
+++ resources/views/market/companies-template2.blade.php
@@ -111,6 +111,14 @@
 				</a>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-sm-4">
+			</div>
+			<div class="col-sm-4">
+			</div>
+			<div class="col-sm-4">
+			</div>
+		</div>
 	</div>
 </div>
 @endsection
