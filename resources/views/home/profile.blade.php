33625e1bf61a4fa961dc8af8b577228c56fb3492
anil <anil@sumra.net>
2017-12-28 19:45:40 +0000

    auto

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -58,7 +58,7 @@
         <div class="form-group row">
             <label for="staticEmail" class="col-sm-2 col-form-label">Display Name</label>
             <div class="col-sm-10">
-                <input type="text"  class="form-control" name="display_name">
+                <input type="text"  class="form-control" name="display_name" value="{{$user->display_name}}">
             </div>
         </div>
         <div class="row form-group">
