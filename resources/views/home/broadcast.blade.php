60e6c480ce08b85da394d46161a7fd51d3c65ec6
anil <anil@sumra.net>
2017-12-28 19:32:15 +0000

    auto

--- resources/views/home/broadcast.blade.php
+++ resources/views/home/broadcast.blade.php
@@ -58,7 +58,7 @@
         <div class="form-group row">
             <label for="staticEmail" class="col-sm-2 col-form-label">Message</label>
             <div class="col-sm-10">
-                <input type="text"  class="form-control" name="title">
+                <input type="text"  class="form-control" name="message">
             </div>
         </div>
 
