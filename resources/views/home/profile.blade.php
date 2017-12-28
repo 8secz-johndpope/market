4c0b955f9188ff652eb048ca5d6183b4c15c0479
anil <anil@sumra.net>
2017-12-28 19:38:48 +0000

    auto

---
+++ resources/views/home/profile.blade.php
@@ -0,0 +1,92 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.next')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+
+            <ul class="nav nav-tabs">
+
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span> &nbsp;&nbsp; Manage  ads</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
+                </li>
+                @if($user->contract!==null)
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
+                    </li>
+                @endif
+                <li class="nav-item active">
+                    <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <form method="post" action="/user/save/profile">
+        <div class="form-group row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Display Name</label>
+            <div class="col-sm-10">
+                <input type="text"  class="form-control" name="display_name">
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class="col-sm-12">
+                <a ><img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$user->image}}" class="add-profile-image" style="cursor: pointer;width: 50px;"></a>
+                <input type="file" id="upload-profile"  style="display: none">
+                <input type="hidden" name="image" value="{{$user->image}}" id="image">
+
+            </div>
+        </div>
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col">
+                <input type="submit" class="btn btn-primary" value="Save Changes">
+
+            </div>
+            <div class="col">
+            </div>
+        </div>
+    </form>
+    <script>
+        $(".add-profile-image").click(function () {
+            $("#upload-profile").click();
+        });
+
+        $("#upload-profile").change(function () {
+            console.log("did change");
+            upload_profile();
+        });
+    </script>
+@endsection
