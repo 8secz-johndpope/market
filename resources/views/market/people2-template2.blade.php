4812e088028778fa113f877e682134c5ec7b1c4b
Mauricio <mauri@sumra.net>
2017-12-29 15:42:00 +0000

    auto

--- resources/views/market/people2-template2.blade.php
+++ resources/views/market/people2-template2.blade.php
@@ -103,14 +103,14 @@
 			</div>
 			<div class="row">
 				<div class="col-sm-4">
-					<div data-widget-id="2236" class="widget insight video grid_1_2 " data-muse-widget-name="video">
-						<a href="http://vimeo.com/53825150">
+					<div data-widget-id="2252" class="widget insight video grid_1_2 " data-muse-widget-name="video">
+						<a href="http://vimeo.com/53830893">
 							<div class="playhead-overlay">
 								<img src="https://www.themuse.com/static/images/white_playhead.png?v=83d7a8492a9fb5ac116accc9aefed38ff98b72c24eb3a5464f6d5d83d3e89f30" alt="click to play video">
 							</div>
-							<img src="https://assets.themuse.com/uploaded/companies/61/widgets/2236.jpg?v=None" alt="video thumbnail image">
+							<img src="https://assets.themuse.com/uploaded/companies/61/widgets/2252.jpg?v=None" alt="video thumbnail image">
 							<div class="text-bottom">
-								<div class="video-text">Fancy Fridays</div>
+								<div class="video-text">Customer Stories &amp; Driver Stories</div>
 							</div>
 						</a>
 					</div>
