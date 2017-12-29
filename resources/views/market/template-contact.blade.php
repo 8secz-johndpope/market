d7c2cda5a5cda8be3a5fe14474f8e029ad556b89
Mauricio <mauri@sumra.net>
2017-12-29 21:13:46 +0000

    auto

--- resources/views/market/template-contact.blade.php
+++ resources/views/market/template-contact.blade.php
@@ -16,7 +16,7 @@
 			<div class="col-sm-12">
 				<div class="parallax-content">
 					<div class="container-logo">
-						<img src="">
+						<img src="/css/black-logo.svg" class="logo-black">
 					</div>
 					<p class="c-white">Contact - Uber Template</p>
 				</div>
