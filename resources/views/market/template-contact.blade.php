f2b4f36711a453bbdee4dd0f414d4e1ad4474491
Mauricio <mauri@sumra.net>
2017-12-29 20:16:28 +0000

    auto

--- resources/views/market/template-contact.blade.php
+++ resources/views/market/template-contact.blade.php
@@ -8,8 +8,8 @@
     <p>This is appended to the master sidebar.</p>
 @endsection
 @section('content')
- <link href="{{asset('/css/template-contact.css?q=874')}}" rel="stylesheet">
  <link href="{{ asset('/css/css/font-awesome.min.css?q=874') }}" rel="stylesheet">
+ <link href="{{asset('/css/template-contact.css?q=874')}}" rel="stylesheet">
  <div class="body">
 	<div class="container-fluid">
 		<div class="row header-container">
