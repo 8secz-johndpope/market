b51184c7ced60c9af87d84ea063f1fca45f27c6d
Mauricio <mauri@sumra.net>
2017-12-30 18:51:28 +0000

    auto

--- resources/views/market/template-job-safety-lead-uki.blade.php
+++ resources/views/market/template-job-safety-lead-uki.blade.php
@@ -209,7 +209,7 @@
 					<section class="job-main-section row">
 						<div class="job-body">
 							<div class="job-post">
-								<div class="bottom-ctas">
+								<div class="bottom-ctas clearfix">
 									<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
 										<div class="external-apply center">
 											<a class="tm-bright-blue-round-button btn-block apply-external" href="https://careers-uber.icims.com/jobs/34729/safety-lead-uki/job?iis=The+Muse&amp;iisn=The+Muse&amp;mode=job" target="_blank" alt="Safety Lead UKI - Apply Now">
