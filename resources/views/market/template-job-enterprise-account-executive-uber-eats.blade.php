aaec4f7cc9ebe164cf32d6bea9bd713f5f9b86fa
Mauricio <mauri@sumra.net>
2017-12-30 19:29:15 +0000

    auto

---
+++ resources/views/market/template-job-enterprise-account-executive-uber-eats.blade.php
@@ -0,0 +1,383 @@
+@extends('layouts.recruiters')
+
+@section('title', env('APP_NAME').' | Contact - Template Uber')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+@section('content')
+ <link href="{{ asset('/css/css/font-awesome.min.css?q=874') }}" rel="stylesheet">
+ <link href="{{asset('/css/template-job.css?q=874')}}" rel="stylesheet">
+ <div id="job-nav" class="docked-nav job hidden-xs">
+ 	<div class="container">
+ 		<div class="row">
+ 			<div class="col-sm-8">
+ 				<div class="docked-title">
+					<h4>Uber</h4>
+					<h3>Safety Lead UK</h3>
+				</div>
+ 			</div>
+ 			<div class="col-sm-4">
+ 				<div class="docked-ctas">
+					<div class="favorite-container">
+						<div>
+							<a aria-label="favorite" href="#" class="favorite">
+								<i aria-hidden="true" class="fa fa-heart-o"></i>
+							</a>
+						</div>
+					</div>
+					<a class="tm-bright-blue-round-button apply-external" href="https://careers-uber.icims.com/jobs/34729/safety-lead-uki/job?iis=The+Muse&amp;iisn=The+Muse&amp;mode=job" target="_blank" alt="Safety Lead UKI - Apply Now">
+						Apply Now 
+						<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+					</a>
+				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ </div>
+ <div class="body">
+	<div class="container-fluid">
+		<div class="row header-container">
+			<div class="col-sm-12">
+				<div id="top-image" class="bg-image">
+					<h1>Safety Lead UK</h1>
+					<div class="ctas text-center">
+						<a class="tm-bright-blue-round-button apply-external hidden-xs" href="https://careers-uber.icims.com/jobs/34729/safety-lead-uki/job?iis=The+Muse&amp;iisn=The+Muse&amp;mode=job" target="_blank" alt="Safety Lead UKI - Apply Now">
+							Apply Now 
+							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="white-container">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="job-side-section sidebar-content">
+						<a id="company-logo-link" href="/companies/uber">
+							<img class="company-logo" src="https://assets.themuse.com/uploaded/companies/61/small_logo.png?v=None" alt="Safety Lead UKI">
+						</a>
+						<a id="company-name-link" class="company-link visible-xs see-more-link" href="/companies/uber">
+							Uber
+							<span aria-hidden="true" class="no-wrap-space">
+								<i aria-hidden="true" class="fa fa-long-arrow-right"></i>
+							</span>
+						</a>
+						<p class="hidden-xs">
+							Uber is your on-demand private driver. Request a ride at any time, to wherever you’re going, with Uber’s iPhone and Android apps.
+						</p>
+						<div class="company-profile-preview-sidebar">
+							<div class="company-profile-preview-sidebar-image">
+								<a href="/companies/uber" target="_blank">
+									<img alt="Uber Company Image" src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff3.jpg%3Fv%3DNone&amp;h=352&amp;w=530">
+									<div class="tm-white-round-button">Company Profile</div>
+								</a>
+							</div>
+						</div>
+						<h3 class="hidden-xs">About This Job</h3>
+						<div class="metadata metadata-list">
+							<div class="metadata-list_section metadata-list_section--blue">
+								<h4 class="metadata-list_header">Location</h4>
+								<ul class="metadata-list_items">
+									<li>
+										<a href="/jobs/l-London%2C-United-Kingdom-jobs">London, United Kingdom</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="hidden-xs">
+							<div class="favorite-box-container btn-block">
+								<div>
+									<a href="#" class="favorite">
+										<i class="heart-empty favroite-icon"></i> 
+										Save For Later
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="visible-sm visible-md visible-lg">
+						<section class="job-side-section sidebar-content">
+							<h3>Useful Links</h3>
+							<ul class="useful-links">
+								<li>
+									<a href="/jobs/uk"><i class="fa fa-chevron-left" aria-hidden="true"></i> Back to All Jobs</a>
+								</li>
+								<li>
+									<a href="/company-jobs/c-uber-jobs"><i class="fa fa-chevron-left" aria-hidden="true"></i> See all Uber Jobs</a>
+								</li>
+								<li>
+								<a href="/jobs/london"><i class="fa fa-chevron-left" aria-hidden="true"></i> See all London, United Kingdom Jobs</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+				<div class="col-sm-8 job-main-section">
+					<div class="job-body">
+						<div class="job-post">
+							<div class="job-post-description">
+								<p>Uber Overview</p>
+								<p>About Uber</p>
+								<p>We're changing the way people think about transportation. Not that long ago we were just an app to request premium black cars in a few metropolitan areas. Now we're a part of the logistical fabric of more than 600 cities around the world. Whether it's a ride, a sandwich, or a package, we use technology to give people what they want, when they want it.</p>
+								<p>For the people who drive with Uber, our app represents a flexible new way to earn money. For cities, we help strengthen local economies, improve access to transportation, and make streets safer.</p>
+								<p>And that's just what we're doing today. We're thinking about the future, too. With teams working on autonomous trucking and self-driving cars, we're in for the long haul. We're reimagining how people and things move from one place to the next.</p>
+								<p>Job Description</p>
+								<p>About the Role</p>
+								<p>The Safety Lead is responsible for driving strategy, processes and partnerships that establish Uber as the industry leader in safety across the UK &amp; Ireland. You will lead the safety team and own the development of internal policies and procedures that inform incident prevention, investigation, treatment and accountability; liaise with our various rider and driver support teams; manage our driver appeals panel; and support the launch of new safety features in our apps.</p>
+								<p>Perks</p>
+								<p>What You'll Do</p>
+								<ul>
+									<li>You will drive the conversation on measures to improve passenger safety with regulatory and industry bodies.</li>
+									<li>You will use analytics to help conduct investigations, assess trends and identify opportunities to deliver solutions to reduce their occurrence. You'll also be working closely with our communications and marketing teams on all safety-related materials for riders, drivers and the public.</li>
+									<li>Own and improve our safety policies and procedures across the UK &amp; Ireland</li>
+									<li>Manage internal procedures and the relationship with rider and driver support teams, including our Centre of Excellence - the support team dealing with responses to riders and drivers</li>
+									<li>Lead a team of Operations Managers based out of London</li>
+									<li>Work to improve and streamline our incident response processes</li>
+									<li>Conduct investigations in collaboration with our legal team</li>
+									<li>Develop and actively measure key performance indicators for safety, with a goal for prevention</li>
+									<li>Support the launch of safety related products and track their impact</li>
+									<li>Work with our policy and marketing team on key initiatives including partnerships with third-party organisations</li>
+									<li>Coordinate and own best-practice materials for major incidents</li>
+								</ul>
+								<p>What You'll Need</p>
+								<ul>
+									<li>A minimum of 8 years of experience in an operational role in the area of safety</li>
+									<li>Experience with managing multiple stakeholders on projects with a high degree of complexity</li>
+									<li>Data-driven decision mentality and sound business judgment through strong analytical thinking.</li>
+									<li>High adaptability to change and ability to execute quickly.</li>
+									<li>Speed, resourcefulness, and go getter attitude - Uber is committed to constant improvement. You're excited to help create a product with value.</li>
+									<li>Curious and eager to learn - you are interested in all things and can build up skills and knowledge very fast to become a recognized subject matter expert amongst your peers</li>
+									<li>High Emotional IQ – you deal with partners, influencers, and riders with no problem.</li>
+									<li>Flexible and forward thinking - we need someone sharp and eager to play with different models to help us scale.</li>
+									<li>Self-starter and collaborative leader. Everywhere you go, you can't help but mobilize people, build things, resolve problems, get into the details, go above and beyond, and draw out the best in people. You are a doer and motivator of others.</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="company-profile-preview">
+						<section class="sidebar-content visible-xs">
+							<h3>Meet employees</h3>
+						</section>
+						<section class="visible-sm visible-md visible-lg">
+							<hr>
+							<h1>Meet Some of Uber's Employees</h1>
+						</section>
+						<div class="employee-preview">
+							<div class="employee-preview-image">
+								<a target="_blank" href="/companies/uber/people/brian">
+									<div class="tm-white-round-button">Learn More</div>
+									<img alt="Uber Employee" src="https://assets.themuse.com/uploaded/companies/61/people/182-large.jpg?v=None">
+								</a>
+							</div>
+							<h2>Brian M.</h2>
+							<h3>Community Management Specialist</h3>
+							<p>Brian makes sure that every Uber user has an amazing experience. He troubleshoots roadblocks to customer happiness and also does outreach to attract new Uber users.</p>
+						</div>
+						<div class="employee-preview">
+							<div class="employee-preview-image">
+								<a target="_blank" href="/companies/uber/people/swathy">
+									<div class="tm-white-round-button">Learn More</div>
+									<img alt="Uber Employee" src="https://assets.themuse.com/uploaded/companies/61/people/184-large.jpg?v=None">
+								</a>
+							</div>
+							<h2>Swathy P.</h2>
+							<h3>Operations &amp; Logistics Manager</h3>
+							<p>Swathy is part of the team that is the driving force behind Uber’s transportation options. She helps sign up the drivers, makes sure the vehicles run smoothly, and tackles any logistical bumps along the road.</p>
+						</div>
+					</div>
+					<div class="visible-xs">
+						<section class="job-side-section sidebar-content">
+							<h3>Useful Links</h3>
+							<ul class="useful-links">
+								<li>
+									<a href="/jobs/uk"><i class="fa fa-chevron-left" aria-hidden="true"></i> Back to All Jobs</a>
+								</li>
+								<li>
+									<a href="/company-jobs/c-uber-jobs"><i class="fa fa-chevron-left" aria-hidden="true"></i> See all Uber Jobs</a>
+								</li>
+								<li>
+									<a href="/jobs/london"><i class="fa fa-chevron-left" aria-hidden="true"></i> See all London, United Kingdom Jobs</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+					<section class="job-main-section row">
+						<div class="job-body">
+							<div class="job-post">
+								<div class="bottom-ctas clearfix">
+									<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+										<div class="external-apply center">
+											<a class="tm-bright-blue-round-button btn-block apply-external" href="https://careers-uber.icims.com/jobs/34729/safety-lead-uki/job?iis=The+Muse&amp;iisn=The+Muse&amp;mode=job" target="_blank" alt="Safety Lead UKI - Apply Now">
+												Apply Now 
+												<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+											</a>
+										</div>
+									</div>
+									<div class="col-xs-12 visible-xs apply-later">
+										<div class="send-to-self">
+											<div>
+												<form method="POST" class="border-input form-styling" role="form">
+													<div>
+														<input type="hidden" name="_xsrf" value="2|2be55c91|c05cb2b21ed70e27cb87c3c642809261|1512660071">
+														<button type="submit" name="action" value="" class="tm-medium-grey-round-button">
+															<i class="fa fa-envelope-o"></i> 
+															Email To Myself For Later
+														</button>
+													</div>
+												</form>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+										<a class="tm-input-grey-round-button btn-block send-to-friend-btn" href="mailto:?Subject=Safety Lead UKI&amp;body=Just saw this awesome job I thought I would share with you! Check it out here: https://dev.sumra.net/jobs/uber/safe-lead-uki?utm_source=job-individual" target="_blank">
+											Send To A Friend
+										</a>
+									</div>
+									<div class="col-sm-6 col-xs-12 hidden-xs">
+										<div class="favorite-box-container btn-block"><div><a href="#" class="favorite"><i class="heart-empty favroite-icon"></i> Save For Later</a></div></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="recommend-jobs-wrapper">
+		<div class="container">
+			<div class="row recommended-jobs">
+				<div class="col-sm-12">
+					<div class="section">
+						<div class="header-row">
+							<h3 class="section-header">A FEW MORE JOBS WE THINK YOU'LL LOVE</h3>
+						</div>
+					</div>					
+				</div>
+				<div class="col-sm-12">
+					<div class="block">
+						<div>
+							<div>
+								<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 track-tile">
+									<div class="job-element Kaiser Associates" data-id="487096">
+										<a href="#" data-tile-link="true">
+											<div class="tile-copy">
+												<h3>
+													<span>Kaiser Associates</span>
+													Vice President (Consumer Goods and Private Equity/M&amp;A)
+												</h3>
+												<ul class="metadata">
+													<li>London, United Kingdom</li>
+												</ul>
+											</div>
+											<div class="white-gradient hidden-xs"></div>
+											<div class="tile-bg">
+												<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F1393%2Fmodules%2F9367%2Fasset1.jpg&amp;h=295&amp;w=400" alt="Now Hiring - Vice President (Consumer Goods and Private Equity/M&amp;A) at Kaiser Associates">
+											</div>
+										</a>
+										<div class="favorite-container">
+											<div>
+												<a aria-label="favorite" href="#" class="favorite">
+													<i aria-hidden="true" class="heart-empty favroite-icon"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div>
+							<div>
+								<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 track-tile">
+									<div class="job-element Uber" data-id="782201">
+										<a href="/jobs/uber/senior-community-operations-manager-ubereats-uki?ref=job-recommendation-1" data-tile-link="true">
+											<div class="tile-copy">
+												<h3>
+													<span>Uber</span>
+													Senior Community Operations Manager, UberEATS - UKI
+												</h3>
+												<ul class="metadata">
+													<li>London, United Kingdom</li>
+												</ul>
+											</div>
+											<div class="white-gradient hidden-xs"></div>
+											<div class="tile-bg">
+												<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F61%2Fpages%2F674%2Ff1.jpg&amp;h=295&amp;w=400" alt="Now Hiring - Senior Community Operations Manager, UberEATS - UKI at Uber">
+											</div>
+										</a>
+										<div class="favorite-container">
+											<div>
+												<a aria-label="favorite" href="#" class="favorite">
+													<i aria-hidden="true" class="heart-empty favroite-icon"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div>
+							<div>
+								<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 track-tile">
+									<div class="job-element Canada Goose" data-id="763458">
+										<a href="/jobs/canadagoose/mandarin-speaking-sales-associate?ref=job-recommendation-2" data-tile-link="true">
+											<div class="tile-copy">
+												<h3>
+													<span>Canada Goose</span>
+													Mandarin Speaking Sales Associate
+												</h3>
+												<ul class="metadata">
+													<li>London, United Kingdom</li>
+												</ul>
+											</div>
+											<div class="white-gradient hidden-xs"></div>
+											<div class="tile-bg">
+												<img src="https://pilbox.themuse.com/image.jpg?url=https%3A%2F%2Fassets.themuse.com%2Fuploaded%2Fcompanies%2F1350%2Fmodules%2F9374%2Fasset1.jpg&amp;h=295&amp;w=400" alt="Now Hiring - Mandarin Speaking Sales Associate at Canada Goose">
+											</div>
+										</a>
+										<div class="favorite-container">
+											<div>
+												<a aria-label="favorite" href="#" class="favorite">
+													<i aria-hidden="true" class="heart-empty favroite-icon"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="scroll" style="display: none;">
+		<a class="btn-scroll" href="#">
+			<i class="fa fa-chevron-up"></i>
+			<br>
+			Back to top
+		</a>
+	</div>
+</div>
+<script>
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 300 && !$('#job-nav').hasClass('open')){
+			$('#job-nav').addClass('open');
+			$('.scroll').show();
+		}
+		else if($(this).scrollTop() < 300 && $('#job-nav').hasClass('open')){
+			$('#job-nav').removeClass('open');
+			$('.scroll').hide();
+		}
+	});
+	$('.btn-scroll').click(function(e){
+		e.preventDefault();
+		$('html').animate({ scrollTop: 0}, 600);
+	});
+</script>
+@endsection
