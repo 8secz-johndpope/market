02c5a7497a01a79447674d30de7c2b862abb369c
anil <anil@sumra.net>
2017-12-30 19:02:57 +0000

    auto

---
+++ resources/views/home/settings.blade.php
@@ -0,0 +1,64 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.next')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+
+            <ul class="nav nav-tabs">
+
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span> &nbsp;&nbsp; Manage  ads</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
+                </li>
+                @if($user->contract!==null)
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
+                    </li>
+                @endif
+                <li class="nav-item active">
+                    <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="notifications">
+        <label class="form-check-label" for="defaultCheck1">
+            Receive Notifications
+        </label>
+    </div>
+
+@endsection
