232f21dc40530a677ae59d3caf5d6ec0bc23b8ad
Mauricio <mauri@sumra.net>
2017-12-30 21:56:39 +0000

    auto

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="{{ asset('/css/css/font-awesome.min.css?q=874') }}" rel="stylesheet">
     <link href="{{ asset('/css/base.css?q=34') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/slick.css?q=43') }}"/>
