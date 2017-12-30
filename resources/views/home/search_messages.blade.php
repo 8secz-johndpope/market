5d3b9ade011527c24364494dd5d4860775fcc03d
anil <anil@sumra.net>
2017-12-30 18:21:16 +0000

    auto

--- resources/views/home/search_messages.blade.php
+++ resources/views/home/search_messages.blade.php
@@ -1,5 +1,5 @@
 <ul class="list-group">
     @foreach($messages as $message)
-    <li class="list-group-item"><span style="padding: 4px;">{{$message->message}}</span></li>
+    <li class="list-group-item"><span style="padding: 10px;">{{$message->message}}</span></li>
         @endforeach
 </ul>
