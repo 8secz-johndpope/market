93f798fca71e42b240702c353ed41e098f3fa758
anil <anil@sumra.net>
2018-01-02 19:19:32 +0000

    auto

--- resources/views/bank/dashboard.blade.php
+++ resources/views/bank/dashboard.blade.php
@@ -12,11 +12,9 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-12">
-
-
-
-
+        <div class="col-sm-3">
+        </div>
+        <div class="col-sm-6">
         </div>
     </div>
 
