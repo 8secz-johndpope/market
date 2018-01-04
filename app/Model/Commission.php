094d1947b9f2a5ba4838d22c062df5e0fee159c5
anil <anil@sumra.net>
2018-01-04 19:15:45 +0000

    auto

---
+++ app/Model/Commission.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 04/01/2018
+ * Time: 19:15
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Commission extends Model
+{
+
+}
