2b315f77138da2c6fdb85a516857848d415ea502
anil <anil@sumra.net>
2018-01-04 19:42:39 +0000

    auto

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,7 +16,9 @@
         </div>
         <div class="col-sm-6">
             <br><br><br>
-            <a href="/admin/manage/commissions" class="btn btn-primary">Commissions</a>
+            <a href="/admin/manage/commissions" class="btn btn-primary">Commissions</a>             <a href="/admin" class="btn btn-primary">Users</a>
+            <a href="/admin" class="btn btn-primary">Contracts</a>
+
         </div>
     </div>
 
