6091a70c56d51d904844cf70fe01d2e145e43ff2
anil <anil@sumra.net>
2018-01-05 18:24:01 +0000

    auto

--- resources/views/home/contacts.blade.php
+++ resources/views/home/contacts.blade.php
@@ -12,48 +12,9 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-12">
-
-            <ul class="nav nav-tabs">
-
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span> &nbsp;&nbsp; Manage  ads</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
-                </li>
-                @if($user->contract!==null)
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
-                    </li>
-                @endif
-                <li class="nav-item active">
-                    <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
-                </li>
-            </ul>
+        <div class="col-sm-1">
         </div>
-    </div>
+        <div class="col-sm-10">
     <a class="btn btn-primary" href="/user/contacts/add">Add Contact</a>
     <a class="btn btn-success" href="/user/groups/create">New Group</a>
 
@@ -86,5 +47,6 @@
         @endforeach
         </tbody>
     </table>
-
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/next.blade.php
+++ resources/views/layouts/next.blade.php
@@ -228,191 +228,30 @@
 </audio>
 <header>
 
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid" style="background: black">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                @if(Auth::user()->contract!==null)
-                <a class="navbar-brand" href="/"><img class="icon-small" src="/css/ggg-business.png"></a>
-                    @else
-                    <a class="navbar-brand" href="/"><img class="icon-small" src="/css/ggg-text.png"></a>
 
-                @endif
-
-            </div>
-            <div class="business-drop collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Help</a></li>
-                    <li><a href="#">Store</a></li>
-                    @if (Auth::guest())
-                        <li><a href="/login">Login</a></li>
-                        <li><a href="/register">Sign Up</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                Hello, {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu options-user" role="menu">
-                                <div class="list-menu-common">
-                                    <div class="title-list">
-                                        <span class="nav-link nav-color">Your account</span>
-                                    </div>
-                                    <ul>
-                                        <li><a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;Manage My Ads</a> </li>
-                                        <li><a class="nav-link nav-color" href="/user/ad/create"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Post an Ad</a> </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span>&nbsp;&nbsp;Orders</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Messages</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;My Details</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/user/manage/favorites"><span class="glyphicon glyphicon-heart"></span>&nbsp;&nbsp;Favorites</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span>&nbsp;&nbsp;Search Alerts</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/business/manage/support"><span class="fa fa-envelope"></span> &nbsp;&nbsp; Support</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                               <span class="glyphicon glyphicon-log-out"></span>&nbsp;&nbsp; Logout
-                                            </a>
-
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                {{ csrf_field() }}
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="list-menu-common">
-                                    <div class="title-list">
-                                        <span class="nav-link nav-color">Invoices</span>
-                                    </div>
-                                    <ul>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-open-file"></span>&nbsp;&nbsp;Send Invoice</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-alert"></span>&nbsp;&nbsp;Unpaid Invoice</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Paid Invoice</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Pending Invoices</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="fa fa-cloud-download"></span>&nbsp;&nbsp;Download Invoice CSV/Excel</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                @if(Auth::user()->contract!==null)
-                                <div class="list-menu-common">
-                                    <div class="title-list">
-                                        <span>Your business</span>
-                                    </div>
-                                    <ul>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Company</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp;Financials</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp;Metrics</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="/user/manage/applications"><span class="glyphicon glyphicon-list-alt"></span> &nbsp;&nbsp;Your Jobs Applications</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="fa fa-car"></span> &nbsp;&nbsp;Your Motors</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="fa fa-building"></span> &nbsp;&nbsp;Your Properties</a>
-                                        </li>
-                                        <li>
-                                            <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-tag"></span> &nbsp;&nbsp;Your Sales</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                @endif
-                                <div class="list-menu-common">
-                                    <div class="title-list">
-                                        <span class="nav-link nav-color">Our Offers</span>
-                                    </div>
-                                    <ul>
-                                        <li>
-                                            <span class="nav-link nav-offer">We do not have offers at the moment</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </li>
-
-                    @endif
-
-                    @if (!Auth::guest())
-
-                        <li class="dropdown messages-nav"><a href="/user/manage/messages"  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                <span > <span  class="glyphicon glyphicon-envelope"></span>    <span class="button__badge" style="display: none" id="message-notification">1</span></span><span class="caret"></span></a>
-                            <ul class="dropdown-menu all-menu-messages list-group" role="menu">
-                                @foreach(Auth::user()->rooms as $room)
-                                    <li class="list-group-item">
-                                        @if($room->last_message())
-                                        <a href="/user/manage/messages/{{$room->id}}">
-                                            <div class="message-inside">
-                                                <span class="message-username">{{$room->last_message()->user->name}}</span>
-                                                <span class="title-advert">{{$room->title}}</span>
-                                                <p class="@if($room->unread===1) unread-message @endif">{{$room->last_message()->message}}</p>
-                                                
-                                            </div>
-                                        </a>
-                                        @endif
-                                    </li>
-                                @endforeach
-                            </ul>
-
-                        </li>
-                    @endif
-                    <li><a class="post-advert" href="/user/ad/create"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Post an Ad</a> </li>
-
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div>
-    </nav>
 
 </header>
 
 
-<section>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="background-color: #000;">
+    <!-- Brand/logo -->
+    <a class="navbar-brand" href="#">
+        <img src="/css/ic_launcher1.png" alt="logo" style="width:40px;">
+    </a>
+
+    <!-- Links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/">{{env('APP_NAME')}}</a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container-fluid">
     @yield('content')
-
-</section>
-
-<div class="copyright style-1">
-    <div class="container">
-        <div class="row">
-            <div class="cell col-md-8 col-xs-12 footer-copy col-md-offset-2 text-center">
-                All rights reserved. Copyright &copy; 2017 <span class="company-rights">{{env('APP_NAME')}}</span>
-            </div>
-        </div>
-    </div>
 </div>
+
+
 <script>
     // This example adds a search box to a map, using the Google Place Autocomplete
     // feature. People can enter geographical searches. The search box will return a
