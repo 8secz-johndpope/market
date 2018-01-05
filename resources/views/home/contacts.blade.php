056917b35e8cfb0a059ea2864cb86dc1cb76866f
anil <anil@sumra.net>
2018-01-05 18:17:28 +0000

    auto

--- resources/views/home/contacts.blade.php
+++ resources/views/home/contacts.blade.php
@@ -57,11 +57,25 @@
     <a class="btn btn-primary" href="/user/contacts/add">Add Contact</a>
     <a class="btn btn-success" href="/user/groups/create">New Group</a>
 
+    <h4>Favorites</h4>
     <table class="table">
         <tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Email</th><th>Send Message</th><th>Send Invoice</th></tr>
         @foreach($user->contacts as $contact)
+            @if($contact->is_user())
             <tr><td>{{$contact->first}}</td><td>{{$contact->last}}</td><td>{{$contact->phone}}</td><td>{{$contact->email}}</td><td>@if($contact->is_user())<a class="btn btn-primary" href="/user/direct/message/{{$contact->uid()}}">Send Message</a> @else <a class="btn btn-outline-primary" href="#">Invite</a> @endif</td><td>@if($contact->is_user())<a class="btn btn-primary" href="/user/direct/invoice/{{$contact->uid()}}">Send Invoice</a> @else  @endif</td></tr>
-            @endforeach
+            @endif
+                @endforeach
+    </table>
+
+
+    <h4>Invite</h4>
+    <table class="table">
+        <tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Email</th><th>Invite</th></tr>
+        @foreach($user->contacts as $contact)
+            @if($contact->is_user())
+                <tr><td>{{$contact->first}}</td><td>{{$contact->last}}</td><td>{{$contact->phone}}</td><td>{{$contact->email}}</td><td> <a class="btn btn-outline-primary" href="#">Invite</a> </td></tr>
+            @endif
+        @endforeach
     </table>
 
 @endsection
