3c62858ff918af5f5d1b664ff9404b1cb301ee37
anil <anil@sumra.net>
2018-01-06 18:15:30 +0000

    auto

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,6 +15,56 @@
 
         <div class="col-sm-4">
             <br><br><br>
+            <h4 style="text-align: center">Total Payments from Contract Invoices: £{{number_format($dtotal/100,2)}}</h4>
+            <br><br><br>
+            <h4>Payments from Contract Invoices</h4>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Description</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($dtransactions as $transaction)
+                    <tr>
+                        <td>{{$transaction->description}}</td>
+                        <td>£{{number_format($transaction->amount/100,2)}}</td>
+                        <td>{{date('d/m/Y',strtotime($transaction->created_at))}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="col-sm-4">
+            <br><br><br>
+            <h4 style="text-align: center">Total Payments from Enhancements: £{{number_format($etotal/100,2)}}</h4>
+            <br><br><br>
+            <h4>Payments from Enhancements</h4>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">Description</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($etransactions as $transaction)
+                    <tr>
+                        <td>{{$transaction->description}}</td>
+                        <td>£{{number_format($transaction->amount/100,2)}}</td>
+                        <td>{{date('d/m/Y',strtotime($transaction->created_at))}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="col-sm-4">
+            <br><br><br>
             <h4 style="text-align: center">Total Commissions from Sales: £{{number_format($stotal/100,2)}}</h4>
             <br><br><br>
             <h4>Commissions from Sales</h4>
