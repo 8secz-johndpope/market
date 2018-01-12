701ed731ff9f761e2aa9d8f8de6ad7b8cba54f67
Mauricio <mauri@sumra.net>
2018-01-12 15:52:25 +0000

    auto

--- resources/views/market/cars.blade.php
+++ resources/views/market/cars.blade.php
@@ -201,7 +201,7 @@
                                                 @if($advert->has_meta('key_facts'))
                                                 <div class="col-sm-2 text-center">
                                                     <div class="item-key-facts">
-                                                        <img class="item-key-img img-responsive" src="/css/icons/mpg.svg">
+                                                        <img class="item-key-img img-responsive" src="/css/icons/bhp.svg">
                                                         <p class="item-key-value">
                                                             {{$advert->meta('key_facts')['Engine power']}}
                                                         </p>
