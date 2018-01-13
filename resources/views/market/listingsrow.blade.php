660d48b7f9db61790b74de37a954c2e2999e38e0
Mauricio <mauri@sumra.net>
2018-01-13 20:52:01 +0000

    chaging filters design for autotrader

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -566,7 +566,8 @@ use App\Model\Advert;
     $("#phone-number-2").intlTelInput();
     $("#phone-number-1").intlTelInput();
     $('.flyout-list .options-button').click(function(){
-        $(this).next().toggleClass(['is-in', 'is-visible']);
+        $(this).next().toggleClass('is-in');
+    $(this).next().toggleClass('is-visible');
     });
 </script>
 @endsection
