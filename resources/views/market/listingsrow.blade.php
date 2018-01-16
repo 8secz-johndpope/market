01d052c1c04323c44a76fdb46f3a61a5c8beff29
Mauricio <mauri@sumra.net>
2018-01-16 16:35:19 +0000

    chaging listing design like autotrader

--- resources/views/market/listingsrow.blade.php
+++ resources/views/market/listingsrow.blade.php
@@ -770,7 +770,7 @@ use App\Model\Advert;
         $(this).next().toggleClass('is-in is-visible');
     });
     $('.sf-flyout-close').click(function(){
-        $(this).closest('.flyout').removeClass('is-visible');
+        $(this).closest('.flyout').toggleClass('is-in is-visible');
     })
 </script>
 @endsection
