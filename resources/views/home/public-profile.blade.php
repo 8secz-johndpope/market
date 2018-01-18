ff296911b33b50e4f2ab12e476a474abe2f1e90f
Mauricio <mauri@sumra.net>
2018-01-18 21:32:46 +0000

    working in public profile

--- resources/views/home/public-profile.blade.php
+++ resources/views/home/public-profile.blade.php
@@ -27,6 +27,18 @@
     <section class="container-details-profile">
         <div class="container">
             <div class="row">
+                <div class="col-sm-3">
+                    <div class="wrapper-picture-profile">
+                        <figure>
+                            <img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$user->image}}" class="img-responsive photo-profile">
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-sm-9">
+                    <div class="wrapper-details-profile">
+                        <h3>{{$user->name}}
+                    </div>
+                </div>
             </div>
         </div>
     </section>
