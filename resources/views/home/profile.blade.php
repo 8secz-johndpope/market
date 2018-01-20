18709686d8293784bed10d98ce2f89449822dee7
Mauricio <mauri@sumra.net>
2018-01-20 15:07:49 +0000

    working in private profile

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -9,76 +9,62 @@
 
     <p>This is appended to the master sidebar.</p>
 @endsection
-
+@php
+    $date = new Datetime();
+    $dateMs = $date->getTimestamp();
+@endphp
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-
-            <ul class="nav nav-tabs">
-
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/ads"><span class="glyphicon glyphicon-folder-open"></span> &nbsp;&nbsp; Manage  ads</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/images"><span class="glyphicon glyphicon-camera"></span>&nbsp;&nbsp;Images</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/orders"><span class="glyphicon glyphicon-credit-card"></span> &nbsp;&nbsp; Orders</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/messages"><span class="glyphicon glyphicon-envelope"></span> &nbsp;&nbsp; Messages</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/business/manage/details"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp; My Details</a>
-                </li>
-                @if($user->contract!==null)
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/company"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp; Company</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/finance"><span class="glyphicon glyphicon-gbp"></span> &nbsp;&nbsp; Financials</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-color" href="/business/manage/metrics"><span class="glyphicon glyphicon-stats"></span> &nbsp;&nbsp; Metrics</a>
-                    </li>
-                @endif
-                <li class="nav-item active">
-                    <a class="nav-link nav-color" href="#"><span class="glyphicon glyphicon-heart"></span> &nbsp;&nbsp; Favorites</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/user/manage/alerts"><span class="glyphicon glyphicon-bell"></span> &nbsp;&nbsp; Search Alerts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-color" href="/business/manage/support"><span class="glyphicon glyphicon-earphone"></span> &nbsp;&nbsp; Support</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <form method="post" action="/user/save/profile">
-        <div class="form-group row">
-            <label for="staticEmail" class="col-sm-2 col-form-label">Display Name</label>
-            <div class="col-sm-10">
-                <input type="text"  class="form-control" name="display_name" value="{{$user->display_name}}">
+<link href="{{ asset("/css/private-profile.css?q=$dateMs") }}" rel="stylesheet" type="text/css">
+<div class="body background-color">
+    <section class="container-profile-header mb-10">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2 class="header-profile-title">Your Sumra Private Profile</h2>
+                </div>
             </div>
         </div>
-        <div class="row form-group">
-            <div class="col-sm-12">
-                <a ><img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$user->image}}" class="add-profile-image" style="cursor: pointer;width: 50px;"></a>
-                <input type="file" id="upload-profile"  style="display: none">
-                <input type="hidden" name="image" value="{{$user->image}}" id="image">
-
+    </section>
+    <section class="container-details-profile mb-10">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-10"></div>           
             </div>
         </div>
-        {{ csrf_field() }}
+    </section>
+    <div class="container">
         <div class="row">
-            <div class="col">
-                <input type="submit" class="btn btn-primary" value="Save Changes">
-
-            </div>
-            <div class="col">
+            <div class="col-sm-12">
             </div>
         </div>
-    </form>
+        <form method="post" action="/user/save/profile">
+            <div class="form-group row">
+                <label for="staticEmail" class="col-sm-2 col-form-label">Display Name</label>
+                <div class="col-sm-10">
+                    <input type="text"  class="form-control" name="display_name" value="{{$user->display_name}}">
+                </div>
+            </div>
+            <div class="row form-group">
+                <div class="col-sm-12">
+                    <a ><img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$user->image}}" class="add-profile-image" style="cursor: pointer;width: 50px;"></a>
+                    <input type="file" id="upload-profile"  style="display: none">
+                    <input type="hidden" name="image" value="{{$user->image}}" id="image">
+
+                </div>
+            </div>
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="col">
+                    <input type="submit" class="btn btn-primary" value="Save Changes">
+
+                </div>
+                <div class="col">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
     <script>
         $(".add-profile-image").click(function () {
             $("#upload-profile").click();
