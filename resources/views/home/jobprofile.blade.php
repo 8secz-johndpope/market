e6b8066537b7e27d2e5a3b4f7e1752a238eb3909
Mauricio <mauri@sumra.net>
2018-01-20 16:25:58 +0000

    working in private profile

--- resources/views/home/jobprofile.blade.php
+++ resources/views/home/jobprofile.blade.php
@@ -58,21 +58,21 @@
                 <div class="col-sm-12">
                     <h2 class="template-options-title">Select & Create Your Profile</h2>
                 </div>
-                <div clas="col-sm-4">
+                <div class="col-sm-4">
                     <div class="template-item">
                         <h3 class="template-item-title">General</h3>
                         <div class="template-content">
                         </div>
                     </div>
                 </div>
-                <div clas="col-sm-4">
+                <div class="col-sm-4">
                     <div class="template-item">
                         <h3 class="template-item-title">Social Care</h3>
                         <div class="template-content">
                         </div>
                     </div>
                 </div>
-                <div clas="col-sm-4">
+                <div class="col-sm-4">
                     <div class="template-item">
                         <h3 class="template-item-title">Sub Contractor</h3>
                         <div class="template-content">
