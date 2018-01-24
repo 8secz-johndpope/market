eca4345f3eba7aedd3b85a66641e6da57fc8d392
Mauricio <mauri@sumra.net>
2018-01-24 16:01:35 +0000

    working in private profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2402,7 +2402,6 @@ class HomeController extends BaseController
     public function looking_for(Request $request){
         $user = Auth::user();
         $jobChildren = Category::find(4000000000)->children;
-        var_dump($jobChildren);
         return view('home.looking_for_edit', ['user' => $user, 'jobChildren' => $jobChildren]);
     }
 }
