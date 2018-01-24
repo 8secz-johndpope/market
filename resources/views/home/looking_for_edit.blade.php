4041e2e203e131a73c5942a4b06d94ef97d617c8
Mauricio <mauri@sumra.net>
2018-01-24 19:39:10 +0000

    working in private profile

--- resources/views/home/looking_for_edit.blade.php
+++ resources/views/home/looking_for_edit.blade.php
@@ -195,7 +195,7 @@
                             <ul class="role row">
                               @foreach($sectorPreferred->children as $subSector)
                                 <li class="form-field checkbox col-sm-6 col-xs-12">
-                                  <input type="checkbox" name="edit-subsector-{{$subSector->id}}"  id="edit-subsector-{{$subSector->id}}" value="{{$subSector->id}}" checked="{{in_array($subSector->id, $idsSubSectorPreferred) ? 'true': ''}}">
+                                  <input type="checkbox" name="edit-subsector-{{$subSector->id}}"  id="edit-subsector-{{$subSector->id}}" value="{{$subSector->id}}" checked="{{in_array($subSector->id, $idsSubSectorPreferred) ? 'true': 'false'}}">
                                   <label for="edit-subsector-{{$subSector->id}}">
                                     {{$subSector->title}}
                                   </label>
