85f704a72224f1753d127e27d936a444e7cc535d
anil <anil@sumra.net>
2018-01-25 14:52:28 +0000

    auto

--- routes/web.php
+++ routes/web.php
@@ -159,6 +159,7 @@ Route::post('/user/accounts/add', 'HomeController@add_bank_account');
 Route::post('/user/addresses/add', 'HomeController@add_address');
 Route::post('/user/covers/add', 'HomeController@add_cover');
 Route::post('/user/jobs/apply', 'HomeController@apply');
+Route::post('/user/jobs/apply/all', 'HomeController@apply_all');
 
 Route::get('/user/manage/order', 'HomeController@order');
 Route::get('/user/manage/sale/{id}', 'HomeController@checkout');
