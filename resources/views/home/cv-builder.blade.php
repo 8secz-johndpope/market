9b7577cad89835989dcc2c07ed3646804065fb33
Mauricio <mauri@sumra.net>
2018-01-25 16:41:33 +0000

    working cv-builder page

--- resources/views/home/cv-builder.blade.php
+++ resources/views/home/cv-builder.blade.php
@@ -139,10 +139,10 @@
           </div>
           <div class="row">
             <div class="col-xs-12 col-sm-5 col-sm-offset-2">
-              <button class="btn btn-submit" type="submit">Continue</button>
+              <button class="btn btn-inverse" type="button">Save and continue later</button>
             </div>
             <div class="col-xs-12 col-sm-5">
-              <button class="btn btn-inverse" type="button">Save and continue later</button>
+              <button class="btn btn-submit" type="submit">Continue</button>
             </div>
           </div>
           </form>
