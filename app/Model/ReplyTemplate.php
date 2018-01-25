161850fa92651f22440a38cd4fec2360eca36ee9
anil <anil@sumra.net>
2018-01-25 15:52:46 +0000

    auto

---
+++ app/Model/ReplyTemplate.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 25/01/2018
+ * Time: 15:49
+ */
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReplyTemplate extends Model
+{
+
+}
