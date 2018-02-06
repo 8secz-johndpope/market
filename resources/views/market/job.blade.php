6b0c642eaeceadfade0fe43ddf7b8eaf48c12dd6
Mauricio <mauri@sumra.net>
2018-02-06 16:56:17 +0000

    improving design for jobs adverts

--- resources/views/market/job.blade.php
+++ resources/views/market/job.blade.php
@@ -558,7 +558,7 @@
                     </p>
                 </div>
         </div>
-        <div class="col-md-3 col-sm-4 col-sm-12">
+        <div class="col-md-3 col-sm-4 col-xs-12">
             <!-- search -->
             <div class="row">
                 @if($advert->user!==null)
