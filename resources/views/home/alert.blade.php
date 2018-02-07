dd9168b0c1f44d758ac4704ea5e457d6757cd027
Mauricio <mauri@sumra.net>
2018-02-07 22:00:50 +0000

    adding create new alert page

--- resources/views/home/alert.blade.php
+++ resources/views/home/alert.blade.php
@@ -17,7 +17,23 @@
         <div class="col-md-6 col-md-offset-3">
             <div class="notification-pages">
               <div class="notification-page selected">
-                <form id="" action="" class="">
+                <form id="add_notifications" action="" name="add_notifications">
+                  <div class="field-id">
+                    <input type="hidden" name="id">
+                  </div>
+                  <div class="create-alert-home">
+                    <div class="notification-title">
+                      <h2>Create new Job Alert</h2>
+                    </div>
+                    <div class="notification-modify-form">
+                      <div class="row">
+                        <div class="col-sm-6">
+                        </div>
+                        <div class="col-sm-6">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </form>
               </div>
             </div>
