c7484fabab9396537ebaece38170b71e7ed573ee
Mauricio <mauri@sumra.net>
2018-02-10 16:55:59 +0000

    working in the pages for search recruiter and explore companiesç

--- resources/views/market/companies.blade.php
+++ resources/views/market/companies.blade.php
@@ -97,7 +97,7 @@
 		</div>
 	</div>
 	<div class="container">
-			<div class="row">
+			<div class="row block">
 				<div class="col-sm-6">
 					<a href="#">
 						<div class="recruiter-feature-box featured">
