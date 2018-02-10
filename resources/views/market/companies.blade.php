8ef15a73d994ef29eb752309eb2b7fac2ccb0b1c
Mauricio <mauri@sumra.net>
2018-02-10 15:44:26 +0000

    working in the pages for search recruiter and explore companiesç

--- resources/views/market/companies.blade.php
+++ resources/views/market/companies.blade.php
@@ -82,60 +82,7 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2 col-xs-12">
-				<div class="search-companies">
-					<div class="search-companies-center">
-						<div class="search-companies-wrap">
-							<ul class="nav nav-tabs">
-								<li class="active">
-									<a href="#search-companies" data-toggle="tab">Employers</a>
-								</li>
-								<li>
-									<a href="#search-companies" data-toggle="tab">Recruitment Agencies</a>
-								</li>
-							</ul>
-							<div class="tab-content">
-								<div class="search-companies-search fade in">
-									<h2>Search Employers</h2>
-									<form action="">
-										<input type="text" name="q" value="" placeholder="Employer Name">
-										<input type="submit" value="Search Employers">
-									</form>
-									<h3>Employers Directory A-Z</h3>
-									<ul class="search-companies-az">
-										<li><a href="">0-9</a></li>
-										<li><a href="">A</a></li>
-										<li><a href="">B</a></li>
-										<li><a href="">C</a></li>
-										<li><a href="">D</a></li>
-										<li><a href="">E</a></li>
-										<li><a href="">F</a></li>
-										<li><a href="">G</a></li>
-										<li><a href="">H</a></li>
-										<li><a href="">I</a></li>
-										<li><a href="">J</a></li>
-										<li><a href="">K</a></li>
-										<li><a href="">L</a></li>
-										<li><a href="">M</a></li>
-										<li><a href="">N</a></li>
-										<li><a href="">O</a></li>
-										<li><a href="">P</a></li>
-										<li><a href="">Q</a></li>
-										<li><a href="">R</a></li>
-										<li><a href="">S</a></li>
-										<li><a href="">T</a></li>
-										<li><a href="">U</a></li>
-										<li><a href="">V</a></li>
-										<li><a href="">W</a></li>
-										<li><a href="">Y</a></li>
-										<li><a href="">Z</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			
 		</div>
 	</div>
 </div>
