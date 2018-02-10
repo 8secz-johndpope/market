eb69c9174354ff16c25bdbe5c56b9b7d66483b10
Mauricio <mauri@sumra.net>
2018-02-10 22:50:35 +0000

    working in the pages for search recruiter and explore companies

--- resources/views/market/companies.blade.php
+++ resources/views/market/companies.blade.php
@@ -13,6 +13,7 @@
 @endphp
 @section('content')
  <link href="{{asset("/css/companies.css?q=$dateMs")}}" rel="stylesheet">
+ <link rel="stylesheet" href="/build/css/intlTelInput.css">
  <div class="body background-body page-companies">
 	<div class="top-hero-section">
 		<div class="bg-image">
