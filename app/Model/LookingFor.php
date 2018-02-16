d7c13d6d24f1643d32b4d1d0c92f394b9b471bbf
mjmondragon <mauro102189@gmail.com>
2018-02-16 14:38:21 +0000

    add new object for profile

--- app/Model/LookingFor.php
+++ app/Model/LookingFor.php
@@ -19,7 +19,7 @@ class LookingFor extends Model
 	}
 	public function jobTypes()
 	{
-		return $this->belongsToMany('App\Model\Field', 'looking_for_contract_type', 'field_id', 'looking_for_id');
+		return $this->belongsToMany('App\Model\Field', 'looking_for_contract_type', 'looking_for_id','field_id');
 	}
 	public function sectors()
 	{
--- resources/views/home/looking_for_edit.blade.php
+++ resources/views/home/looking_for_edit.blade.php
@@ -126,6 +126,9 @@
                   <fieldset class="form-field">
                     @foreach($contractTypes as $contractType)
                     <div class="checkbox">
+                      @php
+                        var_dump($lookingFor->jobTypes);
+                      @endphp
                       <input type="checkbox" name="contract_type[]" id="{{$contractType->slug}}-work" value="{{$contractType->id}}">
                       <label for="{{$contractType->slug}}-work">{{$contractType->title}}</label>
                     </div>
