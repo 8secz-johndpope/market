1102a5c7c88ef2b3b02c73d5c7a54b19644d1a1e
sumra <sumra@d204.tld>
2018-02-17 21:01:32 +0000

    new

---
+++ app/Model/WorkExperience.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anil
+ * Date: 29/11/2017
+ * Time: 15:27
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+
+class WorkExperience extends Model
+{
+	public $timestamps = false;
+}
