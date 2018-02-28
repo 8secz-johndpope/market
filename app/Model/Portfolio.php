bf7e52376a44176b65aa50188874be973303dc16
mjmondragon <mauro102189@gmail.com>
2018-02-28 21:03:36 +0000

    adding portfolio information sub-contractor profile

--- app/Model/Portfolio.php
+++ app/Model/Portfolio.php
@@ -10,4 +10,8 @@ use Illuminate\Database\Eloquent\Model;
 class Portfolio extends Model
 {
 	public $timestamps = false;
+
+	public function images(){
+		return $this->hasMany('App\Model\Image');
+	}
 }
