2915c020b3bb718fa9383e054743369e6a885563
mjmondragon <mauro102189@gmail.com>
2018-03-03 18:00:30 +0000

    add drive google upload cv

--- resources/views/home/upload_cv_cloud.blade.php
+++ resources/views/home/upload_cv_cloud.blade.php
@@ -127,7 +127,7 @@
 @endsection
 @section('scripts')
 <script src="/js/filepicker.js"></script>
-<script>
+<!-- <script>
   function initPicker() {
     var picker = new FilePicker({
       apiKey: 'AIzaSyCZbnf5ZxF0xDYINzcfITR_5E8XVkFD8Hs',
@@ -140,7 +140,74 @@
     }); 
     console.log('created picker');
   }
-</script>
+</script> -->
+<script type="text/javascript">
+
+      // The Browser API key obtained from the Google API Console.
+      var developerKey = 'AIzaSyCZbnf5ZxF0xDYINzcfITR_5E8XVkFD8Hs';
+
+      // The Client ID obtained from the Google API Console. Replace with your own Client ID.
+      var clientId = '847435132616-7k2capek98p6pspvbsjum0rsugcb0kuh.apps.googleusercontent.com';
+
+      // Scope to use to access user's photos.
+      var scope = 'https://www.googleapis.com/auth/photos';
+
+      var pickerApiLoaded = false;
+      var oauthToken;
+
+      // Use the API Loader script to load google.picker and gapi.auth.
+      function onApiLoad() {
+        gapi.load('auth2', onAuthApiLoad);
+        gapi.load('picker', onPickerApiLoad);
+      }
+
+      function onAuthApiLoad() {
+        var authBtn = document.getElementById('googledrive');
+        authBtn.disabled = false;
+        authBtn.addEventListener('click', function() {
+          gapi.auth2.authorize({
+            client_id: clientId,
+            scope: scope
+          }, handleAuthResult);
+        });
+      }
+
+      function onPickerApiLoad() {
+        pickerApiLoaded = true;
+        createPicker();
+      }
+
+      function handleAuthResult(authResult) {
+        if (authResult && !authResult.error) {
+          oauthToken = authResult.access_token;
+          createPicker();
+        }
+      }
+
+      // Create and render a Picker object for picking user Photos.
+      function createPicker() {
+        if (pickerApiLoaded && oauthToken) {
+          var picker = new google.picker.PickerBuilder().
+              addView(google.picker.ViewId.PHOTOS).
+              setOAuthToken(oauthToken).
+              setDeveloperKey(developerKey).
+              setCallback(pickerCallback).
+              build();
+          picker.setVisible(true);
+        }
+      }
+
+      // A simple callback implementation.
+      function pickerCallback(data) {
+        var url = 'nothing';
+        if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+          var doc = data[google.picker.Response.DOCUMENTS][0];
+          url = doc[google.picker.Document.URL];
+        }
+        var message = 'You picked: ' + url;
+        document.getElementById('result').innerHTML = message;
+      }
+    </script>
 <script>
   $('#upload-cv-link').click(function () {
         var title = $('#title').val();
@@ -182,5 +249,5 @@
 </script>
 <script src="https://www.google.com/jsapi?key=AIzaSyCZbnf5ZxF0xDYINzcfITR_5E8XVkFD8Hs"></script>
 <script src="https://apis.google.com/js/client:plusone.js" type="text/javascript"></script>
-<script type="text/javascript" src="https://apis.google.com/js/api.js?onload=initPicker"></script>
+<script type="text/javascript" src="https://apis.google.com/js/api.js?onload=onApiLoad"></script>
 @endsection
