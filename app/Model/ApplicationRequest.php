1970d661dfb952b9d4b7da762b994785af94a605
mjmondragon <mauro102189@gmail.com>
2018-03-12 17:37:00 +0000

    adding candidate portal

--- app/Model/ApplicationRequest.php
+++ app/Model/ApplicationRequest.php
@@ -10,4 +10,7 @@ class ApplicationRequest extends Model
 	public function advert(){
 		return $this->belongsTo('App\Model\Advert');
 	}
+	public function employer(){
+		return $this->belongsTo('App\User', 'user_id');
+	}
 }
