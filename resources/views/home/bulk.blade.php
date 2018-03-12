19cdba2010b7bb93600708898a4ae5e0606e1874
mjmondragon <mauro102189@gmail.com>
2018-03-12 21:26:29 +0000

    working in candidate portal, adding bulk apply in application request

--- resources/views/home/bulk.blade.php
+++ resources/views/home/bulk.blade.php
@@ -22,40 +22,37 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8">
-                <br>
-                <br><br>
-                <h4>Bulk Apply</h4>
+                <h3>Bulk Apply</h3>
                 <form action="/user/jobs/apply/all" method="post">
                     {{ csrf_field() }}
-
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Delete</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($adverts as $advert)
+                    <table class="table table-hover">
+                        <thead>
                         <tr>
-                            <td><a href="{{$advert->url()}}"> {{$advert->param('title')}}</a>
-                                <input required="Please select at least one appplication" class="select-application" type="hidden" name="ids[]" value="{{$advert->id}}">
-
-                            </td>
-                            <td>
-                                <button class="btn btn-danger" onclick="$(this).parent().parent().remove();">Delete</button> 
-                            </td>
-
+                            <th scope="col">Title</th>
+                            <th scope="col">Delete</th>
                         </tr>
-                        @endforeach
-                        <tr>
-                            <td>
-                                <button type="submit" class="btn btn-primary">Apply All</button>
-                            </td>
+                        </thead>
+                        <tbody>
+                            @foreach($adverts as $advert)
+                            <tr>
+                                <td><a href="{{$advert->url()}}"> {{$advert->param('title')}}</a>
+                                    <input required="Please select at least one appplication" class="select-application" type="hidden" name="ids[]" value="{{$advert->id}}">
 
-                        </tr>
-                    </tbody>
-                </table>
+                                </td>
+                                <td>
+                                    <button class="btn btn-danger" onclick="$(this).parent().parent().remove();">Delete</button> 
+                                </td>
+
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <td>
+                                    <button type="submit" class="btn btn-primary">Apply All</button>
+                                </td>
+
+                            </tr>
+                        </tbody>
+                    </table>
                 </form>
             </div>
         </div>
