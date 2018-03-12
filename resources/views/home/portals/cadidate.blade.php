92b4308bdc3082a2c26f8f12402f1ef7a3be6a5a
mjmondragon <mauro102189@gmail.com>
2018-03-12 21:58:08 +0000

    working in candidate portal, adding bulk apply in application request

--- resources/views/home/portals/cadidate.blade.php
+++ resources/views/home/portals/cadidate.blade.php
@@ -291,7 +291,7 @@
                                                     <tr>
                                                         <td>
                                                             <div class="checkbox">
-                                                                <input type="checkbox" name="ids[]" class="request-applications" value="{{$request->advert->id}}">
+                                                                <input type="checkbox" name="ids[]" class="request-applications" value="{{$request->id}}">
                                                             </div>
                                                         </td>
                                                         <td>
