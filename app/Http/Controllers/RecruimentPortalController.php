35569fd98135ffd69bce31612df5efdee7efd859
mjmondragon <mauro102189@gmail.com>
2018-03-14 20:50:39 +0000

    working in recruimentportal

--- app/Http/Controllers/RecruimentPortalController.php
+++ app/Http/Controllers/RecruimentPortalController.php
@@ -27,11 +27,21 @@ class RecruimentPortalController extends BaseController
     {
         $user = Auth::user();
         $myInvitations = $user->applicationInvitations;
+        $jobsNewCandidates = $this->jobsWithNewCandidates();
         $invitationStatus = ApplicationRequest::STATUS;
         $applicationStatus = Application::STATUS_EMPLOYER;
         $jobStatus = Advert::STATUS;
         $balance = \Stripe\Balance::retrieve( array("stripe_account" => $user->stripe_account));
         return view('home.portals.applications',['jobs'=>$user->jobs,'user'=>$user, 'balance' => $balance, 'myInvitations' => $myInvitations, 'invitationStatus' => $invitationStatus, 'applicationStatus' => $applicationStatus]);
+    }
+    public function jobsWithNewCandidates(){
+        $use = Auth::user();
+        $myJobs = $user->applicationInvitations;
+        $jobsWithNewCandidates =  $myJobs->filter(function($value, $key){
+            return $value->applications()->where('status_employer', 0)->count() > 0;
+        });
+        return $jobsWithNewCandidates;
+
     }
 }
 ?>
