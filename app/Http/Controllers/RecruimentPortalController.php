057499c068ec9d5116b4b51f742720c284f95671
mjmondragon <mauro102189@gmail.com>
2018-03-14 20:01:23 +0000

    working in recruiment portal

--- app/Http/Controllers/RecruimentPortalController.php
+++ app/Http/Controllers/RecruimentPortalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Model\Application;
+use App\Model\Advert;
 use App\Model\ApplicationRequest;
 
 class RecruimentPortalController extends BaseController
@@ -28,8 +29,9 @@ class RecruimentPortalController extends BaseController
         $myInvitations = $user->applicationInvitations;
         $invitationStatus = ApplicationRequest::STATUS;
         $applicationStatus = Application::STATUS;
+        $jobStatus = Advert::STATUS;
         $balance = \Stripe\Balance::retrieve( array("stripe_account" => $user->stripe_account));
-        return view('home.portals.applications',['jobs'=>$user->jobs(),'user'=>$user, 'balance' => $balance, 'myInvitations' => $myInvitations, 'invitationStatus' => $invitationStatus, 'applicationStatus' => $applicationStatus]);
+        return view('home.portals.applications',['jobs'=>$user->jobs(),'user'=>$user, 'balance' => $balance, 'myInvitations' => $myInvitations, 'invitationStatus' => $invitationStatus, 'applicationStatus' => $applicationStatus, 'jobStatus' => $jobStatus]);
     }
 }
 ?>
