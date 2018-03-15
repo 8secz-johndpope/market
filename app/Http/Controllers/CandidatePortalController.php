c55926ea9b53964075caae1dc2ee1240df674f62
mjmondragon <mauro102189@gmail.com>
2018-03-15 18:42:52 +0000

    working stuff accept invitation one job

--- app/Http/Controllers/CandidatePortalController.php
+++ app/Http/Controllers/CandidatePortalController.php
@@ -138,6 +138,7 @@ class CandidatePortalController extends BaseController
     }
     public function acceptApply(Request $request){
         $user = Auth::user();
+        return $request->request;
         $requestApplication = ApplicationRequest::find($request->request);
         $advert = $requestApplication->advert;
         $profile = null;
