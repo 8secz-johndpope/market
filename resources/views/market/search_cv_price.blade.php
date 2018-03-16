9ba69c52483f11c3eaf14d87a7b04f08a1e74092
mjmondragon <mauro102189@gmail.com>
2018-03-16 22:51:45 +0000

    working in search cv price page

--- resources/views/market/search_cv_price.blade.php
+++ resources/views/market/search_cv_price.blade.php
@@ -63,8 +63,43 @@
 							<span>&nbsp;</span>
 						</div>
 						<ul class="profilesearch-page-selector">
-							
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
 						</ul>
+						<div class="profilesearch-description">
+							<header>
+								<p class="lense-header">View CVs</p>
+							</header>
+							<p class="lense-description">
+								Save CVs as Word or PDF documents & view candidate profiles online. You can access up to 100 new candidate CVs per day.
+							</p>
+						</div>
+						<span class="prev">previous</span>
+						<span class="next">Next</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="cvsearch-prices">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<header class="heading-container">
+						<h3>Profile & CV Search prices</h3>
+					</header>
+					<div class="profilesearch-price-table">
+						<div class="row">
+							<div class="col-md-3">
+								
+							</div>
+							<div class="col-md-3"></div>
+							<div class="col-md-3"></div>
+							<div class="col-md-3"></div>
+						</div>
 					</div>
 				</div>
 			</div>
