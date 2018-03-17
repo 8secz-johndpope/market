921cc25459bf2e7d6fe1110d184fcb664f4c093f
mjmondragon <mauro102189@gmail.com>
2018-03-17 18:13:49 +0000

    working in search cv price page

--- resources/views/home/recruiter/search_profile.blade.php
+++ resources/views/home/recruiter/search_profile.blade.php
@@ -21,7 +21,45 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                
+                <header>
+                    <h3>I am looking for candidates in:</h3>
+                </header>
+                <div class="row">
+                    <div class="col-sm-4">
+                        General Sector
+                    </div>
+                     <div class="col-sm-4">
+                        Social & Childcare Sector
+                    </div>
+                     <div class="col-sm-4">
+                        Sub Contractor Sector
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="candidates-container">
+                    @foreach($profiles as $profile)
+                    <div class="candidate">
+                        <div class="candidate-wrapper">
+                            <div class="candiate-picture">
+                                <figure>
+                                    <img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$profile->user->image}}">
+                                </figure>
+                            </div>
+                            <div class="candidate-details-wrapper">
+                                <h3 class="candidate-name">{{$profile->user->name}}</h3>
+                                @if(issset($profile->user->address))
+                                <p class="candidate-location">{{$profile->user->address->city}}</p>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="candidate-actions">
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
