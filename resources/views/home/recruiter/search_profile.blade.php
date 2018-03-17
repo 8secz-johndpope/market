0e80ca0ac0037e56b821006897df435621b19cec
mjmondragon <mauro102189@gmail.com>
2018-03-17 18:45:45 +0000

    working in search cv price page

--- resources/views/home/recruiter/search_profile.blade.php
+++ resources/views/home/recruiter/search_profile.blade.php
@@ -49,7 +49,7 @@
                                 </figure>
                             </div>
                             <div class="candidate-details-wrapper">
-                                <h3 class="candidate-name">{{$profile->user->name}}</h3>
+                                <h3 class="candidate-name"><a href="/job/profile/{{$profile->id}}">{{$profile->user->name}}</a></h3>
                                 @if(isset($profile->user->address))
                                 <p class="candidate-location">{{$profile->user->address->city}}</p>
                                 @endif
