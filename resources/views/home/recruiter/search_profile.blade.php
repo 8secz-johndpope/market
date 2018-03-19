e98f4e0f81884ca1dcd9b5d6c99a2298af19468e
mjmondragon <mauro102189@gmail.com>
2018-03-19 17:58:37 +0000

    working in UI for search profiles

--- resources/views/home/recruiter/search_profile.blade.php
+++ resources/views/home/recruiter/search_profile.blade.php
@@ -72,73 +72,76 @@
         </div>
         <div class="row">
             <div class="col-sm-12">
-                <div class="candidates-container">
-                    @foreach($profiles as $profile)
-                    <div class="candidate">
-                        <div class="candidate-wrapper">
-                            <div class="checkbox">
-                                <input type="checkbox" name="candidate_id[]" id="candidate-{{$profile->id}}" value="{{$profile->id}}" class="candidates-checkbox">
-                                <label for="candidate-{{$profile->id}}">
-                                    Select
-                                </label>
-                            </div>
-                            <div class="candidate-picture">
-                                <figure>
-                                    <img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$profile->user->image}}">
-                                </figure>
-                            </div>
-                            <div class="candidate-details-wrapper">
-                                <h3 class="candidate-name"><a href="/job/profile/{{$profile->id}}">{{$profile->user->name}}</a></h3>
-                                @if(isset($profile->user->address))
-                                <p class="candidate-location">{{$profile->user->address->city}}</p>
-                                @endif
-                                @if(isset($profile->looking_for))
-                                    @if(isset($profile->looking_for->job_title))
-                                    <strong>{{$profile->looking_for->job_title}}</strong>
-                                    @endif
-                                    @if(isset($profile->looking_for->min_per_annum) && isset($profile->looking_for->min_per_hour))
-                                    <p>{{$profile->looking_for->min_per_annum}} per annum or {{$profile->looking_for->min_per_hour}} per hour</p>
-                                    @elseif(isset($profile->looking_for->min_per_annum))
-                                    <p>{{$profile->looking_for->min_per_annum}} per annum</p>
-                                    @elseif(isset($profile->looking_for->min_per_hour))
-                                    <p>{{$profile->looking_for->min_per_hour}} per hour</p>
-                                    @endif
-                                @endif
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="candidate-actions text-center">
-                            <div class="row">
-                                <div class="col-xs-4">
-                                    <div class="box">
-                                        <span class="heart-empty favroite-icon"></span>
-                                        <span>Save</span>
-                                    </div>
+                <form action="/recruiter/candidate/application-request/all" method="post">
+                    {{ csrf_field() }}
+                    <div class="candidates-container">
+                        @foreach($profiles as $profile)
+                        <div class="candidate">
+                            <div class="candidate-wrapper">
+                                <div class="checkbox">
+                                    <input type="checkbox" name="candidate_id[]" id="candidate-{{$profile->id}}" value="{{$profile->id}}" class="candidates-checkbox">
+                                    <label for="candidate-{{$profile->id}}">
+                                        Select
+                                    </label>
                                 </div>
-                                <div class="col-xs-4">
-                                    <div class="box">
-                                        <div class="dropdown download-cvs">
-                                            <button class="btn btn-link dropdown-toggle" type="button" data-toggle="dropdown"">Download CV<span class="caret"></span></button>
-                                            <ul class="dropdown-menu">
-                                                @foreach($profile->user->cvs as $cv)
-                                                <li>
-                                                    <a href="{{env('AWS_CV_IMAGE_URL')}}/{{$cv->filename}}" target="_blank" class="download-cv">{{$cv->title}}</a>
-                                                </li>
-                                                @endforeach
-                                            </ul>
+                                <div class="candidate-picture">
+                                    <figure>
+                                        <img src="{{env('AWS_WEB_IMAGE_URL')}}/{{$profile->user->image}}">
+                                    </figure>
+                                </div>
+                                <div class="candidate-details-wrapper">
+                                    <h3 class="candidate-name"><a href="/job/profile/{{$profile->id}}">{{$profile->user->name}}</a></h3>
+                                    @if(isset($profile->user->address))
+                                    <p class="candidate-location">{{$profile->user->address->city}}</p>
+                                    @endif
+                                    @if(isset($profile->looking_for))
+                                        @if(isset($profile->looking_for->job_title))
+                                        <strong>{{$profile->looking_for->job_title}}</strong>
+                                        @endif
+                                        @if(isset($profile->looking_for->min_per_annum) && isset($profile->looking_for->min_per_hour))
+                                        <p>{{$profile->looking_for->min_per_annum}} per annum or {{$profile->looking_for->min_per_hour}} per hour</p>
+                                        @elseif(isset($profile->looking_for->min_per_annum))
+                                        <p>{{$profile->looking_for->min_per_annum}} per annum</p>
+                                        @elseif(isset($profile->looking_for->min_per_hour))
+                                        <p>{{$profile->looking_for->min_per_hour}} per hour</p>
+                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="candidate-actions text-center">
+                                <div class="row">
+                                    <div class="col-xs-4">
+                                        <div class="box">
+                                            <span class="heart-empty favroite-icon"></span>
+                                            <span>Save</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <div class="box">
+                                            <div class="dropdown download-cvs">
+                                                <button class="btn btn-link dropdown-toggle" type="button" data-toggle="dropdown"">Download CV<span class="caret"></span></button>
+                                                <ul class="dropdown-menu">
+                                                    @foreach($profile->user->cvs as $cv)
+                                                    <li>
+                                                        <a href="{{env('AWS_CV_IMAGE_URL')}}/{{$cv->filename}}" target="_blank" class="download-cv">{{$cv->title}}</a>
+                                                    </li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <div class="box">
+                                            <button class="btn btn-link" data-toggle="modal">Request Application</button>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-xs-4">
-                                    <div class="box">
-                                        <button class="btn btn-link" data-toggle="modal">Request Application</button>
-                                    </div>
-                                </div>
                             </div>
                         </div>
+                        @endforeach
                     </div>
-                    @endforeach
-                </div>
+                </form>
             </div>
         </div>
     </div>
