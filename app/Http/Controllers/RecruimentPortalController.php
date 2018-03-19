1a30ad43df78852e278791fbe052a6e46fdf29fe
mjmondragon <mauro102189@gmail.com>
2018-03-19 21:34:39 +0000

    working in iu search profiles

--- app/Http/Controllers/RecruimentPortalController.php
+++ app/Http/Controllers/RecruimentPortalController.php
@@ -211,9 +211,11 @@ class RecruimentPortalController extends BaseController
                             ->where('profiles.status', 1);
         if($request->has('type'))
             $profiles->where('profiles.type', $request->type);
-        if($request->has('job_title')){
+        if($request->has('job_title') || $request->has('location')){
             $profiles->join('looking_fors', 'profiles.id', '=', 'looking_fors.profile_id')
-                    ->where('looking_fors.job_title', 'like', '%'.$request->job_title.'%');
+        }
+        if($request->has('job_title'))
+            $profiles->where('looking_fors.job_title', 'like', '%'.$request->job_title.'%');
         }
         if($request->has('location')){
             $profiles->join('looking_fors', 'profiles.id', '=', 'looking_fors.profile_id')
