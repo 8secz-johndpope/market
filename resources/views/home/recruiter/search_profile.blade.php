3b81f06c73ade7575091cbd85fa96b59c4e78924
mjmondragon <mauro102189@gmail.com>
2018-03-19 20:25:25 +0000

    working in iu search profiles

--- resources/views/home/recruiter/search_profile.blade.php
+++ resources/views/home/recruiter/search_profile.blade.php
@@ -205,15 +205,13 @@
     $('#modalApplicationRequest').on('show.bs.modal', function(event){
         var button = $(event.relatedTarget);
         var request = button.data('whatever');
-        console.log($('.candidates-checkbox:checked').length);
-        console.log(request);
-        if(request == 'all' && $('.candidates-checkbox:checked').length < 1){
-            $('.modal-footer btn-submit').prop('disabled', true);
-            $('.modal validation span').text('Select candidates');
-            $('.modal validation').show();
+        if(request == 'all' && $('.candidates-checkbox:checked').length == 0){
+            $('.modal-footer .btn-submit').prop('disabled', true);
+            $('.modal .validation span').text('Select candidates');
+            $('.modal .validation').show();
         }else{
-            $('.modal validation').hide();
-            $('.modal-footer btn-submit').prop('disabled', false);
+            $('.modal .validation').hide();
+            $('.modal-footer .btn-submit').prop('disabled', false);
             var modal = $(this);
             modal.find('#profile').val(request);
         }
