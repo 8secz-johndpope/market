454c4f6f12b644f3697b8091bfc644f57eb31056
mjmondragon <mauro102189@gmail.com>
2018-03-19 21:11:22 +0000

    working in iu search profiles

--- app/Http/Controllers/RecruimentPortalController.php
+++ app/Http/Controllers/RecruimentPortalController.php
@@ -209,11 +209,11 @@ class RecruimentPortalController extends BaseController
     }
     public function getProfileByQuery(Request $request){
         $profiles = DB::table('profiles')
-                            ->join('looking_fors', 'profiles.id', '=', 'looking_fors.profile_id')
                             //->join('looking_for_location', 'looking_fors.id', '=', 'looking_for_location.looking_for_id')
                             ->where('profiles.status', 1);
         if($request->has('job_title')){
-            $profiles->where('looking_fors.job_title', 'like', $request->job_title);
+            $profile->join('looking_fors', 'profiles.id', '=', 'looking_fors.profile_id')
+                    ->where('looking_fors.job_title', 'like', $request->job_title);
         }
         if($request->has('location')){
             $profiles->where('looking_for_location.location_id', $request->location);
